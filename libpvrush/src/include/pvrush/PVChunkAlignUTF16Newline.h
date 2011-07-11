99103a687aab26c8c6940b1ad74403a368723d0b
Adrien Guinet <aguinet@picviz.com>
2011-07-11 17:45:06 +0200

    PVChunkAlignUTF16Newline: takes into account a possible '\r' and remove it

---
+++ libpvrush/src/include/pvrush/PVChunkAlignUTF16Newline.h
@@ -0,0 +1,30 @@
+#ifndef PVCHUNKALIGNUTF16NEWLINE_FILE_H
+#define PVCHUNKALIGNUTF16NEWLINE_FILE_H
+
+#include <pvcore/general.h>
+#include <pvcore/PVChunk.h>
+#include <pvrush/PVChunkAlign.h>
+#include <pvrush/PVChunkAlignUTF16Char.h>
+
+#include <QString>
+
+namespace PVRush {
+
+/*! \brief Alignement class on an UTF16 characters.
+ * This class herits from PVChunkAlign and is used to align chunks on an UTF16 characters.
+ *
+ * \todo Currently, this class is used to align text file chunks on '\\n'.
+ * It would be better to have a PVChunkAlignBoundary that uses QTextBoundaryFinder.
+ */
+class LibExport PVChunkAlignUTF16Newline : public PVChunkAlign {
+public:
+	PVChunkAlignUTF16Newline();
+public:
+	virtual bool operator()(PVCore::PVChunk &cur_chunk, PVCore::PVChunk &next_chunk);
+protected:
+	PVChunkAlignUTF16Char _align_char;
+};
+
+}
+
+#endif
