14a9a4bf2cef6b21b7aa255fbb0ec2db76ad6b27
Adrien Guinet <aguinet@picviz.com>
2011-07-12 16:35:04 +0200

    Show an error box when unable to write the format file

--- gui-qt/src/widgets/format-builder/PVXmlDomModel.cpp
+++ gui-qt/src/widgets/format-builder/PVXmlDomModel.cpp
@@ -292,12 +292,16 @@ bool PVInspector::PVXmlDomModel::setData(const QModelIndex & index, const QVaria
 bool PVInspector::PVXmlDomModel::saveXml(QString nomDuFichierXml){
     QFile file(nomDuFichierXml);
     if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
-		QMessageBox msg(QMessageBox::Critical, "Error while saving format", QString("An error occured while saving format: ") + file.errorString(), QMessageBox::Ok);
+		QMessageBox msg(QMessageBox::Critical, "Error while saving format", QString("Unable to open %1 for writing: ").arg(nomDuFichierXml) + file.errorString(), QMessageBox::Ok);
 		msg.exec();
 		return false;
 	}
 
-    file.write(xmlFile.toString().toUtf8());
+    if (file.write(xmlFile.toString().toUtf8()) == -1) {
+		QMessageBox msg(QMessageBox::Critical, "Error while saving format", QString("An error occured while saving format: ") + file.errorString(), QMessageBox::Ok);
+		msg.exec();
+		return false;
+	}
     file.close();
 
 	return true;
