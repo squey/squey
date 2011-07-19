301227ca7beea5702b6bf529dfa542bdd56dbe32
Adrien Guinet <aguinet@picviz.com>
2011-07-19 16:09:47 +0200

    [time-mapping] * Do not run in parallel for now, because object allocations take about 3/4s...

--- libpicviz/plugins/mapping-filters/time/default/PVMappingFilterTimeDefault.cpp
+++ libpicviz/plugins/mapping-filters/time/default/PVMappingFilterTimeDefault.cpp
@@ -21,37 +21,36 @@ float* Picviz::PVMappingFilterTimeDefault::operator()(PVRush::PVNraw::nraw_table
 	assert(values.size() >= _dest_size);
 	assert(_format);
 
-	PVCore::PVDateTimeParser dtpars_org(_format->time_format[_cur_col+1]);
-
-	// TODO: vectorize this for RWBITS bits register width by loading N=RWBITS/sizeof(float) time structures
-	// and compute from dest[i] to dest[i+N] using vectorized operations !
-	// It would use SSE in a first time, and AVX when this will be supported !
-	
+	PVCore::PVDateTimeParser dtpars(_format->time_format[_cur_col+1]);
+	UErrorCode err = U_ZERO_ERROR;
+	Calendar* cal = Calendar::createInstance(err);
 	// Create calender and parsers objects that will be used by our threads
+#if 0
 	const int max_threads = omp_get_max_threads();
 #ifdef WIN32
 	Calendar** cals = (Calendar**) malloc(max_threads*sizeof(Calendar*));
-	PVCore::PVDateTimeParser *dtparsers = new PVCore::PVDateTimeParser[max_threads];
+	PVCore::PVDateTimeParser **dtparsers = (PVCore::PVDateTimeParser**) malloc(max_threads*sizeof(PVCore::PVDateTimeParser*));
 #else
 	Calendar* cals[max_threads];
-	PVCore::PVDateTimeParser dtparsers[max_threads];
+	PVCore::PVDateTimeParser *dtparsers[max_threads];
 #endif
 	for (int i = 0; i < max_threads; i++) {
 		UErrorCode err = U_ZERO_ERROR;
 		cals[i] = Calendar::createInstance(err);
-		dtparsers[i] = dtpars_org;
+		dtparsers[i] = new PVCore::PVDateTimeParser(_format->time_format[_cur_col+1]);
 	}
+#endif
 
 	int64_t size = _dest_size;
 	// TODO: compare TBB and OpenMP here !!
-#pragma omp parallel for
+//#pragma omp parallel for
 	for (int64_t i = 0; i < size; i++) {
 		int thread_num = omp_get_thread_num();
-		Calendar* cal = cals[thread_num];
-		PVCore::PVDateTimeParser &dtpars = dtparsers[thread_num];
+		//Calendar* cal = _cals[thread_num];
+		//PVCore::PVDateTimeParser &dtpars = *(dtparsers[thread_num]);
 		bool ret = dtpars.mapping_time_to_cal(values[i], cal);
 		if (!ret) {
-#pragma omp critical
+//#pragma omp critical
 			{
 				PVLOG_WARN("(time-24h mapping) unable to map time string %s. Returns 0 !\n", qPrintable(values[i]));
 			}
@@ -62,7 +61,7 @@ float* Picviz::PVMappingFilterTimeDefault::operator()(PVRush::PVNraw::nraw_table
 		bool success;
 		_dest[i] = cal_to_float(cal, success);
 		if (!success) {
-#pragma omp critical
+//#pragma omp critical
 			{
 				PVLOG_WARN("(time-24h mapping) unable to map time string %s: one field is missing. Returns 0 !\n", qPrintable(values[i]));
 			}
@@ -72,13 +71,17 @@ float* Picviz::PVMappingFilterTimeDefault::operator()(PVRush::PVNraw::nraw_table
 	}
 
 	// Frees the calendar objects
+#if 0
 	for (int i = 0; i < max_threads; i++) {
 		delete cals[i];
+		delete dtparsers[i];
 	}
-#ifdef WIN32
-	free(cals);
-	delete [] dtparsers;
 #endif
+#ifdef WIN32
+	//free(cals);
+	//free(dtparsers);
+#endif
+	delete cal;
 
 	return _dest;
 }
