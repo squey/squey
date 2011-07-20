a9ec338718ce587052befc27769f1ab842f8018b
Sebastien Tricaud <sebastien@honeynet.org>
2011-07-20 20:19:02 +0200

    Add headers from libdnet to deal with cross-platform issues with network

---
+++ libpvcore/src/include/pvcore/net/general.h
@@ -0,0 +1,31 @@
+/*
+ * dnet.h
+ *
+ * Copyright (c) 2001 Dug Song <dugsong@monkey.org>
+ *
+ * $Id: dnet.h 529 2004-09-10 03:10:01Z dugsong $
+ */
+
+#ifndef DNET_H
+#define DNET_H
+
+#include <dumbnet/os.h>
+
+#include <dumbnet/eth.h>
+#include <dumbnet/ip.h>
+#include <dumbnet/ip6.h>
+#include <dumbnet/addr.h>
+#include <dumbnet/arp.h>
+#include <dumbnet/icmp.h>
+#include <dumbnet/tcp.h>
+#include <dumbnet/udp.h>
+
+#include <dumbnet/intf.h>
+#include <dumbnet/route.h>
+#include <dumbnet/fw.h>
+#include <dumbnet/tun.h>
+
+#include <dumbnet/blob.h>
+#include <dumbnet/rand.h>
+
+#endif /* DNET_H */
