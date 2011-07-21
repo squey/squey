246a2ebabfa5c580437b90f36f7eecccae3344f1
Sebastien Tricaud <sebastien@honeynet.org>
2011-07-21 09:52:58 +0200

    Set the PICVIZ_LINES_MAX to a value that behaves OK when doing selection on big files. See ticket:46

--- libpvcore/src/include/pvcore/general.h
+++ libpvcore/src/include/pvcore/general.h
@@ -38,7 +38,7 @@ static QSettings pvconfig(QString("pvconfig.ini"), QSettings::IniFormat);
 #define picviz_max(x,y) ((x)>(y)?(x):(y))
 #define picviz_min(x,y) ((x)<(y)?(x):(y))
 
-#define PICVIZ_LINES_MAX   1000000
+#define PICVIZ_LINES_MAX 1048576
 
 #define PICVIZ_EVENTLINE_LINES_MAX PICVIZ_LINES_MAX
 #define PICVIZ_AXES_MAX 8096 // Max number of axes
