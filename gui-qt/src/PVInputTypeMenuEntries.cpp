ade0c7847bba5442ff41a6efea287bc0e7f0efb4
Adrien Guinet <aguinet@picviz.com>
2011-07-21 11:29:09 +0200

    Forgot 2 files

---
+++ gui-qt/src/PVInputTypeMenuEntries.cpp
@@ -0,0 +1,27 @@
+#include <PVInputTypeMenuEntries.h>
+#include <pvrush/PVInputType.h>
+#include <pvcore/PVClassLibrary.h>
+
+void PVInspector::PVInputTypeMenuEntries::add_inputs_to_menu(QMenu* menu, QObject* parent, const char* slot)
+{
+	LIB_CLASS(PVRush::PVInputType) &input_types = LIB_CLASS(PVRush::PVInputType)::get();
+	LIB_CLASS(PVRush::PVInputType)::list_classes const& lf = input_types.get_list();
+	
+	LIB_CLASS(PVRush::PVInputType)::list_classes::const_iterator it;
+
+	for (it = lf.begin(); it != lf.end(); it++) {
+		PVRush::PVInputType_p in = it.value();
+		QAction* action = new QAction(in->menu_input_name(), parent);
+		action->setData(QVariant(it.key()));
+		action->setShortcut(in->menu_shortcut());
+		QObject::connect(action, SIGNAL(triggered()), parent, slot);
+		menu->addAction(action);
+	}
+}
+
+PVRush::PVInputType_p PVInspector::PVInputTypeMenuEntries::input_type_from_action(QAction* action)
+{
+	QString const& itype = action->data().toString();
+	PVRush::PVInputType_p in_t = LIB_CLASS(PVRush::PVInputType)::get().get_class_by_name(itype);
+	return in_t;
+}
---
+++ gui-qt/src/include/PVInputTypeMenuEntries.h
@@ -0,0 +1,20 @@
+#ifndef PVINPUTTYPEMENUENTRIES_H
+#define PVINPUTTYPEMENUENTRIES_H
+
+#include <QMenu>
+#include <QObject>
+
+#include <pvrush/PVInputType.h>
+
+namespace PVInspector {
+
+class PVInputTypeMenuEntries
+{
+public:
+	static void add_inputs_to_menu(QMenu* menu, QObject* parent, const char* slot);
+	static PVRush::PVInputType_p input_type_from_action(QAction* action);
+};
+
+}
+
+#endif
