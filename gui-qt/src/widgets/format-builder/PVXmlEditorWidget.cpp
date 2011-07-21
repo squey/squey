ba7f4856d273ce355a927598d1334de483da67f7
rpernaudat <rpernaudat@picviz.com>
2011-07-21 12:31:18 +0200

    init param widget

--- gui-qt/src/widgets/format-builder/PVXmlEditorWidget.cpp
+++ gui-qt/src/widgets/format-builder/PVXmlEditorWidget.cpp
@@ -401,7 +401,7 @@ void PVInspector::PVXmlEditorWidget::slotUpdateToolDesabled(const QModelIndex &i
         actionDelete->setEnabled(true);
     } else if (node->getDom().tagName() == "splitter") {
         myTreeView->expandRecursive(index);
-        myParamBord = 
+        myParamBord = node->getSplitterPlugin()->get_param_widget();
         actionAddFilterAfter->setEnabled(false);
         actionAddAxisIn->setEnabled(false);
         actionAddRegExAfter->setEnabled(false);
--- libpvfilter/plugins/normalize/splitter_csv/PVFieldSplitterCSVParamWidget.cpp
+++ libpvfilter/plugins/normalize/splitter_csv/PVFieldSplitterCSVParamWidget.cpp
@@ -27,7 +27,20 @@ PVFilter::PVFieldSplitterCSVParamWidget::PVFieldSplitterCSVParamWidget(const PVF
 void PVFilter::PVFieldSplitterCSVParamWidget::init()
 {
         PVLOG_DEBUG("init PVFieldSplitterCSVParamWidget\n");
+        /*
+         * action menu init
+         */
         action_menu = new QAction(QString("add CSV Splitter"),NULL);
+        
+        /*
+         * creating the widget param
+         */
+        param_widget = new QWidget();
+        //init layout
+        QBoxLayout* layout = new QBoxLayout(param_widget);
+        param_widget->setLayout(layout);
+        QLabel* label = new QLabel(tr("name"),layout);
+        layout->addWidget(label);
 }
 /******************************************************************************
  *
@@ -36,8 +49,7 @@ void PVFilter::PVFieldSplitterCSVParamWidget::init()
  *****************************************************************************/
 QWidget* PVFilter::PVFieldSplitterCSVParamWidget::get_param_widget()
 {
-        QWidget* w = new QWidget();
-	return w;
+        return param_widget;
 }
 
 
