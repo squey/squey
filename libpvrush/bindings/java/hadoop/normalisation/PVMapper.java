6e88e23199078169f228df1460a2f2f696606575
Adrien Guinet <aguinet@picviz.com>
2011-08-02 09:39:48 +0200

    fix pvcore's cmakelists

--- libpvrush/bindings/java/hadoop/normalisation/PVMapper.java
+++ libpvrush/bindings/java/hadoop/normalisation/PVMapper.java
@@ -7,23 +7,24 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import org.apache.commons.lang.StringUtils;
 
-import PVRushJNI;
+import org.picviz.jni.PVRush.PVRushJNI;
 
 public class PVMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
 	private PVRushJNI jni;
 
 	public void map(LongWritable key, Text v, Context context) throws IOException, InterruptedException {
-		String[] arr = jni.process_elt(v);
+		// TOFIX: Hadoop gives us UTF8 that we convert to UTF16, and vice versa !!!! That is not optimal.
+		String[] arr = jni.process_elt(v.toString());
 		if (arr.length == 0) {
 			return;
 		}
 
-		context.write(key, StringUtils.join(arr, "\t"));
+		context.write(key, new Text(StringUtils.join(arr, "\t")));
 	}
 
 	public void setup(Context context) {
 		String s = context.getConfiguration().get("mapreduce.pvjob.format_path");
 		jni.init_with_format(s);
-	}   
+	}
 }
 
