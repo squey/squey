72a2309516b8810d5b3780b135273e8e7f905471
Adrien Guinet <aguinet@picviz.com>
2011-08-04 11:20:51 +0200

    Forgot the dumbnet.h file

---
+++ libpvkernel/src/include/pvkernel/core/dumbnet.h
@@ -0,0 +1,39 @@
+/*
+ * This file is from the dumbnet library version 1.12.
+ * License is in net/README.
+ *
+ * Include paths have been modified to match picviz's
+ * source code structure.
+ *
+ * Here is the original header:
+ *
+ * dnet.h
+ *
+ * Copyright (c) 2001 Dug Song <dugsong@monkey.org>
+ *
+ * $Id: dnet.h 529 2004-09-10 03:10:01Z dugsong $
+ */
+
+#ifndef DNET_H
+#define DNET_H
+
+#include <pvkernel/core/dumbnet/os.h>
+
+#include <pvkernel/core/dumbnet/eth.h>
+#include <pvkernel/core/dumbnet/ip.h>
+#include <pvkernel/core/dumbnet/ip6.h>
+#include <pvkernel/core/dumbnet/addr.h>
+#include <pvkernel/core/dumbnet/arp.h>
+#include <pvkernel/core/dumbnet/icmp.h>
+#include <pvkernel/core/dumbnet/tcp.h>
+#include <pvkernel/core/dumbnet/udp.h>
+
+#include <pvkernel/core/dumbnet/intf.h>
+#include <pvkernel/core/dumbnet/route.h>
+#include <pvkernel/core/dumbnet/fw.h>
+#include <pvkernel/core/dumbnet/tun.h>
+
+#include <pvkernel/core/dumbnet/blob.h>
+#include <pvkernel/core/dumbnet/rand.h>
+
+#endif /* DNET_H */
