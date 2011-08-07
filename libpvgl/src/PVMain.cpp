ae9fb46b22eb46aa65b3f8cbd2a8174d00a773bf
Sebastien Tricaud <sebastien@honeynet.org>
2011-08-07 20:17:48 +0200

    Missed an initialization call

--- libpvgl/src/PVMain.cpp
+++ libpvgl/src/PVMain.cpp
@@ -858,13 +858,8 @@ bool pvgl_init(PVGL::PVCom *com)
 								pvgl_com->post_message_to_qt(message);
 								glutTimerFunc(5/*20*/, PVGL::PVMain::timer_func, 0);
 								glutMainLoop();
-								glutInitContextVersion(3, 3);
-								glutInitContextFlags(GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG);
-								glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
-								//  glutInitContextProfile (GLUT_CORE_PROFILE);
-								glutInit(&argc, argv);
-								glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_STENCIL);
-								glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION);
+
+								PVGL::wtk_init(argc, argv);
 							}
 						break;
 				default:
