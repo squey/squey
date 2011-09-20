5bbca6ab68cb78608380f8cff4666d1377adf489
Adrien Guinet <aguinet@picviz.com>
2011-09-20 13:25:15 +0200

    string_log-alpha mapping filter

---
+++ libpicviz/plugins/mapping-filters/string_log-alpha/PVMappingFilterStringLogAlpha.cpp
@@ -0,0 +1,45 @@
+#include "PVMappingFilterStringDefault.h"
+#include <pvkernel/core/PVTBBMaxArray.h>
+#include <pvkernel/core/string.h>
+
+#include <tbb/parallel_reduce.h>
+
+#include <omp.h>
+
+
+float* Picviz::PVMappingFilterStringDefault::operator()(PVRush::PVNraw::nraw_table_line const& values)
+{
+	assert(_dest);
+	assert(values.size() >= _dest_size);
+
+	// First compute all the "string factors"
+	
+	int64_t size = _dest_size;
+	float* factors = (float*) malloc(size * sizeof(float));
+	if (factors == NULL) {
+		PVLOG_ERROR("Unable to allocate a temporary array for string mapping !\n");
+		return _dest;
+	}
+
+#pragma omp parallel for
+	// Looks like this can be fine optimised with hand made SSE/AVX optimisation
+	for (int64_t i = 0; i < size; i++) {
+		factors[i] = PVCore::String::compute_str_factor(values[i]);
+	}
+
+	// Then find the maximum thanks to TBB
+	PVCore::PVTBBMaxArray<float> mar(factors, STRING_MAX_YVAL);
+	tbb::parallel_reduce(tbb::blocked_range<uint64_t>(0, size), mar, tbb::auto_partitioner());
+	float max_factor = mar.get_max_value();
+
+	// This is optimised by the compiler with SSE/AVX
+	for (int64_t i = 0; i < size; i++) {
+		_dest[i] = factors[i] / max_factor;
+	}
+
+	free(factors);
+
+	return _dest;
+}
+
+IMPL_FILTER_NOPARAM(Picviz::PVMappingFilterStringDefault)
---
+++ libpicviz/plugins/mapping-filters/string_log-alpha/PVMappingFilterStringLogAlpha.h
@@ -0,0 +1,22 @@
+#ifndef PVFILTER_PVMAPPINGFILTERSTRINGDEFAULT_H
+#define PVFILTER_PVMAPPINGFILTERSTRINGDEFAULT_H
+
+#include <pvkernel/core/general.h>
+#include <picviz/PVMappingFilter.h>
+#include <tbb/atomic.h>
+
+#include <pvkernel/core/stdint.h>
+
+namespace Picviz {
+
+class PVMappingFilterStringDefault: public PVMappingFilter
+{
+public:
+	float* operator()(PVRush::PVNraw::nraw_table_line const& values);
+
+	CLASS_FILTER(PVMappingFilterStringDefault)
+};
+
+}
+
+#endif
