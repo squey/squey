0f4580b231e51fff4a11099ae91e7296a46dc3c7
Adrien Guinet <aguinet@picviz.com>
2011-11-14 16:36:49 +0100

    PERL interface

---
+++ libpvkernel/plugins/normalize-helpers/perl/doc/syslog.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+my ($LOGFILE) = 0;
+
+sub picviz_open_file {
+    $filename = $_[0];
+    open $main::LOGFILE, '<', $filename or die $!;
+	print "From PERL: open_file\n";
+}
+
+sub picviz_seek_begin {
+	print "From PERL: seek_begin\n";
+	seek($main::LOGFILE,0,0);
+}
+
+sub picviz_pre_discovery {
+	return 1;
+}
+
+sub picviz_is_element_text {
+	return 1;
+}
+
+sub picviz_get_next_chunk {
+	my ($min_chunk_size) = $_[0];
+}
