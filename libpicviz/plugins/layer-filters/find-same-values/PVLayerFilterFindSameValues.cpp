a162ebddf4f59215ce7c9a92812d4d0a0cc046ee
Sebastien Tricaud <sebastien@honeynet.org>
2011-12-18 19:34:25 +0100

    Add a filter to handle same values (maybe it is a duplicate of duplicate...)

--- libpicviz/plugins/layer-filters/find-same-values/PVLayerFilterFindSameValues.cpp
+++ libpicviz/plugins/layer-filters/find-same-values/PVLayerFilterFindSameValues.cpp
@@ -1,4 +1,4 @@
-//! \file PVLayerFilterDiff.cpp
+//! \file PVLayerFilterFindSameValues.cpp
 //! $Id$
 //! Copyright (C) Sébastien Tricaud 2009-2011
 //! Copyright (C) Philippe Saadé 2009-2011
@@ -6,7 +6,7 @@
 
 #include <QSpinBox>
 
-#include "PVLayerFilterDiff.h"
+#include "PVLayerFilterFindSameValues.h"
 
 #include <pvkernel/core/PVColor.h>
 #include <pvkernel/core/PVAxesIndexType.h>
@@ -16,36 +16,34 @@
 
 /******************************************************************************
  *
- * Picviz::PVLayerFilterDiff::PVLayerFilterDiff
+ * Picviz::PVLayerFilterFindSameValues::PVLayerFilterFindSameValues
  *
  *****************************************************************************/
-Picviz::PVLayerFilterDiff::PVLayerFilterDiff(PVCore::PVArgumentList const& l)
+Picviz::PVLayerFilterFindSameValues::PVLayerFilterFindSameValues(PVCore::PVArgumentList const& l)
 	: PVLayerFilter(l)
 {
-	INIT_FILTER(PVLayerFilterDiff, l);
+	INIT_FILTER(PVLayerFilterFindSameValues, l);
 }
 
 /******************************************************************************
  *
- * DEFAULT_ARGS_FILTER(Picviz::PVLayerFilterDiff)
+ * DEFAULT_ARGS_FILTER(Picviz::PVLayerFilterFindSameValues)
  *
  *****************************************************************************/
-DEFAULT_ARGS_FILTER(Picviz::PVLayerFilterDiff)
+DEFAULT_ARGS_FILTER(Picviz::PVLayerFilterFindSameValues)
 {
 	PVCore::PVArgumentList args;
 	args[PVCore::PVArgumentKey("axes", "Axes")].setValue(PVCore::PVAxesIndexType());
-	args["To line"].setValue(PVCore::PVSpinBoxType());
-	args["From line"].setValue(PVCore::PVSpinBoxType());
 
 	return args;
 }
 
 /******************************************************************************
  *
- * Picviz::PVLayerFilterDiff::get_default_args_for_view
+ * Picviz::PVLayerFilterFindSameValues::get_default_args_for_view
  *
  *****************************************************************************/
-PVCore::PVArgumentList Picviz::PVLayerFilterDiff::get_default_args_for_view(PVView const& view)
+PVCore::PVArgumentList Picviz::PVLayerFilterFindSameValues::get_default_args_for_view(PVView const& view)
 {
 	// Retrieve the key axes of the PVFormat of that PVView
 	PVCore::PVArgumentList args = get_args();
@@ -56,10 +54,10 @@ PVCore::PVArgumentList Picviz::PVLayerFilterDiff::get_default_args_for_view(PVVi
 
 /******************************************************************************
  *
- * Picviz::PVLayerFilterDiff::operator()
+ * Picviz::PVLayerFilterFindSameValues::operator()
  *
  *****************************************************************************/
-void Picviz::PVLayerFilterDiff::operator()(PVLayer& in, PVLayer &out)
+void Picviz::PVLayerFilterFindSameValues::operator()(PVLayer& in, PVLayer &out)
 {	
 	PVRush::PVNraw::nraw_table const& nraw = _view->get_qtnraw_parent();
 	PVRow nb_lines = nraw.get_nrows();
@@ -68,7 +66,7 @@ void Picviz::PVLayerFilterDiff::operator()(PVLayer& in, PVLayer &out)
 		_args = get_default_args_for_view(*_view);
 		axes = _args["axes"].value<PVCore::PVAxesIndexType>();
 		if (axes.size() == 0) {
-			PVLOG_ERROR("(PVLayerFilterDiff) no key axes defined in the format and no axes selected !\n");
+			PVLOG_ERROR("(PVLayerFilterFindSameValues) no key axes defined in the format and no axes selected !\n");
 			if (&in != &out) {
 				out = in;
 			}
@@ -76,27 +74,19 @@ void Picviz::PVLayerFilterDiff::operator()(PVLayer& in, PVLayer &out)
 		}
 	}
 
-	PVCore::PVSpinBoxType fromline_spinbox = _args["From line"].value<PVCore::PVSpinBoxType>();
-	PVCore::PVSpinBoxType toline_spinbox = _args["To line"].value<PVCore::PVSpinBoxType>();
-
-	// PVLOG_INFO("************************************* From line:%d\n", fromline_spinbox.get_value());
-	// PVLOG_INFO("************************************* To line:%d\n", toline_spinbox.get_value());
-	// qDebug << "from line= " << fromline_spinbox.value();
-
-	out.get_selection().select_all();
+	out.get_selection().select_none();
 
 	QHash<QString,int> lines_hash;
 	QString key;
 	PVRow counter;
 
 	/* 1st round: we create our hash from the values */
-	for (counter = 0; counter < toline_spinbox.get_value(); counter++) {
+	for (counter = 0; counter < nb_lines; counter++) {
 		PVRush::PVNraw::const_nraw_table_line nrawvalues = nraw.get_row(counter);
-		key = PVRush::PVUtils::generate_key_from_axes_values(axes, nrawvalues);
-
-		lines_hash.insert(key, 1);
-
-		// PVLOG_INFO("Add the key %s\n", qPrintable(key));
+		if (in.get_selection().get_line(counter)) {
+			key = PVRush::PVUtils::generate_key_from_axes_values(axes, nrawvalues);
+			lines_hash.insert(key, 1);
+		}
 	}
 
 
@@ -107,11 +97,11 @@ void Picviz::PVLayerFilterDiff::operator()(PVLayer& in, PVLayer &out)
 		key = PVRush::PVUtils::generate_key_from_axes_values(axes, nrawvalues);
 
 		int value_from_key = lines_hash[key];
-		if (value_from_key == 1) { 
-			out.get_selection().set_line(counter, 0);
+		if (value_from_key > 0) { 
+			out.get_selection().set_line(counter, true);
 		}
 	}
 
 }
 
-IMPL_FILTER(Picviz::PVLayerFilterDiff)
+IMPL_FILTER(Picviz::PVLayerFilterFindSameValues)
