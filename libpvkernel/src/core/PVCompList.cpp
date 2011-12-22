10ba53217da5a54cd10403c8b69c2e8e1641cb12
Adrien Guinet <aguinet@picviz.com>
2011-12-22 09:02:11 +0100

    Forgot file

---
+++ libpvkernel/src/core/PVCompList.cpp
@@ -0,0 +1,37 @@
+#include <pvkernel/core/PVCompList.h>
+#include <pvkernel/core/PVTimeFormatType.h>
+
+namespace PVCore {
+
+template <>
+bool comp_list(QList<PVArgument> const& l1, QList<PVArgument> const& l2)
+{
+	PVLOG_INFO("In comp_list for pvargument\n");
+	if (l1.size() != l2.size()) {
+		return false;
+	}
+
+	QList<QVariant>::const_iterator it1,it2;
+	it1 = l1.begin();
+	it2 = l2.begin();
+
+	for (; it1 != l1.end(); it1++) {
+		QVariant const& v1 = *it1;
+		QVariant const& v2 = *it2;
+		if (v1.canConvert<PVTimeFormatType>() &&
+		    v2.canConvert<PVTimeFormatType>()) {
+			if (v1.value<PVTimeFormatType>() != v2.value<PVTimeFormatType>()) {
+				return false;
+			}
+		}
+		else
+		if (v1 != v2) {
+			return false;
+		}
+		it2++;
+	}
+
+	return true;
+}
+
+}
