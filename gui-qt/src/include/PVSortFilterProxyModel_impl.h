b6dfbf1dea2a5ed1bb73e4c06f0b8458a5af70c2
Adrien Guinet <aguinet@picviz.com>
2012-01-18 08:44:21 +0100

    Missing files !!

---
+++ gui-qt/src/include/PVSortFilterProxyModel_impl.h
@@ -0,0 +1,44 @@
+#ifndef PVINSPECTOR_PVSORTFILTERPROXYMODEL_IMPL_H
+#define PVINSPECTOR_PVSORTFILTERPROXYMODEL_IMPL_H
+
+namespace PVInspector {
+
+namespace __impl {
+
+struct PVSortProxyAsc
+{
+	PVSortProxyAsc(PVSortFilterProxyModel* m_, int col): column(col), m(m_)
+	{
+		ms = m->sourceModel();
+	}
+	bool operator()(int idx1, int idx2) const
+	{
+		return m->less_than(ms->index(idx1, column), ms->index(idx2, column));
+	}
+private:
+	int column;
+	PVSortFilterProxyModel* m;
+	QAbstractItemModel* ms;
+};
+
+struct PVSortProxyDesc
+{
+	PVSortProxyDesc(PVSortFilterProxyModel* m_, int col): column(col), m(m_)
+	{
+		ms = m->sourceModel();
+	}
+	bool operator()(int idx1, int idx2) const
+	{
+		return m->less_than(ms->index(idx2, column), ms->index(idx1, column));
+	}
+private:
+	int column;
+	PVSortFilterProxyModel* m;
+	QAbstractItemModel* ms;
+};
+
+}
+
+}
+
+#endif
---
+++ libpicviz/tests/sizes.cpp
@@ -0,0 +1,12 @@
+#include <picviz/PVIndexArray.h>
+#include <picviz/PVView.h>
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "sizeof(PVIndexArray) = " << sizeof(Picviz::PVIndexArray) << std::endl;
+	std::cout << "sizeof(PVView) = " << sizeof(Picviz::PVView) << std::endl;
+
+	return 0;
+}
