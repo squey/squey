49b51b00e6c2fd50355c5401384a8d41ce42cc52
Philippe Saadé <psaade@picviz.com>
2012-01-18 23:44:46 +0100

    Preparing files for a dedicated widget : PVStartScreenWidget.

---
+++ gui-qt/src/PVStartScreenWidget.cpp
@@ -0,0 +1,25 @@
+//! \file PVStartScreenWidget.cpp
+//! $Id: PVStartScreenWidget.cpp 3196 2011-06-23 16:24:50Z stricaud $
+//! Copyright (C) Sébastien Tricaud 2009-2012
+//! Copyright (C) Philippe Saadé 2009-2012
+//! Copyright (C) Picviz Labs 2012
+
+#include <QtGui>
+
+#include <pvkernel/core/general.h>
+#include <picviz/PVView.h>
+
+#include <PVMainWindow.h>
+#include <PVTabSplitter.h>
+
+#include <PVStartScreenWidget.h>
+
+/******************************************************************************
+ *
+ * PVInspector::PVStartScreenWidget::PVStartScreenWidget
+ *
+ *****************************************************************************/
+PVInspector::PVStartScreenWidget::PVStartScreenWidget(PVMainWindow *mw, PVMainWindow *parent) : QWidget(parent)
+{
+	
+}
---
+++ gui-qt/src/include/PVStartScreenWidget.h
@@ -0,0 +1,47 @@
+//! \file PVStartScreenWidget.h
+//! $Id: PVStartScreenWidget.h 2496 2011-04-25 14:10:00Z psaade $
+//! Copyright (C) Sébastien Tricaud 2009-2012
+//! Copyright (C) Philippe Saadé 2009-2012
+//! Copyright (C) Picviz Labs 2012
+
+#ifndef PVSTARTSCREENWIDGET_H
+#define PVSTARTSCREENWIDGET_H
+
+#include <QAction>
+#include <QWidget>
+
+
+namespace PVInspector {
+class PVMainWindow;
+
+/**
+ *  \class PVStartScreenWidget
+ */
+class PVStartScreenWidget : public QWidget
+{
+	Q_OBJECT
+
+		PVMainWindow     *main_window;            //!<
+
+	public:
+		/**
+		 * Constructor.
+		 */
+		PVStartScreenWidget(PVMainWindow *mw, PVMainWindow *parent);
+
+		/**
+		 *
+		 * @return
+		 */
+
+	public slots:
+//		void show_hide_layer_stack_Slot(bool visible);
+
+	public:
+		//void refresh();
+};
+}
+
+#endif // PVSTARTSCREENWIDGET_H
+
+
