e43b0d50f8669522423ea54b15d22f552d7978ba
amottet <>
2012-01-19 16:57:34 +0100

    and view.h

---
+++ libpvgl/tests/View.h
@@ -0,0 +1,37 @@
+#include <QGLWidget>
+#include "Point.h"
+
+class View : public QGLWidget
+{
+	Q_OBJECT
+
+	public:
+	View(QWidget *parent) : QGLWidget(parent) { buffer_size = 0;}
+	void set_buffer(Point *p, int size);
+	 QSize sizeHint() const { return QSize(1024,1024); };
+	virtual QSize minimumSizeHint() const { return QSize(300,300); }
+
+	private:
+	virtual void initializeGL()
+	{
+		glClearColor(0,0,0,0);
+		glEnable(GL_DEPTH_TEST);
+	}
+
+	virtual void resizeGL(int w, int h)
+	{
+		glViewport(0,0,(GLint)w, (GLint)h);
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0.0,1024,0,1024,0,1000000000);
+
+		glMatrixMode(GL_MODELVIEW);
+	}
+
+
+	void paintGL();
+
+	Point* buffer;
+	int buffer_size;
+};
