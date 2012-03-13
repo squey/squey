f25d577b6090b9b8f194e67ceb12a380f88af17c
Sebastien Tricaud <sebastien@honeynet.org>
2012-03-13 10:29:54 +0100

    Add new layer filter plugin: entropy

---
+++ libpicviz/plugins/layer-filters/entropy/PVLayerFilterEntropy.cpp
@@ -0,0 +1,116 @@
+//! \file PVLayerFilterEntropy.cpp
+//! $Id$
+//! Copyright (C) Sébastien Tricaud 2009-2012
+//! Copyright (C) Philippe Saadé 2009-2012
+//! Copyright (C) Picviz Labs 2011-2012
+
+#include "PVLayerFilterEntropy.h"
+
+#include <pvkernel/rush/PVAxisTagsDec.h>
+#include <pvkernel/core/PVAxisIndexType.h>
+
+#include <picviz/PVView.h>
+
+#include <QDir>
+#include <QList>
+#include <QString>
+
+#include <math.h>
+
+/******************************************************************************
+ *
+ * Picviz::PVLayerFilterEntropy::PVLayerFilterEntropy
+ *
+ *****************************************************************************/
+Picviz::PVLayerFilterEntropy::PVLayerFilterEntropy(PVCore::PVArgumentList const& l)
+	: PVLayerFilter(l), rules_number(0)
+{
+	INIT_FILTER(PVLayerFilterEntropy, l);
+}
+
+/******************************************************************************
+ *
+ * DEFAULT_ARGS_FILTER(Picviz::PVLayerFilterEntropy)
+ *
+ *****************************************************************************/
+DEFAULT_ARGS_FILTER(Picviz::PVLayerFilterEntropy)
+{
+	PVCore::PVArgumentList args;
+
+	args[PVCore::PVArgumentKey("axis", QObject::tr("Axis From"))].setValue(PVCore::PVAxisIndexType(0));
+	args[PVCore::PVArgumentKey("number", QObject::tr("Number"))] = QString("4");
+
+	return args;
+}
+
+float get_entropy(QString str)
+{
+	float entropy = 0;
+
+	if (str.isEmpty()) {
+		return 0;
+	}
+
+	for(int i=0; i < 256; i++) {
+		float occurences = (float) ((float)str.count(QChar(i))) / str.length();
+		if (occurences > 0) {
+			entropy += - occurences*(log2f(occurences));
+		}
+	}
+
+	return entropy;
+// def H(data):
+//     if not data:
+// 	return 0
+
+//     entropy = 0
+//     for x in range(256):
+// 	p_x = float(data.count(chr(x)))/len(data)
+//         if p_x > 0:
+//         	entropy += - p_x*math.log(p_x, 2)
+
+//     return entropy
+
+// ratio = logf(count_frequency) / logf(highest_frequency);
+
+}
+
+/******************************************************************************
+ *
+ * Picviz::PVLayerFilterEntropy::operator()
+ *
+ *****************************************************************************/
+void Picviz::PVLayerFilterEntropy::operator()(PVLayer& in, PVLayer &out)
+{
+	int axis_id = _args["axis"].value<PVCore::PVAxisIndexType>().get_original_index();
+	QString number = _args["number"].toString();
+	float greater_counter = number.toFloat();
+
+	PVRush::PVNraw::nraw_table const& nraw = _view->get_qtnraw_parent();
+	PVRow nb_lines = _view->get_qtnraw_parent().get_nrows();
+
+	out.get_selection().select_none();
+
+	for (PVRow r = 0; r < nb_lines; r++) {
+		if (should_cancel()) {
+			if (&in != &out) {
+				out = in;
+			}
+			return;
+		}
+
+		if (_view->get_line_state_in_pre_filter_layer(r)) {
+			PVRush::PVNraw::const_nraw_table_line nraw_r = nraw.get_row(r);
+			float entr = get_entropy(nraw_r[axis_id].get_qstr());
+			if (entr > greater_counter) {
+				out.get_selection().set_line(r, true);
+			}
+			// PVLOG_INFO("Entropy:%f\n", entr);
+			// bool sel = !((re.indexIn(nraw_r[axis_id].get_qstr()) != -1) ^ include);
+			// out.get_selection().set_line(r, sel);
+		}
+	}
+
+}
+
+IMPL_FILTER(Picviz::PVLayerFilterEntropy)
---
+++ libpicviz/plugins/layer-filters/entropy/register.cpp
@@ -0,0 +1,20 @@
+// Register the plugin in PVFilterLibrary
+//
+
+#include "PVLayerFilterEntropy.h"
+#include <pvkernel/core/PVClassLibrary.h>
+#include <pvkernel/rush/PVAxisTagsDec.h>
+
+
+// This method will be called by libpicviz
+LibCPPExport void register_class()
+{
+	REGISTER_CLASS("Entropy", Picviz::PVLayerFilterEntropy);
+
+	DECLARE_TAG(PVAXIS_TAG_USER_AGENT, PVAXIS_TAG_USER_AGENT_DESC, Picviz::PVLayerFilterEntropy);
+	DECLARE_TAG(PVAXIS_TAG_SRCPORT, PVAXIS_TAG_SRCPORT_DESC, Picviz::PVLayerFilterEntropy);
+	DECLARE_TAG(PVAXIS_TAG_DSTPORT, PVAXIS_TAG_DSTPORT_DESC, Picviz::PVLayerFilterEntropy);
+	DECLARE_TAG(PVAXIS_TAG_SRCIP, PVAXIS_TAG_SRCIP_DESC, Picviz::PVLayerFilterEntropy);
+	DECLARE_TAG(PVAXIS_TAG_DSTIP, PVAXIS_TAG_DSTIP_DESC, Picviz::PVLayerFilterEntropy);
+
+}
