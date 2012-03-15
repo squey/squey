d7b567df8e3c165ed2bd8d3026270a55e33c2678
Sebastien Tricaud <sebastien@honeynet.org>
2012-03-15 10:07:23 +0100

    Remove useless files

--- libpicviz/tests.old/RelWithDebInfo/wintest.bat
+++
@@ -1,8 +0,0 @@
-SET PICVIZ_PARSERS_DIR=..\..\plugins\parsers\
-SET PICVIZ_NORMALIZE_DIR=..\..\plugins\normalize\Debug\
-SET PICVIZ_FUNCTIONS_DIR=..\..\plugins\functions\Debug\
-SET PICVIZ_FILTERS_DIR=..\..\plugins\filters\Debug\
-SET PICVIZ_DEBUG_LEVEL=DEBUG
-
-%1
-
--- libpicviz/tests.old/devel/run.sh
+++
@@ -1,8 +0,0 @@
-if [ "$1" == "debug" ]
-then
-    PICVIZ_PARSERS_DIR=../plugins/parsers PYTHONPATH=$PYTHONPATH:.:../srcpy/ LD_LIBRARY_PATH=../src/ gdb ./$2
-    exit 0
-fi
-echo "Running..."
-PICVIZ_PARSERS_DIR=../plugins/parsers PYTHONPATH=$PYTHONPATH:.:../srcpy/ LD_LIBRARY_PATH=../src/ ./$1 $2
-echo "[DONE]"
