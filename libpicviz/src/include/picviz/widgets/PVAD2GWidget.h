ceb642161488be983cde27f34e3ef8b9aff87876
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-04-05 11:00:23 +0200

    More work on AD2G Widget.

--- libpicviz/src/include/picviz/widgets/PVAD2GWidget.h
+++ libpicviz/src/include/picviz/widgets/PVAD2GWidget.h
@@ -3,7 +3,10 @@
 
 #include <QtGui>
 
+#include <tulip/InteractorComponent.h>
+#include "tulip/InteractorChainOfResponsibility.h"
 #include <tulip/NodeLinkDiagramComponent.h>
+#include <tulip/Observable.h>
 #include <tulip/TlpQtTools.h>
 
 #include <pvkernel/core/general.h>
@@ -11,16 +14,85 @@
 
 namespace Picviz {
 
-class /*LibPicvizExport*/ PVAD2GWidget : public QWidget
+class AD2GNodeLinkDiagramComponent : public tlp::NodeLinkDiagramComponent
+{
+public:
+	// Disable view standard context menu
+	virtual void buildContextMenu(QObject* object, QContextMenuEvent* event, QMenu* contextMenu){};
+	virtual void computeContextMenuAction(QAction *action){};
+};
+
+class /*LibPicvizExport*/ PVAD2GWidget : public QWidget, public tlp::Observable
 {
 	Q_OBJECT;
+
+	typedef std::set<tlp::Observable*>::iterator ObserverIterator;
+
 public:
-	PVAD2GWidget(PVAD2GView& ad2g, QWidget* parent = NULL);
+	PVAD2GWidget(PVAD2GView& ad2g, QMainWindow* mw = NULL);
 	~PVAD2GWidget();
+
+	QWidget* get_widget();
+
+public slots:
+	void change_interactor_slot();
+	void update(ObserverIterator begin, ObserverIterator end);
+	void observableDestroyed(tlp::Observable* observable) {}
+
+private:
+	void init_toolbar();
+	void initObservers();
+	void clearObservers();
 
 private:
 	PVAD2GView& _ad2g;
-	tlp::NodeLinkDiagramComponent* _nodeLinkView;
+	AD2GNodeLinkDiagramComponent* _nodeLinkView;
+	QWidget* _widget;
+	QMainWindow* _mw;
+	tlp::Graph* _graph;
+	QToolBar* _toolBar;
+};
+
+/**
+ * \class AD2GInteractorComponent
+ */
+class AD2GInteractorComponent : public tlp::InteractorComponent
+{
+	Q_OBJECT
+
+public:
+	AD2GInteractorComponent(PVAD2GWidget* widget, Qt::MouseButton button = Qt::LeftButton, Qt::KeyboardModifier modifier = Qt::NoModifier):_widget(widget){}
+	~AD2GInteractorComponent() {}
+	bool eventFilter(QObject* widget, QEvent* e);
+	tlp::InteractorComponent* clone() { return new AD2GInteractorComponent(_widget, mButton, kModifier); }
+
+protected:
+	Qt::MouseButton mButton;
+	Qt::KeyboardModifier kModifier;
+	Qt::KeyboardModifiers mousePressModifier;
+	PVAD2GWidget* _widget;
+};
+
+/**
+ * \class AD2GInteractor
+ */
+class AD2GInteractor : public tlp::InteractorChainOfResponsibility
+{
+	Q_OBJECT
+
+public:
+	AD2GInteractor(PVAD2GWidget* widget):
+		tlp::InteractorChainOfResponsibility("", "AD2GInteractor"),
+		_widget(widget)
+	{
+		_component1 = new AD2GInteractorComponent(_widget);
+		pushInteractorComponent(_component1);
+	}
+	virtual bool isCompatible(const std::string &viewName) {return false;}
+
+private:
+	AD2GInteractorComponent* _component1;
+	PVAD2GWidget* _widget;
 };
 
 }
--- libpicviz/src/widgets/PVAD2GWidget.cpp
+++ libpicviz/src/widgets/PVAD2GWidget.cpp
@@ -1,19 +1,181 @@
 #include <picviz/widgets/PVAD2GWidget.h>
 
-Picviz::PVAD2GWidget::PVAD2GWidget(PVAD2GView& ad2g, QWidget* parent /*= NULL*/) :
-	_ad2g(ad2g)
+#include <tulip/Interactor.h>
+#include <tulip/InteractorManager.h>
+
+Picviz::PVAD2GWidget::PVAD2GWidget(PVAD2GView& ad2g, QMainWindow* mw /*= NULL*/) :
+	QWidget(mw),
+	_ad2g(ad2g),
+	_mw(mw),
+	_graph(_ad2g.get_graph())
 {
-	_nodeLinkView = new tlp::NodeLinkDiagramComponent();
+	_nodeLinkView = new AD2GNodeLinkDiagramComponent();
+
+	_widget = _nodeLinkView->construct(this);
+	_nodeLinkView->hideOverview(true);
 
 	tlp::DataSet dataSet;
+	const QString& str = "tulip_graph.tlp";
+	dataSet.set("file::filename", str);
 	dataSet.set("arrow", true);
 
-	PVLOG_INFO("glMainWidget=%x\n", _nodeLinkView->getGlMainWidget());
-	openGraphOnGlMainWidget(_ad2g.get_graph(), &dataSet, _nodeLinkView->getGlMainWidget());
+	if (_graph) {
+		openGraphOnGlMainWidget(_graph, &dataSet, _nodeLinkView->getGlMainWidget());
+		initObservers();
+	}
+
+	_nodeLinkView->init();
+	init_toolbar();
 }
 
 Picviz::PVAD2GWidget::~PVAD2GWidget()
 {
+	clearObservers();
 	delete _nodeLinkView;
 }
 
+QWidget* Picviz::PVAD2GWidget::get_widget()
+{
+	return _widget;
+}
+
+void Picviz::PVAD2GWidget::init_toolbar()
+{
+	//std::list<std::string> interactorsNamesAndPriorityMap(tlp::InteractorManager::getInst().getSortedCompatibleInteractors("Node Link Diagram view"));
+
+	std::list<tlp::Interactor *> interactorsList;
+	//for (std::list<std::string>::reverse_iterator it = interactorsNamesAndPriorityMap.rbegin(); it != interactorsNamesAndPriorityMap.rend(); ++it) {
+	//	interactorsList.push_back(tlp::InteractorManager::getInst().getInteractor((*it)));
+	//}
+	AD2GInteractor* ad2g_interactor = new AD2GInteractor(this);
+	interactorsList.push_back(ad2g_interactor);
+
+	_nodeLinkView->setInteractors(interactorsList);
+	_nodeLinkView->setActiveInteractor(ad2g_interactor);
+
+//	_toolBar = new QToolBar(this);
+//	std::list<QAction *> interactorsActionList;
+//	for (std::list<tlp::Interactor *>::iterator it = interactorsList.begin(); it != interactorsList.end(); ++it)
+//		interactorsActionList.push_back((*it)->getAction());
+//	for (std::list<QAction *>::iterator it = interactorsActionList.begin(); it != interactorsActionList.end(); ++it) {
+//		_toolBar->addAction(*it);
+//		connect(*it, SIGNAL(triggered()), this, SLOT(change_interactor_slot()));
+//	}
+//
+//	if (_mw) {
+//		_mw->addToolBar(_toolBar);
+//	}
+}
+
+void Picviz::PVAD2GWidget::change_interactor_slot()
+{
+	QAction *action = (QAction*) sender();
+	QList<QAction*> actions = _toolBar->actions();
+
+	for (QList<QAction*>::iterator it = actions.begin(); it != actions.end(); ++it) {
+		(*it)->setChecked(false);
+	}
+
+	action->setCheckable(true);
+	action->setChecked(true);
+
+	tlp::InteractorAction *interactorAction = (tlp::InteractorAction *) action;
+	_nodeLinkView->setActiveInteractor(interactorAction->getInteractor());
+}
+
+void Picviz::PVAD2GWidget::initObservers()
+{
+	if (_graph) {
+		_graph->addObserver(this);
+		tlp::Iterator<tlp::PropertyInterface*> *it = _graph->getObjectProperties();
+
+		while (it->hasNext()) {
+			tlp::PropertyInterface* tmp = it->next();
+			tmp->addObserver(this);
+		}
+
+		delete it;
+	}
+}
+
+void Picviz::PVAD2GWidget::clearObservers()
+{
+	if (_graph) {
+		_graph->removeObserver(this);
+		tlp::Iterator<tlp::PropertyInterface*> *it = _graph->getObjectProperties();
+
+		while (it->hasNext()) {
+			(it->next())->removeObserver(this);
+		}
+
+		delete it;
+	}
+}
+
+void Picviz::PVAD2GWidget::update(ObserverIterator begin, ObserverIterator end)
+{
+	_nodeLinkView->draw();
+}
+
+bool Picviz::AD2GInteractorComponent::eventFilter(QObject* widget, QEvent* e)
+{
+
+	if (e->type() == QEvent::MouseButtonPress) {
+		PVLOG_INFO("Picviz::AD2GInteractorComponent::eventFilter e->type()=%d\n", e->type());
+		QMouseEvent* qMouseEv = (QMouseEvent *) e;
+		tlp::GlMainWidget* glMainWidget = (tlp::GlMainWidget *) widget;
+
+		if (qMouseEv->button()== Qt::RightButton) {
+			// Enter here if we have released the left button of the mouse
+
+			// doSelect function return node/edge under the mouse
+			tlp::node tmpNode;
+			tlp::edge tmpEdge;
+			tlp::ElementType type;
+			bool result = glMainWidget->doSelect(qMouseEv->x(), qMouseEv->y(), type, tmpNode, tmpEdge);
+
+			if (result) {
+				// Enter here if we have node/edge under the mouse
+
+				// Store selection property
+				tlp::Graph *graph=glMainWidget->getScene()->getGlGraphComposite()->getInputData()->getGraph();
+				std::string selectionPropertyName=glMainWidget->getScene()->getGlGraphComposite()->getInputData()->getElementSelectedPropName();
+				tlp::BooleanProperty* selection=graph->getProperty<tlp::BooleanProperty>(selectionPropertyName);
+
+				// Before do anything on the graph, we push the current state of the graph (this activate the undo/redo system)
+				graph->push();
+
+				// Deselect all nodes/edges
+				selection->setAllNodeValue(false);
+				selection->setAllEdgeValue(false);
+
+				switch(type) {
+					case tlp::NODE:
+					// Set selection at true for selected node
+					selection->setNodeValue(tmpNode, true);
+					break;
+
+					case tlp::EDGE:
+					// Set selection at false for selected edge
+					selection->setEdgeValue(tmpEdge, true);
+					break;
+				}
+
+				//QMessageBox box("Selection", type==tlp::NODE?QString().sprintf("You clicked on Node #%d", tmpNode.id):QString().sprintf("You clicked on Edge  #%d", tmpEdge.id), QMessageBox::Information, QMessageBox::Ok, QMessageBox::NoButton, QMessageBox::NoButton, (tlp::GlMainWidget *) widget);
+				//box.exec();
+				QMenu* menu = new QMenu;
+				QAction* my_action = new QAction(tr("Add view"), menu);
+				menu->addAction(my_action);
+				menu->exec(qMouseEv->globalPos());
+
+				// We have treated the event so we return true
+				// (this event will not be passed to others interactorComponent)
+				return true;
+			}
+		}
+	}
+
+   // We don't have treated the event so we return false
+   // (this event will be passed to others interactorComponent)
+   return false;
+}
