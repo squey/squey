abfc3e4df5e828e7efe85a48fd172bb7a90f5b06
Rémi Hérilier <rherilier@picviz.com>
2012-04-16 15:27:29 +0200

    no excuses...
    * It is better to store the row index for each IPs to retrieve
      the *right* entries...
    * the origin IPs set stores only the selection to avoid random
      values in the set...
    * some corrected comments

--- libpicviz/src/PVTFViewRowFilterFastIPv4.cpp
+++ libpicviz/src/PVTFViewRowFilterFastIPv4.cpp
@@ -7,21 +7,31 @@
 #include <pvkernel/core/network.h>
 #include <tbb/parallel_sort.h>
 
+typedef struct {
+	uint32_t ip;
+	PVRow row;
+} IPStruct;
+
+bool IPStruct_lesser(const IPStruct &a, const IPStruct &b)
+{
+	return a.ip < b.ip;
+}
+
 Picviz::PVSelection Picviz::PVTFViewRowFilterFastIPv4::operator()(PVView const& view_src, PVView const& view_dst, PVSelection const& sel_org) const
 {
-	/* RH: This version is really fast (full selection in 500 ms for 20M lines)
+	/* RH: This version is really fast (full selection in 890 ms for 2M lines)
 	 * but two big limitations:
 	 * - uses mapping space values, not nraw's ones
 	 * - no PVSelRowFilteringFunction
 	 * So:
 	 * 1/ can it be modified to work with nraw values?
-	 * 2/ can the merge can use N entries?
+	 * 2/ can the merge use N entries instead of just 2?
 	 */
 	PVSelection sel_ret;
 	PVRow nlines_src = view_src.get_row_count();
-	uint32_t ip;
-	std::vector<uint32_t> ips_src;
-	std::vector<uint32_t> ips_dst;
+	std::vector<IPStruct> ips_src;
+	std::vector<IPStruct> ips_dst;
+	IPStruct p;
 	PVRow count_src = 0;
 	PVRow count_dst = view_dst.get_row_count();
 	PVRow index_src, index_dst;
@@ -32,20 +42,21 @@ Picviz::PVSelection Picviz::PVTFViewRowFilterFastIPv4::operator()(PVView const& 
 
 	BENCH_START(convert_src);
 	// extraction of src selected rows
-	ips_src.resize(view_src.get_row_count());
-#pragma omp parallel for
+	ips_src.reserve(view_src.get_row_count());
 	for(PVRow r = 0; r < nlines_src; ++r) {
 		if (!sel_org.get_line(r))
 			continue;
 
-		PVCore::Network::ipv4_aton(view_src.get_data_raw(r, 1), ip);
-		ips_src[r] = ip;
+		PVCore::Network::ipv4_aton(view_src.get_data_raw(r, 1), p.ip);
+		p.row = r;
+		ips_src.push_back(p);
 	}
 	count_src = ips_src.size();
         BENCH_END(convert_src, "convert_src", 1, 1, sizeof(uint32_t), ips_src.size());
 
 	BENCH_START(sort_src);
-	tbb::parallel_sort(ips_src.begin(), ips_src.end(), std::less<uint32_t>());
+	// TODO: try sorting with GPU
+	tbb::parallel_sort(ips_src.begin(), ips_src.end(), IPStruct_lesser);
         BENCH_END(sort_src, "sort_src", 1, 1, 1, 1);
 
         BENCH_END(preprocess_src, "preprocess_src", 1, 1, 1, 1);
@@ -57,13 +68,13 @@ Picviz::PVSelection Picviz::PVTFViewRowFilterFastIPv4::operator()(PVView const& 
 	ips_dst.resize(count_dst);
 #pragma omp parallel for
 	for(PVRow r = 0; r < count_dst; ++r) {
-		PVCore::Network::ipv4_aton(view_dst.get_data_raw(r, 1), ip);
-		ips_dst[r] = ip;
+		PVCore::Network::ipv4_aton(view_dst.get_data_raw(r, 1), ips_dst[r].ip);
+		ips_dst[r].row = r;
 	}
 	BENCH_END(convert_dst, "convert_dst", 1, 1, sizeof(uint32_t), ips_dst.size());
 
 	BENCH_START(sort_dst);
-        tbb::parallel_sort(ips_dst.begin(), ips_dst.end(), std::less<uint32_t>());
+        tbb::parallel_sort(ips_dst.begin(), ips_dst.end(), IPStruct_lesser);
         BENCH_END(sort_dst, "sort_dst", 1, 1, 1, 1);
 
         BENCH_END(preprocess_dst, "preprocess_dst", 1, 1, 1, 1);
@@ -76,17 +87,17 @@ Picviz::PVSelection Picviz::PVTFViewRowFilterFastIPv4::operator()(PVView const& 
 	sel_ret.select_none();
 
 	while((index_src < count_src) && (index_dst < count_dst)) {
-		if(ips_src[index_src] < ips_dst[index_dst]) {
+		if(ips_src[index_src].ip < ips_dst[index_dst].ip) {
 			++index_src;
-		} else if(ips_src[index_src] > ips_dst[index_dst]) {
+		} else if(ips_src[index_src].ip > ips_dst[index_dst].ip) {
 			++index_dst;
 		} else {
-			uint32_t &value = ips_src[index_src];
-			while ((value == ips_dst[index_dst]) && (index_dst < count_dst)) {
-				sel_ret.set_line(index_dst, true);
+			uint32_t &value = ips_src[index_src].ip;
+			while ((value == ips_dst[index_dst].ip) && (index_dst < count_dst)) {
+				sel_ret.set_line(ips_dst[index_dst].row, true);
 				++index_dst;
 			}
-			while ((value == ips_src[index_src]) && (index_src < count_src)) {
+			while ((value == ips_src[index_src].ip) && (index_src < count_src)) {
 				++index_src;
 			}
 		}
