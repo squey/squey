55a8da3321df22fc581816126ebe7e72500084e3
Adrien Guinet <aguinet@picviz.com>
2012-04-18 17:14:55 +0200

    Virtual pure function for some of the RFF interface

--- libpicviz/src/include/picviz/PVSelRowFilteringFunction.h
+++ libpicviz/src/include/picviz/PVSelRowFilteringFunction.h
@@ -21,6 +21,7 @@ public:
 	virtual void pre_process(PVView const& view_org, PVView const& view_dst) = 0;
 	virtual void operator()(PVRow row_org, PVView const& view_org, PVView const& view_dst, PVSelection& sel_dst) const = 0;
 	virtual QString get_human_name() const { return registered_name(); }
+	virtual QString get_human_name_with_args() const { return get_human_name(); }
 	
 public:
 	inline PVCore::PVArgumentList get_args_for_org_view() const { return get_args_for_view(get_arg_keys_for_org_view()); }
@@ -30,8 +31,8 @@ public:
 	inline void set_args_for_dst_view(PVCore::PVArgumentList const& v_args) { set_args_for_view(v_args, get_arg_keys_for_dst_view()); }
 
 protected:
-	virtual PVCore::PVArgumentKeyList get_arg_keys_for_org_view() const { return get_default_args().keys(); };
-	virtual PVCore::PVArgumentKeyList get_arg_keys_for_dst_view() const { return get_default_args().keys(); };
+	virtual PVCore::PVArgumentKeyList get_arg_keys_for_org_view() const = 0;
+	virtual PVCore::PVArgumentKeyList get_arg_keys_for_dst_view() const = 0;
 
 private:
 	inline PVCore::PVArgumentList get_args_for_view(PVCore::PVArgumentKeyList const& keys) const
