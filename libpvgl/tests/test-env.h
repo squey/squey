c00461fd0a7e86f4989ee191dea295c92d53d5ef
Adrien Guinet <aguinet@picviz.com>
2012-04-18 10:14:31 +0200

    Fix test-env of PVGL

--- libpvgl/tests/test-env.h
+++ libpvgl/tests/test-env.h
@@ -12,11 +12,10 @@ void init_env()
 	pv_setenv("PVRUSH_NORMALIZE_DIR","../../libpvkernel/plugins/normalize",0);
 	pv_setenv("PVFILTER_NORMALIZE_DIR","../../libpvkernel/plugins/normalize",0);
 	pv_setenv("PVRUSH_NORMALIZE_HELPERS_DIR","../../libpvkernel/plugins/normalize-helpers:./test-formats",0);
-	//pv_setenv("PICVIZ_DEBUG_LEVEL","DEBUG",0);
 	pv_setenv("PICVIZ_CACHE_DIR","./cache",0);
 	pv_setenv("PVRUSH_INPUTTYPE_DIR","../../libpvkernel/plugins/input_types",0);
 	pv_setenv("PVRUSH_SOURCE_DIR","../../libpvkernel/plugins/sources",0);
 	pv_setenv("PICVIZ_MAPPING_FILTERS_DIR","../../libpicviz/plugins/mapping-filters",0);
 	pv_setenv("PICVIZ_PLOTTING_FILTERS_DIR","../../libpicviz/plugins/plotting-filters",0);
-	pv_setenv("PVGL_SHARE_DIR","../../libpvgl/data/",0);
+	pv_setenv("PICVIZ_ROW_FILTERS_DIR","../../libpicviz/plugins/row-filters",0);
 }
