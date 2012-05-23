3fc02649a341a632b9c1b052a102235f2e79331a
Adrien Guinet <aguinet@picviz.com>
2012-05-23 10:09:43 +0200

    Missing files..

---
+++ libparallelview/src/include/pvparallelview/PVZone.h
@@ -0,0 +1,43 @@
+#ifndef PVPARALLELVIEW_PVZONE_H
+#define PVPARALLELVIEW_PVZONE_H
+
+#include <pvparallelview/PVZoneTree.h>
+
+namespace PVParallelView {
+
+class PVZone
+{
+public:
+	PVZone():
+		_ztree(new PVZoneTree()),
+		_width(256)
+	{ }
+
+public:
+	inline void set_width(uint32_t width) { _width = width; }
+	uint32_t width() const { return _width; }
+
+	PVZoneTree& ztree() { return *_ztree; }
+	PVZoneTree const& ztree() const { return *_ztree; }
+
+	template <class Tree>
+	Tree const& get_tree() const
+	{
+		assert(false);
+		return Tree();
+	}
+
+private:
+	PVZoneTree_p _ztree;
+	uint32_t _width;
+};
+
+template <>
+inline PVZoneTree const& PVZone::get_tree<PVZoneTree>() const
+{
+	return *_ztree;
+}
+
+}
+
+#endif
