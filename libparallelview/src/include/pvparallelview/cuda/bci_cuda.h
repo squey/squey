8a931212edd1b808ab89066b404eaa2007762ffc
Adrien Guinet <aguinet@picviz.com>
2012-05-23 10:08:34 +0200

    Still missing a file

---
+++ libparallelview/src/include/pvparallelview/cuda/bci_cuda.h
@@ -0,0 +1,12 @@
+#ifndef PVPARALLELVIEW_BCICUDA_H
+#define PVPARALLELVIEW_BCICUDA_H
+
+// for GLuint
+#include <GL/gl.h>
+
+#include <pvparallelview/PVBCICode.h>
+
+void show_codes_cuda(PVParallelView::PVBCICode* device_codes, uint32_t n, uint32_t width, uint32_t* device_img, uint32_t img_width, uint32_t x_start, cudaStream_t stream = NULL);
+void show_codes_cuda(PVParallelView::PVBCICode* codes, uint32_t n, uint32_t width, GLuint buffer_id);
+
+#endif
