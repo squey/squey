d7035a8652a371f94d8a1a598e4a019e225566f2
Adrien Guinet <aguinet@picviz.com>
2012-05-23 10:00:34 +0200

    Add missing files !

---
+++ libparallelview/src/PVZonesDrawing.cpp
@@ -0,0 +1,20 @@
+#include <pvparallelview/PVBCIDrawingBackend.h>
+#include <pvparallelview/PVZonesDrawing.h>
+
+PVParallelView::PVZonesDrawing::PVZonesDrawing(PVZonesManager const& zm, PVBCIDrawingBackend const& backend, PVHSVColor const& colors):
+	_zm(zm),
+	_draw_backend(&backend),
+	_colors(&colors)
+{
+	_computed_codes = PVBCICode::allocate_codes(NBUCKETS);
+}
+
+PVParallelView::PVZonesDrawing::~PVZonesDrawing()
+{
+	PVBCICode::free_codes(_computed_codes);
+}
+
+void PVParallelView::PVZonesDrawing::draw_bci(PVBCIBackendImage& dst_img, uint32_t x_start, PVZoneID zone, PVBCICode* codes, size_t n)
+{
+	_draw_backend->operator()(dst_img, x_start, _zm.get_zone_width(zone), codes, n);
+}
---
+++ libparallelview/tests/lines_drawing.cpp
@@ -0,0 +1,113 @@
+#include <pvkernel/core/picviz_bench.h>
+#include <picviz/PVPlotted.h>
+#include <pvparallelview/PVBCICode.h>
+#include <pvparallelview/PVBCIBackendImage.h>
+#include <pvparallelview/PVBCIDrawingBackendCUDA.h>
+#include <pvparallelview/PVHSVColor.h>
+#include <pvparallelview/PVHSVColor.h>
+#include <pvparallelview/PVTools.h>
+#include <pvparallelview/PVZonesDrawing.h>
+#include <pvparallelview/PVZonesManager.h>
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include <QApplication>
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QString>
+
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
+
+void usage(const char* path)
+{
+	std::cerr << "Usage: " << path << " [plotted_file] [nrows] [ncols]" << std::endl;
+}
+
+void init_rand_plotted(Picviz::PVPlotted::plotted_table_t& p, PVRow nrows, PVCol ncols)
+{
+	srand(time(NULL));
+	p.clear();
+	p.reserve(nrows*ncols);
+	for (PVRow i = 0; i < nrows*ncols; i++) {
+		p.push_back((float)((double)(rand())/(double)RAND_MAX));
+	}
+}
+
+void show_qimage(QString const& title, QImage const& img)
+{
+	QDialog* dlg = new QDialog();
+	dlg->setWindowTitle(title);
+	QVBoxLayout* layout = new QVBoxLayout();
+	QLabel* limg = new QLabel();
+	limg->setPixmap(QPixmap::fromImage(img));
+	layout->addWidget(limg);
+	dlg->setLayout(layout);
+	dlg->show();
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	QApplication app(argc, argv);
+
+	PVCol ncols, nrows;
+	Picviz::PVPlotted::plotted_table_t plotted;
+	QString fplotted(argv[1]);
+	if (fplotted == "0") {
+		if (argc < 4) {
+			usage(argv[0]);
+			return 1;
+		}
+		srand(time(NULL));
+		nrows = atol(argv[2]);
+		ncols = atol(argv[3]);
+
+		init_rand_plotted(plotted, nrows, ncols);
+	}
+	else
+	{
+		if (!Picviz::PVPlotted::load_buffer_from_file(plotted, ncols, true, QString(argv[1]))) {
+			std::cerr << "Unable to load plotted !" << std::endl;
+			return 1;
+		}
+		nrows = plotted.size()/ncols;
+	}
+
+	PVParallelView::PVHSVColor* colors = PVParallelView::PVHSVColor::init_colors(nrows);
+
+	Picviz::PVPlotted::uint_plotted_table_t norm_plotted;
+	Picviz::PVPlotted::norm_int_plotted(plotted, norm_plotted, ncols);
+
+	PVParallelView::PVZonesManager &zm = *(new PVParallelView::PVZonesManager());
+	zm.set_uint_plotted(norm_plotted, nrows, ncols);
+	zm.update_all();
+
+	PVParallelView::PVBCIDrawingBackendCUDA backend_cuda;
+	PVParallelView::PVZonesDrawing &zones_drawing = *(new PVParallelView::PVZonesDrawing(zm, backend_cuda, *colors));
+
+	PVParallelView::PVBCIBackendImage_p dst_img = zones_drawing.create_image(768);
+	zones_drawing.draw_zone<PVParallelView::PVZoneTree>(*dst_img, 0, 0, &PVParallelView::PVZoneTree::browse_tree_bci);
+	printf("%u\n", zm.get_zone_absolute_pos(1));
+	zones_drawing.draw_zone<PVParallelView::PVZoneTree>(*dst_img, zm.get_zone_absolute_pos(1), 1, &PVParallelView::PVZoneTree::browse_tree_bci);
+
+	show_qimage("test", dst_img->qimage());
+
+	dst_img->set_width(256);
+	zones_drawing.draw_zone<PVParallelView::PVZoneTree>(*dst_img, 0, 0, &PVParallelView::PVZoneTree::browse_tree_bci);
+
+	show_qimage("test2", dst_img->qimage());
+
+	app.exec();
+
+
+	return 0;
+}
