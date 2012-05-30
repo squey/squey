a3ebf3b79c72174cb7666c3a032ab9dfe83a3526
Adrien Guinet <aguinet@picviz.com>
2012-05-30 19:50:04 +0200

    Forgotten file.... !!!!!

---
+++ libpicviz/src/include/picviz/PVAD2GView_types.h
@@ -0,0 +1,14 @@
+#ifndef PICVIZ_PVAD2GVIEWTYPES_H
+#define PICVIZ_PVAD2GVIEWTYPES_H
+
+#include <boost/shared_ptr.hpp>
+
+namespace Picviz {
+
+class PVAD2GView;
+typedef boost::shared_ptr<PVAD2GView> PVAD2GView_p;
+
+}
+
+
+#endif
