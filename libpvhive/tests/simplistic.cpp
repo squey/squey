ce6c1b8b29a69a2fb2c5c1e0b2a7f0a88a86d4f7
Rémi Hérilier <rherilier@picviz.com>
2012-06-20 18:19:35 +0200

    [PVHive] test simplistic updated

--- libpvhive/tests/simplistic.cpp
+++ libpvhive/tests/simplistic.cpp
@@ -7,38 +7,64 @@
 #include <pvhive/PVActor.h>
 #include <pvhive/PVObserver.h>
 
-class Ci
+
+struct Obj1
 {
-public:
-	Ci(int i) : _i(i) {}
+	~Obj1()
+	{
+		std::cout << "  Obj1::~Obj1 for object " << this << std::endl;
+	}
 
-	void set_i(int i) { _i = i; }
-
-	int get_i() const { return _i; }
-
-private:
-	int _i;
+	void print()
+	{
+		std::cout << "    Obj1::print" << std::endl;
+	}
 };
 
-typedef PVHive::PVActor<Ci> CiActor;
+struct Obj2
+{
+	~Obj2()
+	{
+		std::cout << "  Obj2::~Obj2 for object " << this << std::endl;
+	}
 
-class CiObs : public PVHive::PVObserver<Ci>
+	void print()
+	{
+		std::cout << "    Obj2::print" << std::endl;
+	}
+};
+
+class Obj1Observer : public PVHive::PVObserver<Obj1>
 {
 public:
-	CiObs()
-	{}
-
 	virtual void refresh()
 	{
-		std::cout << "Obs::refresh(): " << get_object()->get_i() << std::endl;
+		std::cout << "    Obj1Observer::refresh for object " << _object << std::endl;
 	}
 
 	virtual void about_to_be_deleted()
 	{
-		std::cout << "Obs::about_to_be_deleted()" << std::endl;
+		std::cout << "    Obj1Observer::about_to_be_deleted for object " << _object << std::endl;
 	}
+
+private:
 };
 
+class Obj2Observer : public PVHive::PVObserver<Obj2>
+{
+public:
+	virtual void refresh()
+	{
+		std::cout << "    Obj2Observer::refresh for object " << _object << std::endl;
+	}
+
+	virtual void about_to_be_deleted()
+	{
+		std::cout << "    Obj2Observer::about_to_be_deleted for object " << _object << std::endl;
+	}
+
+private:
+};
 
 int main(int argc, char **argv)
 {
@@ -47,29 +73,43 @@ int main(int argc, char **argv)
 	PVHive::PVHive &h1 = PVHive::PVHive::get();
 	PVHive::PVHive &h2 = PVHive::PVHive::get();
 
-
 	if (&h1 != &h2) {
 		std::cerr << "PVHive::get() returns different addresses" << std::endl;
 		return 1;
 	}
 
-	std::cout << "PVHive::get() works" << std::endl;
+	std::cout << "scene creation" << std::endl;
 
-	Ci i(0);
-	CiActor a;
+	std::cout << "  hive " << &h1 << std::endl;
 
-	PVHive::PVHive::get().register_actor(i, a);
+	Obj1 *o1 = new Obj1;
+	PVHive::PVActor<Obj1> a1o1;
+	PVHive::PVHive::get().register_actor(*o1, a1o1);
 
-	CiObs o;
+	Obj2 *o2 = new Obj2;
+	PVHive::PVActor<Obj2> a1o2;
+	PVHive::PVHive::get().register_actor(*o2, a1o2);
 
-	PVHive::PVHive::get().register_observer(i, o);
+	Obj1Observer o1o1;
+	PVHive::PVHive::get().register_observer(*o1, o1o1);
 
-	a.call<decltype(&Ci::set_i), &Ci::set_i>(24);
+	Obj2Observer o1o2;
+	PVHive::PVHive::get().register_observer(*o2, o1o2);
 
-	PVACTOR_CALL(a, &Ci::set_i, 42);
+	Obj2Observer o2o2;
+	PVHive::PVHive::get().register_observer(*o2, o2o2);
 
-	PVHive::PVHive::get().unregister_observer(o);
-	PVHive::PVHive::get().unregister_actor(a);
+
+	PVACTOR_CALL(a1o1, &Obj1::print);
+	PVACTOR_CALL(a1o2, &Obj2::print);
+
+	std::cout << "unregister a1o1" << std::endl;
+	PVHive::PVHive::get().unregister_actor(a1o1);
+
+	std::cout << "unregister o1o2" << std::endl;
+	PVHive::PVHive::get().unregister_observer(o2o2);
+
+	std::cout << "end" << std::endl;
 
 	return 0;
 }
