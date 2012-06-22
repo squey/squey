07b8a2d78ecbd9b830147d9c3d8c674ad72f9de1
Rémi Hérilier <rherilier@picviz.com>
2012-06-22 16:50:14 +0200

    [PVKernel] Add type pv_shared_ptr
    It misses atomic code for multi-threaded accesses.

---
+++ libpvkernel/src/include/pvkernel/core/PVSharedPointer.h
@@ -0,0 +1,229 @@
+
+#ifndef PVCORE_SHAREDPOINTER_H
+#define PVCORE_SHAREDPOINTER_H
+
+#include <cstdint>
+#include <atomic>
+
+namespace PVCore
+{
+
+namespace __impl
+{
+
+template <typename T>
+class pv_ref_counter
+{
+public:
+	typedef T        type;
+	typedef T*       pointer;
+	typedef    void(*deleter)(pointer);
+
+	pv_ref_counter() : _data(nullptr), _deleter(nullptr), _count(0)
+	{}
+
+	pv_ref_counter(pointer p, deleter d) : _data(p), _deleter(d), _count(1)
+	{}
+
+	~pv_ref_counter() {}
+
+	void inc_count()
+	{
+		++_count;
+	}
+
+	void dec_count()
+	{
+		--_count;
+		if(_count == 0) {
+			if (_deleter) {
+				_deleter(_data);
+			} else {
+				delete _data;
+			}
+		}
+	}
+
+	uint32_t use_count() const
+	{
+		return _count;
+	}
+
+	pointer get() const
+	{
+		return _data;
+	}
+
+	void set_deleter(deleter d)
+	{
+		_deleter = d;
+	}
+
+	deleter get_deleter()
+	{
+		return _deleter;
+	}
+
+private:
+	volatile pointer  _data;
+	volatile deleter  _deleter;
+	volatile uint32_t _count;
+};
+
+}
+
+/**
+ * @class pv_shared_ptr
+ *
+ * @brief a shared pointer with a deleter which can be changed at run-time.
+ *
+ */
+template <typename T>
+class pv_shared_ptr
+{
+public:
+	typedef __impl::pv_ref_counter<T> ref_counter_t;
+	typedef T                         type;
+	typedef T                        *pointer;
+	typedef                     void(*deleter)(pointer);
+
+	pv_shared_ptr() : pv_shared_ptr(nullptr)
+	{}
+
+	/* MINOR BUG: this constructor is not delegate to pv_shared_ptr(p, d)
+	 * because of a bug in Debian's (or vanilla) gcc 4.6 (4.7 works well).
+	 */
+	pv_shared_ptr(pointer p)
+	{
+		_ref_count = new __impl::pv_ref_counter<type>(p, nullptr);
+	}
+
+	pv_shared_ptr(pointer p, deleter d)
+	{
+		_ref_count = new __impl::pv_ref_counter<type>(p, d);
+	}
+
+	pv_shared_ptr(const pv_shared_ptr<type> &rhs)
+	{
+		rhs._ref_count->inc_count();
+		_ref_count = rhs._ref_count;
+	}
+
+	~pv_shared_ptr()
+	{
+		reset();
+	}
+
+	void reset()
+	{
+		internal_reset();
+		_ref_count = new __impl::pv_ref_counter<type>();
+	}
+
+	pv_shared_ptr<type> &operator=(const pv_shared_ptr<type> &rhs)
+	{
+		internal_reset();
+		rhs._ref_count->inc_count();
+		_ref_count = rhs._ref_count;
+		return *this;
+	}
+
+	operator bool() const
+	{
+		return (_ref_count->get() != nullptr);
+	}
+
+	pointer get() const
+	{
+		return _ref_count->get();
+	}
+
+	T &operator*() const
+	{
+		return *(_ref_count->get());
+	}
+
+	T *operator->() const
+	{
+		return _ref_count->get();
+	}
+
+	void set_deleter(deleter d)
+	{
+		_ref_count->set_deleter(d);
+	}
+
+	deleter get_deleter() const
+	{
+		return _ref_count->get_deleter();
+	}
+
+	uint32_t use_count() const
+	{
+		return _ref_count->use_count();
+	}
+
+private:
+	void internal_reset()
+	{
+		_ref_count->dec_count();
+		if (_ref_count->use_count() == 0) {
+			delete _ref_count;
+		}
+	}
+
+private:
+	ref_counter_t *_ref_count;
+};
+
+template <class T, class U>
+bool operator==(const pv_shared_ptr<T>& lhs, const pv_shared_ptr<U>& rhs)
+{
+	return (lhs.get() == rhs.get());
+}
+
+template <class T, class U>
+bool operator!=(const pv_shared_ptr<T>& lhs, const pv_shared_ptr<U>& rhs)
+{
+	return (lhs.get() != rhs.get());
+}
+
+template <class T, class U>
+bool operator<(const pv_shared_ptr<T>& lhs, const pv_shared_ptr<U>& rhs)
+{
+	return (lhs.get() < rhs.get());
+}
+
+template <class T, class U>
+bool operator<=(const pv_shared_ptr<T>& lhs, const pv_shared_ptr<U>& rhs)
+{
+	return (lhs.get() <= rhs.get());
+}
+
+template <class T, class U>
+bool operator>(const pv_shared_ptr<T>& lhs, const pv_shared_ptr<U>& rhs)
+{
+	return (lhs.get() > rhs.get());
+}
+
+template <class T, class U>
+bool operator>=(const pv_shared_ptr<T>& lhs, const pv_shared_ptr<U>& rhs)
+{
+	return (lhs.get() >= rhs.get());
+}
+
+template <typename T>
+pv_shared_ptr<T> make_shared(T *t)
+{
+	return pv_shared_ptr<T>(t);
+}
+
+template <typename T, typename D>
+pv_shared_ptr<T> make_shared(T *t, D d)
+{
+	return pv_shared_ptr<T>(t, d);
+}
+
+}
+
+#endif // PVCORE_SHAREDPOINTER_H
---
+++ libpvkernel/tests/core/shared_ptr.cpp
@@ -0,0 +1,274 @@
+
+#include <iostream>
+#include <cassert>
+#include <sstream>
+
+#include <pvkernel/core/PVSharedPointer.h>
+
+struct pouet
+{
+	std::string print()
+	{
+		std::stringstream ss;
+		ss << "pouet: " << this;
+		return ss.str();
+	}
+};
+
+typedef struct PVCore::pv_shared_ptr<pouet> pouet_p;
+
+template <typename T>
+void deleter(T *p)
+{
+	std::cout << "    template deleter for address "
+	          << p  << std::endl;
+	delete p;
+}
+
+int main()
+{
+	std::cout << "##########################################" << std::endl;
+	std::cout << "creation" << std::endl;
+	std::cout << "  p_p1 = new pouet..." << std::endl;
+	pouet_p p_p1(new pouet);
+
+	assert(p_p1.use_count() == 1);
+	std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test in sub scope" << std::endl;
+	{
+		std::cout << "  enter scope" << std::endl;
+		pouet_p p_p2 = p_p1;
+
+		std::cout << "    p_p2 = p_p1" << std::endl;
+		assert(p_p1.use_count() == 2);
+		assert(p_p2.use_count() == 2);
+		std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+		std::cout << "    p_p2.use_count() -> " << p_p2.use_count() << std::endl;
+	}
+
+	std::cout << "  leave scope" << std::endl;
+	std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of reset()" << std::endl;
+	std::cout << "  p_p2 = p_p1" << std::endl;
+	pouet_p p_p2 = p_p1;
+
+	assert(p_p1.use_count() == 2);
+	assert(p_p2.use_count() == 2);
+
+	std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+	std::cout << "  call to p_p2.reset()" << std::endl;
+	p_p2.reset();
+
+	assert(p_p1.use_count() == 1);
+	assert(p_p2.use_count() == 0);
+
+	std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+	std::cout << "    p_p2.use_count() -> " << p_p2.use_count() << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of reset() in sub scope" << std::endl;
+	{
+		std::cout << "  enter scope" << std::endl;
+		std::cout << "    p_p3 = p_p1" << std::endl;
+		pouet_p p_p3 = p_p1;
+
+		std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+		std::cout << "    p_p3.use_count() -> " << p_p3.use_count() << std::endl;
+
+		std::cout << "  call to p_p3.reset()" << std::endl;
+		p_p3.reset();
+
+		assert(p_p1.use_count() == 1);
+		assert(p_p3.use_count() == 0);
+
+		std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+		std::cout << "    p_p3.use_count() -> " << p_p3.use_count() << std::endl;
+	}
+
+	std::cout << "  leave scope" << std::endl;
+	std::cout << "    p_p1.use_count() -> " << p_p1.use_count() << std::endl;
+	assert(p_p1.use_count() == 1);
+
+	std::cout << std::boolalpha;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of get()" << std::endl;
+	std::cout << "  p_1 is set" << std::endl;
+	std::cout << "  p_2 is reset" << std::endl;
+	p_p2.reset();
+
+	assert(p_p1.get() != nullptr);
+	std::cout << "    p_1.get() -> " << p_p1.get() << std::endl;
+	assert(p_p2.get() == nullptr);
+	std::cout << "    p_2.get() -> " << p_p2.get() << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of operator->" << std::endl;
+
+	std::cout << "    p_1->print() -> '" << p_p1->print() << "'" << std::endl;
+	std::cout << "    p_2->print() -> '" << p_p2->print() << "'" << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of operator*" << std::endl;
+
+	std::cout << "    (*p_1).print() -> '" << (*p_p1).print() << "'" << std::endl;
+	std::cout << "    (*p_2).print() -> '" << (*p_p2).print() << "'" << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of operator bool" << std::endl;
+	std::cout << "  p_1 is set" << std::endl;
+	std::cout << "  p_2 is reset" << std::endl;
+	p_p2.reset();
+
+	assert(p_p1);
+	std::cout << "    (bool)p_1 -> " << (bool)p_p1 << std::endl;
+	assert(!p_p2);
+	std::cout << "    (bool)p_2 -> " << (bool)p_p2 << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of comparison operator" << std::endl;
+
+	std::cout << "  p_p2 = p_p1" << std::endl;
+	p_p2 = p_p1;
+	std::cout << "  p_p3 = new pouet..." << std::endl;
+	pouet_p p_p3 = new pouet;
+
+	std::cout << "  operator ==" << std::endl;
+	assert(p_p1 == p_p2);
+	std::cout << "    p_1 == p_p2 -> " << (p_p1 == p_p2) << std::endl;
+	assert(p_p1 != p_p3);
+	std::cout << "    p_1 == p_p3 -> " << (p_p1 == p_p3) << std::endl;
+
+	std::cout << "  operator !=" << std::endl;
+	assert(!(p_p1 != p_p2));
+	std::cout << "    p_1 != p_p2 -> " << (p_p1 != p_p2) << std::endl;
+	assert(!(p_p1 == p_p3));
+	std::cout << "    p_1 != p_p3 -> " << (p_p1 != p_p3) << std::endl;
+
+	std::cout << "  operator <" << std::endl;
+	assert(!(p_p1 < p_p2));
+	std::cout << "    p_1 < p_p2 -> " << (p_p1 < p_p2) << std::endl;
+	std::cout << "    p_1 < p_p3 -> " << (p_p1 < p_p3) << std::endl;
+
+	std::cout << "  operator <=" << std::endl;
+	assert(p_p1 <= p_p2);
+	std::cout << "    p_1 <= p_p2 -> " << (p_p1 <= p_p2) << std::endl;
+	std::cout << "    p_1 <= p_p3 -> " << (p_p1 <= p_p3) << std::endl;
+
+	std::cout << "  operator >" << std::endl;
+	assert(!(p_p1 > p_p2));
+	std::cout << "    p_1 > p_p2 -> " << (p_p1 > p_p2) << std::endl;
+	std::cout << "    p_1 > p_p3 -> " << (p_p1 > p_p3) << std::endl;
+
+	std::cout << "  operator >=" << std::endl;
+	assert(p_p1 >= p_p2);
+	std::cout << "    p_1 >= p_p2 -> " << (p_p1 >= p_p2) << std::endl;
+	std::cout << "    p_1 >= p_p3 -> " << (p_p1 >= p_p3) << std::endl;
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "p_p1.reset() (needed for next steps)" << std::endl;
+	p_p1.reset();
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of lambda deleter on one shared_ptr" << std::endl;
+	pouet *p = new pouet;
+	std::cout << "  p = " << p << std::endl;
+
+	p_p2 = pouet_p(p);
+	std::cout << "  p_p2 is set" << std::endl;
+	std::cout << "  p_p2.set_deleter(...)" << std::endl;
+	p_p2.set_deleter([&](pouet* p)
+	                 {
+		                 std::cout << "    lambda deleter for type pouet "
+		                           << p << std::endl;
+		                 delete p;
+	                 });
+	std::cout << "  p_p2.reset()" << std::endl;
+	p_p2.reset();
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of template deleter on one shared_ptr" << std::endl;
+	p = new pouet;
+	std::cout << "  p = " << p << std::endl;
+
+	p_p2 = pouet_p(p);
+	std::cout << "  p_p2 is set" << std::endl;
+	std::cout << "  p_p2.set_deleter(...)" << std::endl;
+	p_p2.set_deleter(deleter<pouet>);
+	std::cout << "  p_p2.reset()" << std::endl;
+	p_p2.reset();
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of deleter between two shared_ptr" << std::endl;
+	std::cout << "  p_p3 is set" << std::endl;
+	std::cout << "  p_p2 = p_p3" << std::endl;
+	p_p2 = p_p3;
+
+	std::cout << "  p_p2.set_deleter(...)" << std::endl;
+	p_p2.set_deleter([&](pouet* p)
+	                 {
+		                 std::cout << "    lambda deleter for type pouet "
+		                           << p << std::endl;
+		                 delete p;
+	                 });
+	std::cout << "  p_p2.reset()" << std::endl;
+	p_p2.reset();
+	std::cout << "  p_p3.reset()" << std::endl;
+	p_p3.reset();
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of make_shared(p)" << std::endl;
+
+	p = new pouet;
+	std::cout << "  p = " << p << std::endl;
+
+	p_p2 = PVCore::make_shared<pouet>(p);
+	std::cout << "  p_p2 is set" << std::endl;
+	std::cout << "  (bool)p_p2 -> " << (bool)p_p2 << std::endl;
+	std::cout << "  p_p2.get() -> " << p_p2.get() << std::endl;
+	std::cout << "  p_p2.use_count() -> " << p_p2.use_count() << std::endl;
+	std::cout << "  p_p2.reset()" << std::endl;
+	p_p2.reset();
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of make_shared(p, lambda deleter)" << std::endl;
+
+	p = new pouet;
+	std::cout << "  p = " << p << std::endl;
+
+	p_p2 = PVCore::make_shared<pouet>(p, [&](pouet* p)
+	                                  {
+		                                  std::cout << "    lambda deleter for type pouet "
+		                                            << p  << std::endl;
+		                                  delete p;
+	                                  });
+	std::cout << "  p_p2 is set" << std::endl;
+	std::cout << "  (bool)p_p2 -> " << (bool)p_p2 << std::endl;
+	std::cout << "  p_p2.get() -> " << p_p2.get() << std::endl;
+	std::cout << "  p_p2.use_count() -> " << p_p2.use_count() << std::endl;
+	std::cout << "  p_p2.reset()" << std::endl;
+	p_p2.reset();
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "test of make_shared(p, template deleter)" << std::endl;
+
+	p = new pouet;
+	std::cout << "  p = " << p << std::endl;
+
+	p_p2 = PVCore::make_shared<pouet>(p, deleter<pouet>);
+	std::cout << "  p_p2 is set" << std::endl;
+	std::cout << "  (bool)p_p2 -> " << (bool)p_p2 << std::endl;
+	std::cout << "  p_p2.get() -> " << p_p2.get() << std::endl;
+	std::cout << "  p_p2.use_count() -> " << p_p2.use_count() << std::endl;
+	std::cout << "  p_p2.reset()" << std::endl;
+	p_p2.reset();
+
+	std::cout << "##########################################" << std::endl;
+	std::cout << "end (there must be no text after)" << std::endl;
+
+	return 0;
+}
