82ffd220a01bf26cc40f2c71108bf560412309c4
Rémi Hérilier <rherilier@picviz.com>
2012-06-27 17:21:33 +0200

    {PVHive] updated test program "functional"
    All kind of observers have been added.

--- libpvhive/tests/functional_dlg.h
+++ libpvhive/tests/functional_dlg.h
@@ -11,6 +11,9 @@
 
 #include "functional_objs.h"
 
+#define POS_LIST 4
+#define POS_DEL (POS_LIST + 1)
+
 class FunctionalDlg : public QDialog
 {
 	Q_OBJECT
@@ -23,6 +26,8 @@ public:
 
 		QGridLayout *gb = new QGridLayout(this);
 
+		/* add buttons for entity and actor
+		 */
 		pb = new QPushButton(QString("Add entity"), this);
 		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_add_entity()));
 		gb->addWidget(pb, 0, 0);
@@ -31,32 +36,48 @@ public:
 		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_add_actor()));
 		gb->addWidget(pb, 0, 1);
 
-		pb = new QPushButton(QString("Add observer"), this);
+		/* add buttons for each type of observer
+		 */
+		pb = new QPushButton(QString("Add Observer"), this);
 		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_add_observer()));
 		gb->addWidget(pb, 0, 2);
 
+		pb = new QPushButton(QString("Add QObserver"), this);
+		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_add_qobserver()));
+		gb->addWidget(pb, 1, 2);
 
+		pb = new QPushButton(QString("Add ObserverCB"), this);
+		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_add_observercb()));
+		gb->addWidget(pb, 2, 2);
+
+		pb = new QPushButton(QString("Add ObserverSignal"), this);
+		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_add_observersignal()));
+		gb->addWidget(pb, 3, 2);
+
+		/* lists
+		 */
 		_entity_lw = new QListWidget();
-		gb->addWidget(_entity_lw, 1, 0);
+		gb->addWidget(_entity_lw, POS_LIST, 0);
 
 		_actor_lw = new QListWidget();
-		gb->addWidget(_actor_lw, 1, 1);
+		gb->addWidget(_actor_lw, POS_LIST, 1);
 
 		_observer_lw = new QListWidget();
-		gb->addWidget(_observer_lw, 1, 2);
+		gb->addWidget(_observer_lw, POS_LIST, 2);
 
-
+		/* del buttons
+		 */
 		pb = new QPushButton(QString("Del entity"), this);
 		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_del_entity()));
-		gb->addWidget(pb, 2, 0);
+		gb->addWidget(pb, POS_DEL, 0);
 
 		pb = new QPushButton(QString("Del actor"), this);
 		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_del_actor()));
-		gb->addWidget(pb, 2, 1);
+		gb->addWidget(pb, POS_DEL, 1);
 
 		pb = new QPushButton(QString("Del observer"), this);
 		connect(pb, SIGNAL(clicked(bool)), this, SLOT(do_del_observer()));
-		gb->addWidget(pb, 2, 2);
+		gb->addWidget(pb, POS_DEL, 2);
 
 		resize(320,200);
 	}
@@ -80,7 +101,7 @@ private slots:
 
 	void do_close_actor(int)
 	{
-		EntityGUIActor *a = qobject_cast<EntityGUIActor *>(sender());
+		EntityActor *a = qobject_cast<EntityActor *>(sender());
 		auto items = _actor_lw->findItems(QString::number(a->get_id()),
 		                                  Qt::MatchExactly);
 		if (items.isEmpty() == false) {
@@ -91,7 +112,7 @@ private slots:
 
 	void do_close_observer(int)
 	{
-		EntityGUIObserver *o = qobject_cast<EntityGUIObserver *>(sender());
+		Interactor *o = dynamic_cast<Interactor *>(sender());
 		auto items = _observer_lw->findItems(QString::number(o->get_id()),
 		                                     Qt::MatchExactly);
 		if (items.isEmpty() == false) {
@@ -110,105 +131,172 @@ private slots:
 		++_entity_next;
 	}
 
-	void do_del_entity()
-	{
-		auto selected = _entity_lw->selectedItems();
-
-		if (selected.isEmpty() == false) {
-			QListWidgetItem *item = selected.at(0);
-			int eid = item->text().toInt();
-			Entity *e = _entity_list[eid];
-			std::cout << "start unregistering entity " << eid << std::endl;
-			PVHive::PVHive::get().unregister_object(*e);
-			std::cout << "end unregistering entity " << eid << std::endl;
-			_entity_list.remove(eid);
-			delete item;
-		}
-	}
-
 	void do_add_actor()
 	{
 		auto selected = _entity_lw->selectedItems();
 
-		if (selected.isEmpty() == false) {
-			int eid = selected.at(0)->text().toInt();
-			QString ent_name = "object " + QString::number(eid);
-
-			EntityGUIActor *a = new EntityGUIActor(_actor_next, ent_name, this);
-			_actor_list[_actor_next] = a;
-			_actor_lw->addItem(QString::number(_actor_next));
-			++_actor_next;
-
-			connect(a, SIGNAL(finished(int)), this, SLOT(do_close_actor(int)));
-
-			Entity *e = _entity_list[eid];
-			PVHive::PVHive::get().register_actor(*e, *a);
-
-			a->show();
+		if (selected.isEmpty()) {
+			return;
 		}
-	}
 
-	void do_del_actor()
-	{
-		auto selected = _actor_lw->selectedItems();
+		int eid = selected.at(0)->text().toInt();
+		QString ent_name = "object " + QString::number(eid);
 
-		if (selected.isEmpty() == false) {
-			QListWidgetItem *item = selected.at(0);
-			int aid = item->text().toInt();
-			EntityGUIActor *a = _actor_list[aid];
-			_actor_list.remove(aid);
-			delete item;
-			a->close();
-		}
+		EntityActor *a = new EntityActor(_actor_next, ent_name, this);
+		_actor_list[_actor_next] = a;
+		_actor_lw->addItem(QString::number(_actor_next));
+		++_actor_next;
+
+		connect(a, SIGNAL(finished(int)), this, SLOT(do_close_actor(int)));
+
+		Entity *e = _entity_list[eid];
+		PVHive::PVHive::get().register_actor(*e, *a);
+
+		a->show();
 	}
 
 	void do_add_observer()
 	{
 		auto selected = _entity_lw->selectedItems();
 
-		if (selected.isEmpty() == false) {
-			int eid = selected.at(0)->text().toInt();
-			QString ent_name = "object " + QString::number(eid);
-			EntityGUIObserver *o = new EntityGUIObserver(_observer_next, ent_name, this);
-			_observer_list[_observer_next] = o;
-			_observer_lw->addItem(QString::number(_observer_next));
-			++_observer_next;
-
-			connect(o, SIGNAL(finished(int)), this, SLOT(do_close_observer(int)));
-
-			Entity *e = _entity_list[eid];
-			PVHive::PVHive::get().register_observer(*e, *o);
-
-			o->show();
+		if (selected.isEmpty()) {
+			return;
 		}
+
+		int eid = selected.at(0)->text().toInt();
+		QString ent_name = "object " + QString::number(eid);
+		EntityObserver *o = new EntityObserver(_observer_next, ent_name, this);
+		add_observer(_observer_next, o, o, eid);
+		++_observer_next;
+
+		connect(o, SIGNAL(finished(int)), this, SLOT(do_close_observer(int)));
+
+		o->show();
+	}
+
+	void do_add_qobserver()
+	{
+		auto selected = _entity_lw->selectedItems();
+
+		if (selected.isEmpty()) {
+			return;
+		}
+
+		int eid = selected.at(0)->text().toInt();
+		QString ent_name = "object " + QString::number(eid);
+		EntityQObserver *o = new EntityQObserver(_observer_next, this);
+		add_observer(_observer_next, o, o, eid);
+		++_observer_next;
+
+		connect(o, SIGNAL(finished(int)), this, SLOT(do_close_observer(int)));
+	}
+
+	void do_add_observercb()
+	{
+		auto selected = _entity_lw->selectedItems();
+
+		if (selected.isEmpty()) {
+			return;
+		}
+
+		int eid = selected.at(0)->text().toInt();
+		QString ent_name = "object " + QString::number(eid);
+		EntityObserverCB *o = new EntityObserverCB(_observer_next);
+		add_observer(_observer_next, o, o->get(), eid);
+		++_observer_next;
+
+		connect(o, SIGNAL(finished(int)), this, SLOT(do_close_observer(int)));
+	}
+
+	void do_add_observersignal()
+	{
+		auto selected = _entity_lw->selectedItems();
+
+		if (selected.isEmpty()) {
+			return;
+		}
+
+		int eid = selected.at(0)->text().toInt();
+		QString ent_name = "object " + QString::number(eid);
+		EntityObserverSignal *o = new EntityObserverSignal(_observer_next, this);
+		add_observer(_observer_next, o, o->get(), eid);
+		++_observer_next;
+
+		connect(o, SIGNAL(finished(int)), this, SLOT(do_close_observer(int)));
+	}
+
+	void do_del_entity()
+	{
+		auto selected = _entity_lw->selectedItems();
+
+		if (selected.isEmpty()) {
+			return;
+		}
+
+		QListWidgetItem *item = selected.at(0);
+		int eid = item->text().toInt();
+		Entity *e = _entity_list[eid];
+		std::cout << "start unregistering entity " << eid << std::endl;
+		PVHive::PVHive::get().unregister_object(*e);
+		std::cout << "end unregistering entity " << eid << std::endl;
+		_entity_list.remove(eid);
+		delete item;
+	}
+
+	void do_del_actor()
+	{
+		auto selected = _actor_lw->selectedItems();
+
+		if (selected.isEmpty()) {
+			return;
+		}
+
+		QListWidgetItem *item = selected.at(0);
+		int aid = item->text().toInt();
+		Interactor *a = _actor_list[aid];
+		_actor_list.remove(aid);
+		delete item;
+		a->terminate();
 	}
 
 	void do_del_observer()
 	{
 		auto selected = _observer_lw->selectedItems();
 
-		if (selected.isEmpty() == false) {
-			QListWidgetItem *item = selected.at(0);
-			int oid = item->text().toInt();
-			EntityGUIObserver *o = _observer_list[oid];
-			_observer_list.remove(oid);
-			delete item;
-			o->close();
+		if (selected.isEmpty()) {
+			return;
 		}
+
+		QListWidgetItem *item = selected.at(0);
+		int oid = item->text().toInt();
+		Interactor *o = dynamic_cast<Interactor*>(_observer_list[oid]);
+		_observer_list.remove(oid);
+		delete item;
+		o->terminate();
 	}
 
 private:
-	int                 _entity_next;
-	QHash<int, Entity*> _entity_list;
-	QListWidget        *_entity_lw;
+	void add_observer(int id, Interactor *i, PVHive::PVObserverBase *o, int eid)
+	{
+		_observer_list[id] = i;
+		_observer_lw->addItem(QString::number(id));
 
-	int                         _actor_next;
-	QHash<int, EntityGUIActor*> _actor_list;
-	QListWidget                *_actor_lw;
+		Entity *e = _entity_list[eid];
+		PVHive::PVHive::get().register_observer<Entity>(*e, *o);
+	}
 
-	int                            _observer_next;
-	QHash<int, EntityGUIObserver*> _observer_list;
-	QListWidget                   *_observer_lw;
+private:
+	int                  _entity_next;
+	QHash<int, Entity*>  _entity_list;
+	QListWidget         *_entity_lw;
+
+	int                      _actor_next;
+	QHash<int, Interactor*>  _actor_list;
+	QListWidget             *_actor_lw;
+
+	int                      _observer_next;
+	QHash<int, Interactor*>  _observer_list;
+	QListWidget             *_observer_lw;
 };
 
 #endif // BIG_TEST_DLG_H
--- libpvhive/tests/functional_objs.h
+++ libpvhive/tests/functional_objs.h
@@ -3,6 +3,12 @@
 #define BIG_TEST_OBJS_H
 
 #include <pvhive/PVActor.h>
+#include <pvhive/PVObserver.h>
+#include <pvhive/PVQObserver.h>
+#include <pvhive/PVObserverCallback.h>
+#include <pvhive/PVObserverSignal.h>
+
+#include <functional>
 
 #include <QDialog>
 #include <QMessageBox>
@@ -21,6 +27,11 @@ public:
 		return _id;
 	}
 
+	QString get_id_str() const
+	{
+		return QString::number(_id);
+	}
+
 	void set_value(int v)
 	{
 		_v = v;
@@ -36,13 +47,36 @@ private:
 	int _v;
 };
 
-class EntityGUIActor : public QDialog, public PVHive::PVActor<Entity>
+class Interactor
+{
+public:
+	Interactor(int id) : _id(id)
+	{}
+
+	int get_id() const
+	{
+		return _id;
+	}
+
+	QString get_id_str() const	{
+		return QString::number(_id);
+	}
+
+	virtual void terminate() = 0;
+
+private:
+	int _id;
+};
+
+class EntityActor : public QDialog, public Interactor, public PVHive::PVActor<Entity>
 {
 	Q_OBJECT
 
 public:
-	EntityGUIActor(int id, QString &ent_name, QWidget *parent) : QDialog(parent),
-	                                                             _id(id), _value(0)
+	EntityActor(int id, QString &obj_name, QWidget *parent) :
+		QDialog(parent),
+		Interactor(id),
+		_value(0)
 	{
 		int duration = random() % 2000;
 
@@ -58,21 +92,21 @@ public:
 		connect(_timer, SIGNAL(timeout()), this, SLOT(do_update()));
 		_timer->start(duration);
 
-		setWindowTitle("actor " + QString::number(id) + " - " + ent_name);
+		setWindowTitle("actor " + QString::number(id) + " (" + obj_name + ")");
 
-		setAttribute( Qt::WA_DeleteOnClose, true );
+		setAttribute(Qt::WA_DeleteOnClose, true);
 
 		resize(256, 64);
 	}
 
-	~EntityGUIActor()
+	~EntityActor()
 	{
-		std::cout << "death of "<< windowTitle().toStdString() << std::endl;
+		std::cout << "actor " << get_id() << ": death" << std::endl;
 	}
 
-	int get_id() const
+	virtual void terminate()
 	{
-		return _id;
+		close();
 	}
 
 private slots:
@@ -90,34 +124,30 @@ private:
 	QLabel *_vl;
 };
 
-class EntityGUIObserver : public QDialog, public PVHive::PVObserver<Entity>
+class EntityObserver : public QDialog, public Interactor, public PVHive::PVObserver<Entity>
 {
 	Q_OBJECT
 
 public:
-	EntityGUIObserver(int id, QString &obj_name, QWidget *parent) : QDialog(parent),
-	                                                                _id(id)
+	EntityObserver(int id, QString &obj_name, QWidget *parent) :
+		QDialog(parent),
+		Interactor(id)
 	{
 		QVBoxLayout *vb = new QVBoxLayout(this);
 
 		_vl = new QLabel("value: NA");
 		vb->addWidget(_vl);
 
-		setWindowTitle("observer " + QString::number(id) + " - " + obj_name);
+		setWindowTitle("observer " + QString::number(id) + " (" + obj_name + ")");
 
-		setAttribute( Qt::WA_DeleteOnClose, true );
+		setAttribute(Qt::WA_DeleteOnClose, true);
 
 		resize(256, 64);
 	}
 
-	int get_id() const
+	~EntityObserver()
 	{
-		return _id;
-	}
-
-	~EntityGUIObserver()
-	{
-		std::cout << "death of "<< windowTitle().toStdString() << std::endl;
+		std::cout << "observer " << get_id() << ": death" << std::endl;
 	}
 
 	void refresh()
@@ -128,16 +158,167 @@ public:
 	void about_to_be_deleted()
 	{
 		QMessageBox box;
-		box.setText("Sure to quit observer " + QString::number(_id) + "?");
+		box.setText("Sure to quit observer " + get_id_str() + "?");
 		box.setStandardButtons(QMessageBox::Ok);
 		box.exec();
-		std::cout << "suicide of "<< windowTitle().toStdString() << std::endl;
+		std::cout << "observer " << get_id() << ": suicide" << std::endl;
+		terminate();
+	}
+
+	virtual void terminate()
+	{
 		close();
 	}
 
 private:
 	int     _id;
 	QLabel *_vl;
+};
+
+class EntityQObserver : public PVHive::PVQObserver<Entity>, public Interactor
+{
+	Q_OBJECT
+
+public:
+	EntityQObserver(int id, QObject *parent) :
+		PVHive::PVQObserver<Entity>(parent),
+		Interactor(id)
+	{
+	}
+
+	~EntityQObserver()
+	{
+		emit finished(0);
+		std::cout << "qobserver " << get_id() << ": death" << std::endl;
+	}
+
+	virtual void do_refresh(PVHive::PVObserverBase *)
+	{
+		std::cout << "qobserver " << get_id() << ": new value is "
+		          << get_object()->get_value() << std::endl;
+	}
+
+	virtual void do_about_to_be_deleted(PVHive::PVObserverBase *)
+	{
+		QMessageBox box;
+		box.setText("Sure to quit qobserver " + get_id_str() + "?");
+		box.setStandardButtons(QMessageBox::Ok);
+		box.exec();
+		std::cout << "qobserver " << get_id() << ": suicide" << std::endl;
+		terminate();
+	}
+
+	virtual void terminate()
+	{
+		deleteLater();
+	}
+
+signals:
+	void finished(int);
+};
+
+class EntityObserverSignal : public QObject, public Interactor
+{
+	Q_OBJECT
+
+public:
+	EntityObserverSignal(int id, QObject *parent) :
+		QObject(parent),
+		Interactor(id),
+		_os(this)
+	{
+		_os.connect_refresh(this, "do_refresh");
+		_os.connect_about_to_be_deleted(this, "do_about_to_be_deleted");
+	}
+
+	~EntityObserverSignal()
+	{
+		emit finished(0);
+		std::cout << "observersignal " << get_id() << ": death" << std::endl;
+	}
+
+	PVHive::PVObserverBase *get()
+	{
+		return &_os;
+	}
+
+	virtual void terminate()
+
+	{
+		deleteLater();
+	}
+
+signals:
+	void finished(int);
+
+public slots:
+	void do_refresh(PVHive::PVObserverBase *)
+	{
+		std::cout << "observersignal " << get_id() << ": new value is "
+		          << _os.get_object()->get_value() << std::endl;
+	}
+
+	void do_about_to_be_deleted(PVHive::PVObserverBase *)
+	{
+		QMessageBox box;
+		box.setText("Sure to quit observersignal " + get_id_str() + "?");
+		box.setStandardButtons(QMessageBox::Ok);
+		box.exec();
+		std::cout << "observersignal " << get_id() << ": suicide" << std::endl;
+		terminate();
+	}
+
+private:
+	PVHive::PVObserverSignal<Entity> _os;
+};
+
+class EntityObserverCB : public QObject, public Interactor
+{
+	Q_OBJECT
+
+public:
+	typedef std::function<void(const Entity *)> func_type;
+	typedef PVHive::PVObserverCallback<Entity, func_type, func_type> ocb_t;
+
+	EntityObserverCB(int id) :
+		Interactor(id)
+	{
+		_ocb = PVHive::create_observer_callback<Entity,
+		                                        func_type,
+		                                        func_type>(std::bind([] (const Entity *e, EntityObserverCB *o) {
+					                                        std::cout << "observercb " << o->get_id() << ": " << e->get_value() << std::endl;
+				                                        }, std::placeholders::_1, this),
+			                                        std::bind([](const Entity *e, EntityObserverCB *o){
+					                                        QMessageBox box;
+					                                        box.setText("Sure to quit observersignal " + o->get_id_str() + "?");
+					                                        box.setStandardButtons(QMessageBox::Ok);
+					                                        box.exec();
+					                                        std::cout << "observersignal of object" << e->get_id() << ": suicide" << std::endl;
+					                                        o->terminate();
+				                                        }, std::placeholders::_1, this));
+	}
+
+	~EntityObserverCB()
+	{
+		emit finished(0);
+		std::cout << "observer " << get_id() << ": death" << std::endl;
+	}
+
+	ocb_t *get()
+	{
+		return &_ocb;
+	}
+
+	void terminate()
+	{
+		deleteLater();
+	}
+
+signals:
+	void finished(int);
+
+private:
+	ocb_t _ocb;
 };
 
 #endif // BIG_TEST_OBJS_H
