db07bb2e2dd358f4cfd783c32cced37f48179dc5
Rémi Hérilier <rherilier@picviz.com>
2012-07-17 17:43:12 +0200

    [PVKernel]  add memory benchmark

--- libpvkernel/src/include/pvkernel/core/picviz_bench.h
+++ libpvkernel/src/include/pvkernel/core/picviz_bench.h
@@ -3,11 +3,14 @@
 
 #include <tbb/tick_count.h>
 #include <iostream>
+#include <pvkernel/core/PVAllocators.h>
 
 #define BENCH_START(var)\
 	tbb::tick_count __bench_start_##var = tbb::tick_count::now();
+#define BENCH_END_NODISP(var)\
+	tbb::tick_count __bench_end_##var = tbb::tick_count::now();
 #define BENCH_END(var, desc, nelts_start, size_elt_start, nelts_end, size_elt_end)\
-	tbb::tick_count __bench_end_##var = tbb::tick_count::now();\
+	BENCH_END_NODISP(var);\
 	{\
 		double time = (__bench_end_##var-__bench_start_##var).seconds();\
 		double size_in_mb = (double)(nelts_start*size_elt_start)/(1024.0*1024.0);\
@@ -17,8 +20,22 @@
 		std::cout << desc << ": in " << time*1000 << " ms. Input (#/size/BW): " << nelts_start << "/" << size_in_mb << " MB/" << bw_in << " MB/s | Output (#/size/BW): " << nelts_end << "/" << size_out_mb << " MB/" << bw_out << " MB/s" << std::endl;\
 	}
 
+#define BENCH_END_TIME(var)\
+		((__bench_end_##var-__bench_start_##var).seconds())
+
 #define BENCH_END_SAME_TYPE(var, desc, nelts_in, nelts_out, size_elt) BENCH_END(var, desc, nelts_in, size_elt, nelts_out, size_elt)
 #define BENCH_END_TRANSFORM(var, desc, nelts, size_elt) BENCH_END_SAME_TYPE(var, desc, nelts, nelts, size_elt)
+
+// Memory benchmarking
+#define MEM_START(var)\
+	double __mem_bench_##var_vm, __mem_bench_##var_rss;\
+	PVCore::PVMemory::get_memory_usage(__mem_bench_##var_vm, __mem_bench_##var_rss);
+#define MEM_END(var, desc)\
+	double __mem_bench_##var_vm_end, __mem_bench_##var_rss_end;\
+	PVCore::PVMemory::get_memory_usage(__mem_bench_##var_vm_end, __mem_bench_##var_rss_end);\
+	double diff_vm_mb = (__mem_bench_##var_vm_end-__mem_bench_##var_vm)/(1024.0);\
+	double diff_rss_mb = (__mem_bench_##var_rss_end-__mem_bench_##var_rss)/(1024.0);\
+	std::cout << desc << ": memory footprint is: VM=" << diff_vm_mb << " MB / RES=" << diff_rss_mb << " MB" << std::endl;
 
 #define CHECK(v) __CHECK(v, __FILE__, __LINE__)
 #define __CHECK(v,F,L)\
