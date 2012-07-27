b875cf0f27af7599853169d7a8b848431879616f
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-07-27 18:33:12 +0200

    Forgotten one more file...

---
+++ libpvkernel/src/include/pvkernel/core/PVEnableSharedFromThis.h
@@ -0,0 +1,73 @@
+/**
+ * \file PVEnableSharedFromThis.h
+ *
+ * Copyright (C) Picviz Labs 2012
+ */
+
+#ifndef PVENABLESHAREDFROMTHIS_H_
+#define PVENABLESHAREDFROMTHIS_H_
+
+#include <pvkernel/core/PVSharedPointer.h>
+#include <pvkernel/core/PVWeakPointer.h>
+
+namespace PVCore
+{
+
+template<class T>
+class PVEnableSharedFromThis
+{
+protected:
+
+	PVEnableSharedFromThis()
+    {
+    }
+
+	PVEnableSharedFromThis(PVEnableSharedFromThis const &)
+    {
+    }
+
+	PVEnableSharedFromThis & operator=(PVEnableSharedFromThis const &)
+    {
+        return *this;
+    }
+
+    ~PVEnableSharedFromThis()
+    {
+    }
+
+public:
+
+    PVSharedPtr<T> shared_from_this()
+    {
+    	PVSharedPtr<T> p(_weak_this);
+        assert(p.get() == this);
+        return p;
+    }
+
+    PVSharedPtr<T const> shared_from_this() const
+    {
+    	PVSharedPtr<T const> p(_weak_this);
+        assert(p.get() == this);
+        return p;
+    }
+
+public: // actually private, but avoids compiler template friendship issues
+
+    // Note: invoked automatically by shared_ptr; do not call
+    template<class X, class Y> void _internal_accept_owner(PVSharedPtr<X> const* ppx, Y* py) const
+    {
+        if(_weak_this.expired())
+        {
+        	_weak_this = PVSharedPtr<T>(*ppx, py);
+        }
+    }
+
+private:
+
+    mutable PVWeakPtr<T> _weak_this;
+};
+
+}
+
+
+#endif /* PVENABLESHAREDFROMTHIS_H_ */
