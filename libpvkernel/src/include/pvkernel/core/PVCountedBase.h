e074abae7f9d93da4de1baa5a003ffe930a052f2
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-07-27 18:26:12 +0200

    Forgotten some files...

---
+++ libpvkernel/src/include/pvkernel/core/PVCountedBase.h
@@ -0,0 +1,104 @@
+#ifndef PVCOUNTEDBASE_H_
+#define PVCOUNTEDBASE_H_
+
+namespace PVCore
+{
+
+template <typename T>
+class PVCountedBase
+{
+public:
+	typedef T type;
+	typedef T*  pointer;
+	typedef  void(*deleter)(pointer);
+
+public:
+
+    PVCountedBase(deleter d = nullptr) : _deleter(d)
+    {
+    	_use_count = 1;
+    	_weak_count = 1;
+    }
+
+    virtual ~PVCountedBase()
+    {
+    }
+
+    virtual void destroy()
+    {
+        delete this;
+    }
+
+    virtual void dispose(pointer p)
+    {
+    	if (_deleter != nullptr) {
+			(_deleter)(p);
+		}
+		else {
+			delete p;
+		}
+    }
+
+    long add_ref_copy()
+	{
+    	return _use_count.fetch_and_increment();
+	}
+
+    long add_ref_lock()
+	{
+		return _use_count.fetch_and_increment() != 0;
+	}
+
+	void release(pointer p)
+	{
+		if(_use_count.fetch_and_decrement() == 1)
+		{
+			dispose(p);
+			weak_release();
+		}
+	}
+
+	long weak_add_ref()
+    {
+    	return _weak_count.fetch_and_increment();
+    }
+
+    void weak_release()
+    {
+        if(_weak_count.fetch_and_decrement() == 1)
+        {
+            destroy();
+        }
+    }
+
+    long use_count() const
+    {
+    	return _use_count.fetch_and_add((long)0);
+    }
+
+	inline void set_deleter(deleter d = nullptr)
+	{
+		pv_spin_lock_guard_t guard(_spin_lock);
+		_deleter = d;
+	}
+
+	inline deleter get_deleter() const
+	{
+		pv_spin_lock_guard_t guard(_spin_lock);
+		return _deleter;
+	}
+
+private:
+	PVCountedBase<T>(PVCountedBase<T> const & );
+	PVCountedBase<T> & operator= (PVCountedBase<T> const & );
+
+	mutable tbb::atomic<long> _use_count;
+	mutable tbb::atomic<long> _weak_count;
+
+	deleter _deleter;
+	mutable pv_spin_lock_t _spin_lock;
+};
+
+}
+
+#endif /* PVCOUNTEDBASE_H_ */
---
+++ libpvkernel/src/include/pvkernel/core/PVWeakPointer.h
@@ -0,0 +1,195 @@
+/**
+ * \file PVWeakPointer.h
+ *
+ * Copyright (C) Picviz Labs 2012
+ */
+
+#ifndef PVWEAKPOINTER_H_
+#define PVWEAKPOINTER_H_
+
+#include <pvkernel/core/PVCountedBase.h>
+
+namespace PVCore
+{
+
+template <typename T>
+class PVSharedPtr;
+
+template <typename T>
+class PVSharedCounter;
+
+namespace __impl
+{
+
+template <typename T>
+class PVWeakCounter
+{
+public:
+	template<typename X> friend class PVSharedCounter;
+    friend class PVCountedBase<T>;
+
+public:
+	typedef T type;
+	typedef T* pointer;
+
+public:
+
+    PVWeakCounter(pointer p = nullptr): _px(p), _counted_base()
+    {
+    }
+
+    PVWeakCounter(PVWeakCounter<T> const & r) : _px(r.px), _counted_base(r._counted_base)
+    {
+        if(_counted_base != nullptr) {
+        	_counted_base->weak_add_ref();
+        }
+    }
+
+    ~PVWeakCounter()
+    {
+        if(_counted_base != nullptr) {
+        	_counted_base->weak_release();
+        }
+    }
+
+    PVWeakCounter<T>& operator=(PVWeakCounter<T> const& r)
+    {
+    	PVCountedBase<T>* tmp = r._counted_base;
+
+        if(tmp != _counted_base)
+        {
+            if(tmp != nullptr) {
+            	tmp->weak_add_ref();
+            }
+            if(_counted_base != nullptr) {
+            	_counted_base->weak_release();
+            }
+            _counted_base = tmp;
+            _px = r._px;
+        }
+
+        return *this;
+    }
+
+    PVWeakCounter<T>& operator=(PVSharedCounter<T> const& r)
+	{
+    	PVCountedBase<T>* tmp = r._counted_base;
+
+		if(tmp != _counted_base)
+		{
+			if(tmp != nullptr) {
+				tmp->weak_add_ref();
+			}
+			if(_counted_base != nullptr) {
+				_counted_base->weak_release();
+			}
+			_counted_base = tmp;
+			_px = r._px;
+		}
+
+		return *this;
+	}
+
+    void swap(PVWeakCounter<T> & r)
+    {
+    	pointer* tmp_px = r._px;
+    	PVCountedBase<T> * tmp_cb = r._counted_base;
+        r._counted_base = _counted_base;
+        r._px = _px;
+        _counted_base = tmp_cb;
+        _px = tmp_px;
+    }
+
+    long use_count() const
+    {
+        return !empty() ? _counted_base->use_count(): 0;
+    }
+
+    bool empty() const
+    {
+        return _counted_base == nullptr;
+    }
+
+    friend inline bool operator==(PVWeakCounter<T> const & a, PVWeakCounter<T> const & b)
+    {
+        return a._counted_base == b._counted_base;
+    }
+
+private:
+    pointer _px;
+	PVCountedBase<T>* _counted_base;
+};
+
+
+}
+
+template <typename T>
+class PVWeakPtr
+{
+public:
+	friend class PVSharedPtr<T>;
+
+public:
+	typedef T type;
+    typedef __impl::PVWeakCounter<type> weak_counter_t;
+    typedef type* pointer;
+
+public:
+	PVWeakPtr(): _weak_count()
+	{
+	}
+
+	PVWeakPtr(PVWeakPtr const & r): _weak_count(r._weak_count)
+	{
+	}
+
+	template<class Y>
+	PVWeakPtr(PVWeakPtr<Y> const & r) : _weak_count(r._weak_count)
+	{
+	}
+
+	PVWeakPtr& operator=(PVWeakPtr const & r)
+	{
+		_weak_count = r._weak_count;
+		return *this;
+	}
+
+	template<class Y>
+	PVWeakPtr & operator=(PVWeakPtr<Y> const & r)
+	{
+		_weak_count = r._weak_count;
+		return *this;
+	}
+
+    PVSharedPtr<T> lock() const
+	{
+		return PVSharedPtr<type>(*this);
+	}
+
+	long use_count() const
+	{
+		return _weak_count.use_count();
+	}
+
+	bool expired() const
+	{
+		return _weak_count.use_count() == 0;
+	}
+
+	void reset()
+	{
+		PVWeakPtr<T>().swap(*this);
+	}
+
+	void swap(PVWeakPtr<T> & other)
+	{
+		_weak_count.swap(other._weak_count);
+	}
+
+private:
+	weak_counter_t _weak_count;
+};
+
+}
+
+#endif /* PVWEAKPOINTER_H_ */
