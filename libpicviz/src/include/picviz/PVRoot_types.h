985eef3bad67da0eda59c27d266e230adc25f72e
Adrien Guinet <aguinet@picviz.com>
2012-08-21 09:14:46 +0200

    Forgotten files, again

---
+++ libpicviz/src/include/picviz/PVRoot_types.h
@@ -0,0 +1,13 @@
+#ifndef PICVIZ_PVROOT_TYPES_H
+#define PICVIZ_PVROOT_TYPES_H
+
+#include <pvkernel/core/PVSharedPointer.h>
+
+namespace Picviz {
+
+class PVRoot;
+typedef PVCore::PVSharedPtr<PVRoot> PVRoot_sp;
+
+}
+
+#endif
---
+++ libpicviz/src/include/picviz/PVScene_types.h
@@ -0,0 +1,13 @@
+#ifndef PICVIZ_PVSCENE_TYPES_H
+#define PICVIZ_PVSCENE_TYPES_H
+
+#include <pvkernel/core/PVSharedPointer.h>
+
+namespace Picviz {
+
+class PVScene;
+typedef PVCore::PVSharedPtr<PVScene> PVScene_sp;
+
+}
+
+#endif
