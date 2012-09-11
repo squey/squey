3a6093e666e1598639103675963431593a575f6b
Adrien Guinet <aguinet@picviz.com>
2012-09-11 10:33:30 +0200

    Forgotten mappe_dtypes

---
+++ libpicviz/src/include/picviz/PVMapped_types.h
@@ -0,0 +1,14 @@
+#ifndef PICVIZ_PVMAPPED_TYPES_H
+#define PICVIZ_PVMAPPED_TYPES_H
+
+#include <pvkernel/core/PVDecimalStorage.h>
+
+namespace Picviz {
+
+class PVMapped;
+typedef PVCore::PVDecimalStorage<32> mapped_decimal_storage_type;
+
+}
+
+
+#endif
