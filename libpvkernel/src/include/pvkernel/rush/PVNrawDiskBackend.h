0bee194cf95cf26bfed6fc9ee8149aaef1053bd1
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-09-13 18:01:27 +0200

    [PVNrawDiskBackend] Cleanup (added PVColumn class)

--- libpvkernel/src/include/pvkernel/rush/PVNrawDiskBackend.h
+++ libpvkernel/src/include/pvkernel/rush/PVNrawDiskBackend.h
@@ -105,72 +105,70 @@ public:
 public:
 	PVNRawDiskBackend(std::string const& nraw_folder, uint64_t num_cols) : _nraw_folder(nraw_folder), _num_cols(num_cols)
 	{
-		_files.reserve(_num_cols);
-		for (int i = 0 ; i < _num_cols ; i++) {
-			std::stringstream filename;
-			filename << nraw_folder << "/column_" << i;
+		_columns.reserve(_num_cols);
+		for (int col = 0 ; col < _num_cols ; col++) {
+			PVColumn column;
 
 			// Open file
-			file_t file;
-			if(!this->Open(filename.str(), &file)) {
+			std::stringstream filename;
+			filename << nraw_folder << "/column_" << col;
+			if(!this->Open(filename.str(), &column.file)) {
 				PVLOG_ERROR("PVNRawDiskBackend: Error opening file %s (%s)\n", filename.str().c_str(), strerror(errno));
 				return;
 			}
-			_files.push_back(file);
 
 			// Create buffer
-			_buffers_size_idx.push_back(0);
 			uint64_t buffer_size = _buffers_size_pattern[0];
-			char* buffer = PVCore::PVAlignedAllocator<char, BUF_ALIGN>().allocate(buffer_size);
-			_buffers.push_back(buffer);
-			_buffers_ptr.push_back(buffer);
-			_buffers_end_ptr.push_back(buffer + buffer_size);
+			column.buffer = PVCore::PVAlignedAllocator<char, BUF_ALIGN>().allocate(buffer_size);
+			column.buffer_ptr = column.buffer;
+			column.buffer_end_ptr = column.buffer + buffer_size;
+
+			_columns.push_back(std::move(column));
 		}
 	}
 
-	void add(PVCol col, const char* field, uint64_t field_size)
+	void add(PVCol col_idx, const char* field, uint64_t field_size)
 	{
+		PVColumn& column = get_col(col_idx);
 		uint64_t field_part2_size = 0;
 		char* field_part2 = nullptr;
 
 		// Fill the buffer with complete field
-		if (_buffers_ptr[col] + field_size < _buffers_end_ptr[col]) {
-			memcpy(_buffers_ptr[col], field, field_size+1);
-			_buffers_ptr[col] += field_size+1;
+		if (column.buffer_ptr + field_size < column.buffer_end_ptr) {
+			memcpy(column.buffer_ptr, field, field_size+1);
+			column.buffer_ptr += field_size+1;
 		}
 		// Fill the buffer with splitted field
 		else {
-			uint64_t field_part1_size = _buffers_end_ptr[col] - _buffers_ptr[col];
-			memcpy(_buffers_ptr[col], field, field_part1_size);
+			uint64_t field_part1_size = column.buffer_end_ptr - column.buffer_ptr;
+			memcpy(column.buffer_ptr, field, field_part1_size);
 			field_part2 = const_cast<char*>(field) + field_part1_size; // ugly
 			field_part2_size = field_size - field_part1_size +1;
-			_buffers_ptr[col] += field_part1_size;
+			column.buffer_ptr += field_part1_size;
 		}
 
-
-		if (_buffers_ptr[col] == _buffers_end_ptr[col]) {
+		if (column.buffer_ptr == column.buffer_end_ptr) {
 			// Buffer is full: write it to disk
-			if(!this->Write(_buffers[col], _buffers_size_pattern[_buffers_size_idx[col]], _files[col])) {
-				PVLOG_ERROR("PVNRawDiskBackend: Error writing column %d to disk (%s)\n", col, strerror(errno));
+			if(!this->Write(column.buffer, _buffers_size_pattern[column.buffers_size_idx], column.file)) {
+				PVLOG_ERROR("PVNRawDiskBackend: Error writing column %d to disk (%s)\n", col_idx, strerror(errno));
 				return;
 			}
 
-			uint64_t buffer_max_size = _buffers_size_pattern[_buffers_size_idx[col]];
+			uint64_t buffer_max_size = _buffers_size_pattern[column.buffers_size_idx];
 			uint64_t max_size_idx = sizeof(_buffers_size_pattern)/sizeof(uint64_t)-1;
-			if (_buffers_size_idx[col] < max_size_idx) {
+			if (column.buffers_size_idx < max_size_idx) {
 				// Reallocate a bigger buffer and copy the end of splitted field
-				PVCore::PVAlignedAllocator<char, BUF_ALIGN>().deallocate(_buffers[col], buffer_max_size);
-				_buffers_size_idx[col]++;
-				uint64_t new_buffer_max_size = _buffers_size_pattern[_buffers_size_idx[col]];
-				_buffers[col] = PVCore::PVAlignedAllocator<char, BUF_ALIGN>().allocate(new_buffer_max_size);
-				_buffers_end_ptr[col] = _buffers[col] + new_buffer_max_size;
+				PVCore::PVAlignedAllocator<char, BUF_ALIGN>().deallocate(column.buffer, buffer_max_size);
+				uint64_t new_buffer_max_size = _buffers_size_pattern[++column.buffers_size_idx];
+				column.buffer = PVCore::PVAlignedAllocator<char, BUF_ALIGN>().allocate(new_buffer_max_size);
+				column.buffer_end_ptr = column.buffer + new_buffer_max_size;
 
-				memcpy(_buffers[col], field_part2, field_part2_size);
-				_buffers_ptr[col] = _buffers[col] + field_part2_size;
+				memcpy(column.buffer, field_part2, field_part2_size);
+				column.buffer_ptr = column.buffer + field_part2_size;
 			}
 			else {
 				// Recycle previously allocated buffer
-				_buffers_ptr[col] = _buffers[col];
+				column.buffer_ptr = column.buffer;
 			}
 		}
 	}
@@ -179,10 +177,11 @@ public:
 	{
 		// WARNING: O_DIRECT option prevent to write a buffer of an arbitrary file (must be multiple of 512)
 
-		/*for (uint64_t col = 0 ; col < _num_cols; col++) {
-			uint64_t partial_buffer_size = _buffers_end_ptr[col] - _buffers_ptr[col];
-			if(!this->Write(_buffers[col], partial_buffer_size, _files[col])) {
-				PVLOG_ERROR("PVNRawDiskBackend: Error writing column %d to disk (%s)\n", col, strerror(errno));
+		/*for (uint64_t col_idx = 0 ; col_idx < _num_cols; col_idx++) {
+		 	PVColumn& column = get_col(col_idx);
+			uint64_t partial_buffer_size = column.buffer_end_ptr - column.buffer_ptr;
+			if(!this->Write(column.buffer, partial_buffer_size,  column.file)) {
+				PVLOG_ERROR("PVNRawDiskBackend: Error writing column %d to disk (%s)\n", col_idx, strerror(errno));
 				return;
 			}
 		}*/
@@ -196,20 +195,32 @@ public:
 	~PVNRawDiskBackend()
 	{
 		// Close files and deallocate all buffers
-		for (uint64_t col = 0 ; col < _num_cols; col++) {
-			this->Close(_files[col]);
-			PVCore::PVAlignedAllocator<char, BUF_ALIGN>().deallocate(_buffers[col], _buffers_size_pattern[_buffers_size_idx[col]]);
+		for (uint64_t col_idx = 0 ; col_idx < _num_cols; col_idx++) {
+			PVColumn& column = get_col(col_idx);
+			this->Close(column.file);
+			PVCore::PVAlignedAllocator<char, BUF_ALIGN>().deallocate(column.buffer, _buffers_size_pattern[column.buffers_size_idx]);
 		}
 	}
+
+private:
+	struct PVColumn
+	{
+		file_t file = 0;
+		char* buffer = nullptr;
+		char* buffer_ptr = nullptr;
+		char* buffer_end_ptr = nullptr;
+		uint64_t buffers_size_idx = 0;
+	};
+
+private:
+	inline PVColumn & get_col(uint64_t col) { return _columns[col]; }
+
 private:
 	const std::string& _nraw_folder;
 	uint64_t _num_cols;
 
-	std::vector<file_t> _files;
-	std::vector<char*> _buffers;
-	std::vector<char*> _buffers_ptr;
-	std::vector<char*> _buffers_end_ptr;
-	std::vector<uint64_t> _buffers_size_idx;
+	std::vector<PVColumn> _columns;
+
 	uint64_t _buffers_size_pattern[8] = { // too bad we need to specify array size...
 		128*1024,
 		256*1024,
