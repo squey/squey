8fc0ca9e189578ea4feee5dc642149a5686d85cf
Adrien Guinet <aguinet@picviz.com>
2012-09-14 12:00:11 +0200

    Small fix in PVLibView

--- libparallelview/src/include/pvparallelview/PVLibView.h
+++ libparallelview/src/include/pvparallelview/PVLibView.h
@@ -56,7 +56,7 @@ protected:
 private:
 	PVZonesManager                            _zones_manager;
 	PVSlidersManager_p                        _sliders_manager_p;
-	PVHive::PVObserver_p<Picviz::PVLayer>     _obs_output_layer;
+	PVHive::PVObserver_p<Picviz::PVLayer>     _obs_sel_layer;
 	PVHive::PVObserver_p<Picviz::PVView>      _obs_view;
 	views_list_t                              _parallel_scenes;
 	zoomed_scene_list_t                       _zoomed_parallel_scenes;
