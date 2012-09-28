fa871f3856cf44a83ef5098f7f97756833a52e5e
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-09-28 13:07:12 +0200

    [PVWorkspace] switch_with_central_widget working.

--- libpvguiqt/src/include/pvguiqt/PVWorkspace.h
+++ libpvguiqt/src/include/pvguiqt/PVWorkspace.h
@@ -7,23 +7,82 @@
 #ifndef __PVGUIQT_PVWORKSPACE_H__
 #define __PVGUIQT_PVWORKSPACE_H__
 
+#include <iostream>
+
 #include <QMainWindow>
 #include <QWidget>
 #include <QList>
 #include <QDockWidget>
+#include <QEvent>
+#include <QPushButton>
+#include <QHBoxLayout>
 
 namespace PVGuiQt
 {
 
+class PVDockWidget : public QDockWidget
+{
+public:
+	PVDockWidget(QWidget* parent = 0) : QDockWidget(parent) {}
+
+	void setFlag(bool f) { flag = f; }
+
+protected:
+	/*bool event(QEvent* ev)
+	{
+		if (flag) {
+			return QDockWidget::event(ev);
+		}
+
+		return false; // ?
+	}*/
+
+private:
+	bool flag = true;
+};
+
 class PVWorkspace : public QMainWindow
 {
+	Q_OBJECT;
 public:
 	PVWorkspace(QWidget* parent = 0);
 
 	void add_view_display(QWidget* view_display, const QString& name);
 
+	void switch_with_central_widget(PVDockWidget* display_dock)
+	{
+		QWidget* display = display_dock->widget();
+		display->setParent(nullptr);
+
+		QWidget* central_widget = centralWidget();
+		central_widget->setParent(display_dock);
+
+		setCentralWidget(display);
+		display_dock->setWidget(central_widget);
+	}
+
 private:
 	QList<QDockWidget*> _displays;
+};
+
+class SlotHandler : public QObject
+{
+	Q_OBJECT;
+public:
+	SlotHandler(
+		PVWorkspace* workspace,
+		PVDockWidget* dock_display_to_switch
+	) : _workspace(workspace), _dock_display(dock_display_to_switch) {}
+
+public slots:
+	void switch_displays()
+	{
+		_workspace->switch_with_central_widget(_dock_display);
+	}
+
+private:
+	PVWorkspace* _workspace;
+	PVDockWidget* _dock_display;
 };
 
 }
--- libpvguiqt/tests/workspaces.cpp
+++ libpvguiqt/tests/workspaces.cpp
@@ -4,14 +4,17 @@
  * Copyright (C) Picviz Labs 2012
  */
 
+#include <pvguiqt/PVWorkspace.h>
+
+#include <iostream>
+
 #include <QApplication>
 #include <QMainWindow>
 #include <QTabWidget>
-#include <QDockWidget>
 #include <QStyle>
 #include <QDesktopWidget>
 #include <QLabel>
-#include <QLabel>
+#include <QPushButton>
 
 class CustomMainWindow : public QMainWindow
 {
@@ -19,7 +22,7 @@ public:
 
 	CustomMainWindow()
 	{
-		setMinimumSize(500,600);
+		setMinimumSize(500, 600);
 
 		setGeometry(
 		    QStyle::alignedRect(
@@ -31,16 +34,6 @@ public:
 	}
 };
 
-class FakeMainWindow : public QMainWindow
-{
-public:
-
-	FakeMainWindow()
-	{
-
-	}
-};
-
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
@@ -49,48 +42,69 @@ int main(int argc, char** argv)
 
 	QWidget* main_widget = new QWidget();
 
-	FakeMainWindow* fake_main_window1 = new FakeMainWindow();
-	fake_main_window1->setTabPosition(Qt::TopDockWidgetArea, QTabWidget::North);
+	PVGuiQt::PVWorkspace* workspace1 = new PVGuiQt::PVWorkspace();
+	workspace1->setTabPosition(Qt::TopDockWidgetArea, QTabWidget::North);
 
-	FakeMainWindow* fake_main_window2 = new FakeMainWindow();
-	fake_main_window2->setTabPosition(Qt::TopDockWidgetArea, QTabWidget::North);
+	PVGuiQt::PVWorkspace* workspace2 = new PVGuiQt::PVWorkspace();
+	workspace2->setTabPosition(Qt::TopDockWidgetArea, QTabWidget::North);
 
 	QTabWidget* tab_widget = new QTabWidget(mw);
 	tab_widget->resize(640, 600);
 	tab_widget->setTabsClosable(true);
 
-	tab_widget->addTab(fake_main_window1, "Workspace 1");
-	tab_widget->addTab(fake_main_window2, "Workspace 2");
+	tab_widget->addTab(workspace1, "Workspace 1");
+	tab_widget->addTab(workspace2, "Workspace 2");
 
-	QDockWidget* dock_widget1 = new QDockWidget();
-	QLabel* label1 = new QLabel();
+	PVGuiQt::PVDockWidget* dock_widget1 = new PVGuiQt::PVDockWidget();
+	QLabel* label1 = new QLabel("LABEL1");
+	std::cout << "label1:" << label1 << std::endl;
 	dock_widget1->setWidget(label1);
 	dock_widget1->setWindowTitle("View 1");
-	dock_widget1->setStyleSheet("background-color: purple;");
-	QDockWidget* dock_widget2 = new QDockWidget();
+	label1->setStyleSheet("background-color: purple;");
+
+	PVGuiQt::PVDockWidget* dock_widget2 = new PVGuiQt::PVDockWidget();
 	dock_widget2->setStyleSheet("background-color: blue;");
 	QLabel* label2 = new QLabel();
 	dock_widget2->setWidget(label2);
 	dock_widget2->setWindowTitle("View 2");
-	QDockWidget* dock_widget3 = new QDockWidget();
+
+	PVGuiQt::PVDockWidget* dock_widget3 = new PVGuiQt::PVDockWidget();
 	dock_widget3->setStyleSheet("background-color: green;");
 	QLabel* label3 = new QLabel();
 	dock_widget3->setWidget(label3);
 	dock_widget3->setWindowTitle("View 3");
 
-	QDockWidget* dock_widget4 = new QDockWidget();
+	PVGuiQt::PVDockWidget* dock_widget4 = new PVGuiQt::PVDockWidget();
+	dock_widget4->setStyleSheet("background-color: red;");
+	QLabel* label4 = new QLabel();
+	dock_widget4->setWidget(label4);
 	dock_widget4->setWindowTitle("View 4");
 
-	fake_main_window1->addDockWidget(Qt::TopDockWidgetArea, dock_widget1);
-	fake_main_window1->addDockWidget(Qt::TopDockWidgetArea, dock_widget2);
-	fake_main_window1->tabifyDockWidget(dock_widget1, dock_widget2);
-	fake_main_window1->addDockWidget(Qt::TopDockWidgetArea, dock_widget3);
-	fake_main_window1->tabifyDockWidget(dock_widget2, dock_widget3);
+	PVGuiQt::PVDockWidget* dock_widget5 = new PVGuiQt::PVDockWidget();
+	dock_widget5->setStyleSheet("background-color: red;");
+	QLabel* label5 = new QLabel();
+	QLabel* label6 = new QLabel("Label6");
+	dock_widget5->setWidget(label5);
+	dock_widget5->setWidget(label6);
+	dock_widget5->setWindowTitle("View 5");
 
-	dock_widget1->raise(); // Set active the specified QDockWidget
+	workspace1->addDockWidget(Qt::TopDockWidgetArea, dock_widget1);
+	workspace1->addDockWidget(Qt::TopDockWidgetArea, dock_widget2);
+	workspace1->tabifyDockWidget(dock_widget1, dock_widget2);
+	workspace1->addDockWidget(Qt::TopDockWidgetArea, dock_widget3);
+	workspace1->tabifyDockWidget(dock_widget2, dock_widget3);
+	workspace1->addDockWidget(Qt::TopDockWidgetArea, dock_widget4);
+	workspace1->tabifyDockWidget(dock_widget3, dock_widget4);
 
+	dock_widget1->raise(); // Set active the specified PVGuiQt::PVDockWidget
 
-	fake_main_window2->addDockWidget(Qt::TopDockWidgetArea, dock_widget4);
+	QPushButton* button = new QPushButton("set view1 to central widget");
+	workspace1->setCentralWidget(button);
+
+	PVGuiQt::SlotHandler slot_handler(workspace1, dock_widget1);
+	QObject::connect(button, SIGNAL(clicked()), &slot_handler, SLOT(switch_displays()));
+
+	workspace2->addDockWidget(Qt::TopDockWidgetArea, dock_widget5);
 
 	mw->show();
 
