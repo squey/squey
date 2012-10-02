6d56ea1d77f3a9bca80c8b08a85055e84cd83bf9
Adrien Guinet <aguinet@picviz.com>
2012-10-02 10:45:41 +0200

    Forgotten waxes files

---
+++ libpvhive/src/include/pvhive/waxes/picviz/PVMapped.h
@@ -0,0 +1,12 @@
+#ifndef PVHIVE_WAXES_PVMAPPED_PVVIEW_H
+#define PVHIVE_WAXES_PVMAPPED_PVVIEW_H
+
+#include <pvhive/PVWax.h>
+#include <picviz/PVMapped.h>
+
+// Plotted updating waxes
+//
+
+DECLARE_WAX(Picviz::PVMapped::process_from_parent_source)
+
+#endif
