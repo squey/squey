a87718c732b3f717400489ec78134c980e809fcf
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-11-21 09:33:30 +0100

    added AUT registering.

--- squish/run_sample.sh
+++ squish/run_sample.sh
@@ -1,4 +1,7 @@
-export $SQUISH_DIR=`pwd`
-export SQUISH_SCRIPT_DIR=$SQUISH_DIR/scripts
-cd $SQUISH_DIR/test_suites
+#!/bin/bash
+export SQUISH_DIR=`pwd`
+export SQUISH_SCRIPT_DIR="$SQUISH_DIR/scripts"
+squishserver --config addAUT picviz-inspector.sh "`pwd`/.." 
+cd "$SQUISH_DIR/test_suites"
+squishserver --daemon
 squishrunner --testsuite suite_test_suite_1 --testcase tst_correlations_menu
