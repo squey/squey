92e97e8614669f22c0719dfc66c7335a451a9d2e
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-11-21 14:44:49 +0100

    Added CMake scripts and changed folder tree.

--- squish/scripts/common-vars.py
+++
@@ -1,4 +0,0 @@
-import os
-
-SQUISH_DIR = os.environ.get("SQUISH_DIR")
-SQUISH_FILES = os.path.join(os.environ.get("SQUISH_DIR"), "files")
---
+++ tests/squish/scripts/common-vars.py
@@ -0,0 +1,4 @@
+import os
+
+SQUISH_DIR = os.environ.get("SQUISH_DIR")
+SQUISH_FILES = os.path.join(os.environ.get("INSPECTOR_TESTS_DIR"), "files")
