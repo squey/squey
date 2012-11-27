554f4df5ee2b1b152a0d55852400c8c4d6a0ca90
Rémi Hérilier <rherilier@picviz.com>
2012-11-27 10:12:37 +0100

    [PVKernel] enhance test program const_fwd

--- libpvkernel/tests/core/const_fwd.cpp
+++ libpvkernel/tests/core/const_fwd.cpp
@@ -1,5 +1,8 @@
+
 #include <pvkernel/core/PVTypeTraits.h>
 #include <iostream>
+
+#include <pvkernel/core/picviz_assert.h>
 
 struct A
 {
@@ -9,14 +12,61 @@ struct A
 
 class B { };
 
+
+template <typename T>
+struct defined_with_const
+{
+	constexpr static bool value = false;
+};
+
+template <typename T>
+struct defined_with_const<const T>
+{
+	constexpr static bool value = true;
+};
+
+template <typename T>
+struct defined_with_const<const T*>
+{
+	constexpr static bool value = true;
+};
+
+template <typename T>
+struct defined_with_const<const T&>
+{
+	constexpr static bool value = true;
+};
+
+template <typename T>
+bool is_const(T&)
+{
+	return defined_with_const<T>::value;
+}
+
 int main()
 {
+	A a;
+
 	typename PVCore::PVTypeTraits::const_fwd<A, B>::type gna;
 	typename PVCore::PVTypeTraits::const_fwd<A, const B>::type gna2;
-	typename PVCore::PVTypeTraits::const_fwd<void, const B>::type* gna4;
 
-	std::cout << gna.is_const() << std::endl;
-	std::cout << gna2.is_const() << std::endl;
+	typename PVCore::PVTypeTraits::const_fwd<void, B>::type* gna4;
+	typename PVCore::PVTypeTraits::const_fwd<void, const B>::type* gna8;
+
+	typename PVCore::PVTypeTraits::const_fwd<A, B>::type& gnaa = a;
+	typename PVCore::PVTypeTraits::const_fwd<A, const B>::type& gna10 = a;
+
+	PV_VALID_P(gna.is_const(), false);
+
+	PV_VALID_P(gna2.is_const(), true);
+
+	PV_VALID_P(is_const(gna4), false);
+
+	PV_VALID_P(is_const(gna8), true);
+
+	PV_VALID_P(is_const(gnaa), false);
+
+	PV_VALID_P(is_const(gna10), true);
 
 	return 0;
 }
