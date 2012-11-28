9bf2c535b531f4109c219ba51f60a91de8dfa3a7
Adrien Guinet <aguinet@picviz.com>
2012-11-28 16:00:25 +0100

    Test files in SVN + script that checks the SVN revision

--- tests/check-files-svn-rev.sh
+++ tests/check-files-svn-rev.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #
 
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 test-files-root rev-file" 1>&2
+	exit 1
+fi
+
 SCRIPT_DIR=$(/usr/bin/realpath $(dirname $0))
-FILES_DIR="$SCRIPT_DIR/files"
-FILE_REV="$SCRIPT_DIR/files-svn-rev"
+FILES_DIR=$1
+FILE_REV=$2
 
 if [ ! -f "$FILE_REV" ]; then
 	echo "File $FILE_REV does not exist." 1>&2
@@ -26,7 +31,7 @@ fi
 CMP_REV=$(cat "$FILE_REV")
 
 if [ $REV -ne $CMP_REV ]; then
-	echo "Test files are at the revision $REV, and $CMP_REV is needed (as specified by $FILE_REV). Please update yoru svn copy according to this revision." 1>&2
+	echo "Test files are at the revision $REV, and $CMP_REV is needed (as specified by $FILE_REV). Please update your SVN repository according to this revision." 1>&2
 	exit 1
 fi
 
