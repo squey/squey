fbe1d563acc47f570198523324a4241b0cd97606
Rémi Hérilier <rherilier@picviz.com>
2012-12-04 18:04:50 +0100

    [all] some changes for test programs
    libpvkernel/test/filter/
      Tfilter_filter_library is back (the API had changed)
      Tfilter_shared_ptr has a new comment... FIXME O:-)
      test-env.h did not distinguish src dir and build dir
    
    libpvparallelview/tests/
      some statistics format changes in Pquadtree-perf

--- libpvkernel/tests/filter/filter_library.cpp
+++ libpvkernel/tests/filter/filter_library.cpp
@@ -5,12 +5,16 @@
  */
 
 #include "test-env.h"
-#include <pvkernel/filter/PVFilterLibrary.h>
+//#include <pvkernel/filter/PVFilterLibrary.h>
 #include <pvkernel/filter/PVPluginsLoad.h>
 #include <pvkernel/filter/PVFieldsFilter.h>
 
 #include <iostream>
 #include <QString>
+
+#include <pvkernel/core/picviz_assert.h>
+
+typedef LIB_CLASS(PVFilter::PVFieldsFilter<PVFilter::one_to_many>) fields_filter_o2m_t;
 
 int main()
 {
@@ -18,28 +22,39 @@ int main()
 
 	PVFilter::PVPluginsLoad::load_all_plugins();
 
-	LIB_CLASS(PVFilter::PVFieldsFilter<PVFilter::one_to_many>)::list_filters const& lf = LIB_CLASS(PVFilter::PVFieldsFilter<PVFilter::one_to_many>)::get().get_list();
-	LIB_CLASS(PVFilter::PVFieldsFilter<PVFilter::one_to_many>)::list_filters::const_iterator it;
-	PVFilter::PVFieldsSplitter_p regexp = LIB_CLASS(PVFilter::PVFieldsFilter<PVFilter::one_to_many>)::get().get_filter_by_name("regexp");
-	assert(regexp);
-	{
-		std::cout << "Splitters:" << std::endl;
-		for (it = lf.begin(); it != lf.end(); it++) {
-			std::cout << qPrintable(it.key()) << std::endl;
+	fields_filter_o2m_t::list_classes const lc1 = fields_filter_o2m_t::get().get_list();
+	fields_filter_o2m_t::list_classes::const_iterator it;
+
+	std::cout << "Splitters:" << std::endl;
+	for (it = lc1.begin(); it != lc1.end(); it++) {
+		std::cout << qPrintable(it.key()) << std::endl;
+	}
+
+	fields_filter_o2m_t::list_classes::const_iterator regexp_it = lc1.find("regexp");
+	PV_ASSERT_VALID(regexp_it != lc1.end());
+
+	PVFilter::PVFieldsSplitter_p regexp = regexp_it.value();
+	PV_ASSERT_VALID(regexp.get() != nullptr);
+
+	fields_filter_o2m_t::get().register_class("test", *regexp);
+	fields_filter_o2m_t::list_classes const lc2 = fields_filter_o2m_t::get().get_list();
+	fields_filter_o2m_t::list_classes::const_iterator it2;
+
+	for (it2 = lc2.begin(); it2 != lc2.end(); it2++) {
+		if (lc1.contains(it2.key())) {
+			continue;
+		} else if (it2.key() == "test") {
+			continue;
+		} else {
+			std::cout << "where does the key '" << qPrintable(it2.key()) << "' come from" << std::endl;
+			return 1;
 		}
 	}
 
-	{
-		PVFilter::PVFilterLibrary<PVFilter::PVFieldsFilterReg>::get().register_filter<PVFilter::PVFieldsSplitter>("test", *regexp);
-		PVFilter::PVFilterLibrary<PVFilter::PVFieldsFilterReg>::list_filters const& lf = PVFilter::PVFilterLibrary<PVFilter::PVFieldsFilterReg>::get().get_list();
-		PVFilter::PVFilterLibrary<PVFilter::PVFieldsFilterReg>::list_filters::const_iterator it;
-
-		std::cout << "All fields filters:" << std::endl;
-		for (it = lf.begin(); it != lf.end(); it++) {
-			std::cout << qPrintable(it.key()) << std::endl;
-		}
+	std::cout << "All fields filters:" << std::endl;
+	for (it2 = lc2.begin(); it2 != lc2.end(); it2++) {
+		std::cout << qPrintable(it2.key()) << std::endl;
 	}
-
 
 	return 0;
 }
