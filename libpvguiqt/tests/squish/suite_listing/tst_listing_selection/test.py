a1417179bed76e2bb38bf5a13ab770ca1ceba6a9
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2012-12-07 17:09:55 +0100

    [squish] Added squish listing selection test: libpvguiqt/tests/squish/suite_listing/tst_selection_listing

---
+++ libpvguiqt/tests/squish/suite_listing/tst_listing_selection/test.py
@@ -0,0 +1,31 @@
+def test_selection():
+    type(waitForObject(":PVListingView_PVGuiQt::PVListingView"), "<Shift>")
+    waitForObjectItem(":PVListingView_PVGuiQt::PVListingView", "6/1")
+    clickItem(":PVListingView_PVGuiQt::PVListingView", "6/1", 53, 15, 33554432, Qt.LeftButton)
+    waitForObjectItem(":PVListingView_PVGuiQt::PVListingView", "19/1")
+    clickItem(":PVListingView_PVGuiQt::PVListingView", "19/1", 33, 15, 33554432, Qt.LeftButton)
+    type(waitForObject(":PVListingView_PVGuiQt::PVListingView"), "<Control>")
+    waitForObjectItem(":PVListingView_PVGuiQt::PVListingView", "27/1")
+    clickItem(":PVListingView_PVGuiQt::PVListingView", "27/1", 26, 12, 67108864, Qt.LeftButton)
+    type(waitForObject(":PVListingView_PVGuiQt::PVListingView"), "<Return>")
+    
+    # Verify lines text
+    test.vp("VP1")
+    
+    # Verify lines count
+    verticalHeader = waitForObject(":PVListingView.verticalHeader_of_PVListingView_QHeaderView")
+    test.verify(verticalHeader.count() == 21)
+    
+def test_select_all():
+    pass
+
+def main():
+    # Include common functions
+    source(findFile("scripts", "common.py"))
+    
+    # Start test
+    startApplication("Tqt_listing_model " +INSPECTOR_FILES_DIR+ "/sources/test-petit.log " +INSPECTOR_FILES_DIR+ "/formats/v5/syslog.format")
+    setWindowState(waitForObject(":_QMainWindow"), WindowState.Maximize)
+    
+    test_selection()
+    test_select_all()
