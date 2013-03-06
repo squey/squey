3f4f21abba8bed54c6acc9c87e80498a686ab7ba
Adrien Guinet <aguinet@picviz.com>
2013-03-06 10:20:38 +0100

    Fix in PVHitGraphBuffer in process_zoom_reduction Comment HitGraphDataZT

--- libpvparallelview/src/PVHitGraphBuffer.cpp
+++ libpvparallelview/src/PVHitGraphBuffer.cpp
@@ -36,7 +36,8 @@ void PVParallelView::PVHitGraphBuffer::set_zero()
 
 void PVParallelView::PVHitGraphBuffer::shift_left(int n)
 {
-	// TODO: implement
+	assert(n <= nblocks());
+	
 }
 
 void PVParallelView::PVHitGraphBuffer::shift_right(int n)
@@ -46,14 +47,14 @@ void PVParallelView::PVHitGraphBuffer::shift_right(int n)
 
 void PVParallelView::PVHitGraphBuffer::process_zoom_reduction_inplace(const float alpha)
 {
-	assert(alpha >= 0.5f && alpha <= 1.0f);
+	assert((alpha >= 0.5f) && (alpha < 1.0f));
 	const int sint = size_int();
 	for (int idx = 1; idx < sint; idx++) {
 		// AG: this must be the same "rounding" method than the one used
 		// in the CUDA kernel.
 		// TODO: this formula should be exported in a common place and used
 		// everywhere a "pixel" position is needed for a given zoom level.
-		const int new_idx = (int) (((float)idx*alpha) + 0.5f);
+		const int new_idx = (int) ((float)idx*alpha);
 		if (new_idx != idx) {
 			_buf[new_idx] += _buf[idx];
 			_buf[idx] = 0;
@@ -63,12 +64,12 @@ void PVParallelView::PVHitGraphBuffer::process_zoom_reduction_inplace(const floa
 
 void PVParallelView::PVHitGraphBuffer::process_zoom_reduction(const float alpha, uint32_t* res)
 {
-	assert(alpha >= 0.5f && alpha <= 1.0f);
+	assert((alpha >= 0.5f) && (alpha < 1.0f));
 	memset(res, 0, size_bytes());
 	res[0] = _buf[0];
 	const int sint = size_int();
 	for (int idx = 1; idx < sint; idx++) {
-		const int new_idx = (int) (((float)idx*alpha) + 0.5f);
+		const int new_idx = (int) ((float)idx*alpha);
 		res[new_idx] += _buf[idx];
 	}
 }
