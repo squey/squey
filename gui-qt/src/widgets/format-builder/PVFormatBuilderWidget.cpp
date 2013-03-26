squey/squey

commit 40b8230262a073b145d551a5d1c99fd61eef193a

gui-qt compile sw/ the new listing !

gui-qt/src/widgets/format-builder/PVFormatBuilderWidget.cpp
@@ -132,8 +132,8 @@ void PVInspector::PVFormatBuilderWidget::init(QWidget* parent)
 	vb_widget->setLayout(vb);
 	_main_tab->addTab(vb_widget, tr("Filters"));
 
-	_axes_comb_widget = new PVAxesCombinationWidget(myTreeModel->get_axes_combination());
-	_main_tab->addTab(_axes_comb_widget, tr("Axes combination"));
+	//_axes_comb_widget = new PVAxesCombinationWidget(myTreeModel->get_axes_combination());
+	//_main_tab->addTab(_axes_comb_widget, tr("Axes combination"));
 
 	main_splitter->addWidget(_main_tab);
 
@@ -949,6 +949,6 @@ void PVInspector::PVFormatBuilderWidget::slotMainTabChanged(int idx)
 
 		// Get the list of axes and update the axis combination
 		myTreeModel->updateAxesCombination();
-		_axes_comb_widget->update_all();
+		//_axes_comb_widget->update_all();
 	}
 }

CMakeLists.txt
@@ -149,11 +149,6 @@ add_subdirectory(libpvhive)
 add_subdirectory(libparallelview)
 add_subdirectory(libpvguiqt)
 
-if(USE_PVGL2)
-  add_subdirectory(libpvgl2)
-else(USE_PVGL2)
-  add_subdirectory(libpvgl)
-endif(USE_PVGL2)
 add_subdirectory(gui-qt)
 
 

gui-qt/CMakeLists.txt
@@ -13,8 +13,7 @@ option(USE_OWN_LIBCURL "We have problems with quite some distributions with libc
 #########################################
 include_directories(AFTER ${PVKERNEL_INCLUDE_DIRS})
 include_directories(AFTER ${PICVIZ_INCLUDE_DIRS})
-include_directories(AFTER ${PVGL2_INCLUDE_DIRS})
-include_directories(AFTER ${PVSDK_INCLUDE_DIRS})
+include_directories(AFTER ${PVHIVE_INCLUDE_DIRS})
 include_directories(AFTER ${PVGUIQT_INCLUDE_DIRS})
 include_directories(AFTER ${ICU_INCLUDE})
 

gui-qt/src/CMakeLists.txt
@@ -50,8 +50,8 @@ add_definitions(${PVGL_FLAGS})
 SET(MOC_HEADERS
 include/PVAxisComputationDlg.h
 include/PVAxisPropertiesWidget.h
-include/PVAxesCombinationWidget.h
-include/PVAxesCombinationDialog.h
+#include/PVAxesCombinationWidget.h
+#include/PVAxesCombinationDialog.h
 include/PVCustomStyle.h
 include/PVCustomTitleDialog.h
 include/PVExtractorWidget.h
@@ -71,9 +71,9 @@ include/PVLayerStackView.h
 include/PVLayerStackWidget.h
 include/PVListColNrawDlg.h
 include/PVListDisplayDlg.h
-include/PVListingModel.h
+#include/PVListingModel.h
 include/PVListingsTabWidget.h
-include/PVListingSortFilterProxyModel.h
+#include/PVListingSortFilterProxyModel.h
 include/PVMainWindow.h
 include/PVMappingPlottingEditDialog.h
 include/PVMapWidget.h
@@ -86,8 +86,8 @@ include/PVSimpleStringListModel.h
 include/PVStartScreenWidget.h
 include/PVStringListChooserWidget.h
 include/PVTabSplitter.h
-include/PVViewsListingView.h
-include/PVViewsListingWidget.h
+#include/PVViewsListingView.h
+#include/PVViewsiListingWidget.h
 include/PVViewsModel.h
 
 widgets/PVCheckableComboBox.h
@@ -131,8 +131,8 @@ SET(SRC_FILES
 main.cpp
 PVAxisComputationDlg.cpp
 PVAxisPropertiesWidget.cpp
-PVAxesCombinationWidget.cpp
-PVAxesCombinationDialog.cpp
+#PVAxesCombinationWidget.cpp
+#PVAxesCombinationDialog.cpp
 PVCustomStyle.cpp
 PVCustomTitleDialog.cpp
 PVFilesTypesSelWidget.cpp
@@ -151,9 +151,9 @@ PVLayerStackView.cpp
 PVLayerStackWidget.cpp
 PVListColNrawDlg.cpp
 PVListDisplayDlg.cpp
-PVListingModel.cpp
+#PVListingModel.cpp
 PVListingsTabWidget.cpp
-PVListingSortFilterProxyModel.cpp
+#PVListingSortFilterProxyModel.cpp
 PVMainWindow.cpp
 PVMainWindowMenusActions.cpp
 PVMainWindowSlots.cpp
@@ -168,8 +168,8 @@ PVSerializeOptionsWidget.cpp
 PVStartScreenWidget.cpp
 PVStringListChooserWidget.cpp
 PVTabSplitter.cpp
-PVViewsListingView.cpp
-PVViewsListingWidget.cpp
+#PVViewsListingView.cpp
+#PVViewsListingWidget.cpp
 PVViewsModel.cpp
 
 widgets/PVCheckableComboBox.cpp
@@ -249,7 +249,7 @@ if(SET_PROTECT)
 endif()
 
 set_property(TARGET picviz-inspector APPEND PROPERTY LINK_FLAGS ${GALVEZ_LDFLAGS})
-target_link_libraries(picviz-inspector ${QT_LIBRARIES} ${PVGUIQT_LIBRARY} ${PVHIVE_LIBRARY} ${PVSDK_LIBRARY} ${PICVIZ_LIBRARY} ${PVKERNEL_LIBRARY} ${PVGL_LIBRARY} ${WIN32_EXTRA_LIBS} ${TBB_LIBRARIES} ${Boost_LIBRARIES_PYTHON} ${PYTHON_LIBRARY} ${ICU_LIBRARY} ${GALVEZ_LIBS} ${OpenMP_LIBRARIES})
+target_link_libraries(picviz-inspector ${QT_LIBRARIES} ${PVGUIQT_LIBRARY} ${PVHIVE_LIBRARY} ${PICVIZ_LIBRARY} ${PVKERNEL_LIBRARY} ${WIN32_EXTRA_LIBS} ${TBB_LIBRARIES} ${Boost_LIBRARIES_PYTHON} ${PYTHON_LIBRARY} ${ICU_LIBRARY} ${GALVEZ_LIBS} ${OpenMP_LIBRARIES})
 
 INSTALL(TARGETS picviz-inspector DESTINATION .)
 

gui-qt/src/PVAxesCombinationWidget.cpp
@@ -315,6 +315,7 @@ void PVInspector::PVAxesCombinationWidget::sel_singleton_Slot()
 
 void PVInspector::PVAxesCombinationWidget::sel_range_Slot()
 {
+#if 0
 	assert(_view);
 	PVAxesCombinationWidgetSelRange* dlg = new PVAxesCombinationWidgetSelRange((QWidget*) this);
 	if (dlg->exec() != QDialog::Accepted) {
@@ -349,6 +350,7 @@ void PVInspector::PVAxesCombinationWidget::sel_range_Slot()
 		}
 	}
 	set_selection_from_cols(cols);
+#endif
 }
 
 // PVAxesCombinationWidgetSelRange implementation

gui-qt/src/PVExtractorWidget.cpp
@@ -22,8 +22,6 @@
 #include <pvkernel/core/PVProgressBox.h>
 #include <pvkernel/rush/PVRawSourceBase.h>
 
-#include <pvsdk/PVMessenger.h>
-
 #include <boost/thread.hpp>
 
 /******************************************************************************

gui-qt/src/PVLayerFilterProcessWidget.cpp
@@ -195,16 +195,11 @@ void PVInspector::PVLayerFilterProcessWidget::save_Slot()
 	_view->pre_filter_layer.get_selection() = _view->post_filter_layer.get_selection();
 	_view->process_from_filter();
 
-	// We select lines
-	// _view->output_layer.get_lines_properties().A2B_copy_restricted_by_selection_and_nelts(current_selected_layer.get_lines_properties(), _view->real_output_selection, _view->row_count);
-	/* We need to process the view from the layer_stack */
-	// _view->process_from_layer_stack();
 	/* We refresh the PVView_p */
-	_tab->get_main_window()->update_pvglview(_view, PVSDK_MESSENGER_REFRESH_SELECTION|PVSDK_MESSENGER_REFRESH_COLOR);
-	_tab->refresh_listing_Slot();
+	// FIXME: hive
 
 	// FIXME: I think this refreshes the listing too. We shall remove the refresh listing slot then
-	_tab->get_main_window()->refresh_view(_view);
+	//_tab->get_main_window()->refresh_view(_view);
 
 	// Save last used filter
 	_view->set_last_used_filter(_filter_p->registered_name());
@@ -252,8 +247,9 @@ bool PVInspector::PVLayerFilterProcessWidget::process()
 
 	// We reprocess the pipeline from the eventline stage
 	_view->process_from_eventline();
-	_tab->get_main_window()->update_pvglview(_view, PVSDK_MESSENGER_REFRESH_SELECTION|PVSDK_MESSENGER_REFRESH_COLOR);
-	_tab->refresh_listing_Slot();
+	// FIXME: hive !
+	//_tab->get_main_window()->//update_pvglview(_view, PVSDK_MESSENGER_REFRESH_SELECTION|PVSDK_MESSENGER_REFRESH_COLOR);
+	//_tab->refresh_listing_Slot();
 	_has_apply = true;
 	_args_widget->clear_args_state();
 
@@ -283,8 +279,9 @@ void PVInspector::PVLayerFilterProcessWidget::cancel_Slot()
 
 	// Update everything
 	_view->process_from_layer_stack();
-	_tab->get_main_window()->update_pvglview(_view, PVSDK_MESSENGER_REFRESH_SELECTION|PVSDK_MESSENGER_REFRESH_COLOR);
-	_tab->refresh_listing_Slot();
+	// FIXME: Hive!
+	//_tab->get_main_window()->//update_pvglview(_view, PVSDK_MESSENGER_REFRESH_SELECTION|PVSDK_MESSENGER_REFRESH_COLOR);
+	//_tab->refresh_listing_Slot();
 
 	reject();
 }

gui-qt/src/PVLayerStackDelegate.cpp
@@ -106,7 +106,8 @@ bool PVInspector::PVLayerStackDelegate::editorEvent(QEvent *event, QAbstractItem
 				/* now we reprocess from the selection */
 				lib_view->process_from_selection();
 				// We refresh the PVGLView
-				main_window->update_pvglview(lib_view, PVSDK_MESSENGER_REFRESH_Z|PVSDK_MESSENGER_REFRESH_COLOR|PVSDK_MESSENGER_REFRESH_ZOMBIES|PVSDK_MESSENGER_REFRESH_SELECTION);
+				// FIXME: Hive!
+				//main_window->update_pvglview(lib_view, PVSDK_MESSENGER_REFRESH_Z|PVSDK_MESSENGER_REFRESH_COLOR|PVSDK_MESSENGER_REFRESH_ZOMBIES|PVSDK_MESSENGER_REFRESH_SELECTION);
 				/* We must update all dynamic listing model... */
 				//temp->current_pv_view->update_row_count_in_all_dynamic_listing_model_Slot();
 				// FIXME: we should send ... well we should do something, probably!
@@ -128,7 +129,8 @@ bool PVInspector::PVLayerStackDelegate::editorEvent(QEvent *event, QAbstractItem
 		case 2:
 			if (event->type() == QEvent::MouseButtonPress) {
 				lib_view->set_layer_stack_selected_layer_index(lib_index);
-				main_window->update_pvglview(layer_stack_view->get_parent()->get_parent_tab()->get_lib_view(), PVSDK_MESSENGER_REFRESH_SELECTED_LAYER);
+				// FIXME: Hive!
+				//main_window->update_pvglview(layer_stack_view->get_parent()->get_parent_tab()->get_lib_view(), PVSDK_MESSENGER_REFRESH_SELECTED_LAYER);
 				/* We force a refresh of the layer_stack because of weird hover artifacts */
 				main_window->current_tab->get_layer_stack_model()->emit_layoutChanged();
 				return false;

commit 62b2e84a0c46162a2667edae61631d9515481e31

Closes ticket #222 : Axes combination in FormatBuilder is not working.
* Uncommented "_axes_comb_widget->update_all()" call in "PVInspector::PVFormatBuilderWidget::slotMainTabChanged". This regression was introduced in commit 40b8230262a073b145d551a5d1c99fd61eef193a "gui-qt compile sw/ the new listing !"

gui-qt/src/widgets/format-builder/PVFormatBuilderWidget.cpp
@@ -956,6 +956,6 @@ void PVInspector::PVFormatBuilderWidget::slotMainTabChanged(int idx)
 
 		// Get the list of axes and update the axis combination
 		myTreeModel->updateAxesCombination();
-		//_axes_comb_widget->update_all();
+		_axes_comb_widget->update_all();
 	}
 }