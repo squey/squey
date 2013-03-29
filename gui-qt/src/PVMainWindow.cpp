ea60ff52575dcf6493bc6bdcb121d2bf586fa9f4
Adrien Guinet <aguinet@picviz.com>
2013-03-29 11:42:02 +0100

    Fix post cherry-pick from 3.0 issues

--- gui-qt/src/PVMainWindow.cpp
+++ gui-qt/src/PVMainWindow.cpp
@@ -189,9 +189,6 @@ PVInspector::PVMainWindow::PVMainWindow(QWidget *parent):
 	css_file.close();
 	setStyleSheet(css_string);
 
-	splash.showMessage("cleaning temporary files");
-	PVRush::PVNraw::remove_unused_nraw_directories();
-
 	show();
 }
 
