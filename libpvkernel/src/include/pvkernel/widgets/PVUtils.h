4ef0d719485e65639fce2862de0ad3a54fd85789
Adrien Guinet <aguinet@picviz.com>
2013-04-04 17:00:56 +0200

    Uniformize tooltip max width

--- libpvkernel/src/include/pvkernel/widgets/PVUtils.h
+++ libpvkernel/src/include/pvkernel/widgets/PVUtils.h
@@ -10,6 +10,9 @@
 #include <QFont>
 #include <QString>
 
+
+#define PICVIZ_TOOLTIP_MAX_WIDTH 800
+
 namespace PVWidgets
 {
 
@@ -17,6 +20,10 @@ namespace PVUtils
 {
 	QString shorten_path(const QString& s, const QFont& font, uint64_t nb_px);
 	void html_word_wrap_text(QString& string, const QFont& font, uint64_t nb_px);
+
+	/*! \brief Returns the maximum tooltip width related to a widget
+	 */
+	uint32_t tooltip_max_width(QWidget* w);
 };
 
 }
