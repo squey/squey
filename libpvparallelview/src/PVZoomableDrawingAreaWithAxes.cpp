5c4e15923fea103e536ea5d331b1ac690456dcd3
Rémi Hérilier <rherilier@picviz.com>
2013-04-17 15:53:16 +0200

    [PVParallelView] better ticks drawing algorithm in PVZDAWA
    ticks *must not* depend on scene's size to have integer value at any
    scale.

--- libpvparallelview/src/PVZoomableDrawingAreaWithAxes.cpp
+++ libpvparallelview/src/PVZoomableDrawingAreaWithAxes.cpp
@@ -13,9 +13,33 @@
 #define SCALE_VALUE_OFFSET 8
 
 #define SUBTICK_RATIO 0.45
+#define SUBTICK_MINSIZE 32
 
 #define DEFAULT_HMARGIN 50
 #define DEFAULT_VMARGIN 40
+
+#define print_r(R) print_rect(R)
+#define print_rect(R) __print_rect(#R, R)
+
+template <typename R>
+void __print_rect(const char *text, const R &r)
+{
+	std::cout << text << ": "
+	          << r.x() << " " << r.y() << ", "
+	          << r.width() << " " << r.height()
+	          << std::endl;
+}
+
+#define print_s(V) print_scalar(V)
+#define print_scalar(V) __print_scalar(#V, V)
+
+template <typename V>
+void __print_scalar(const char *text, const V &v)
+{
+	std::cout << text << ": "
+	          << v
+	          << std::endl;
+}
 
 /**
  * NOTE:
@@ -157,7 +181,7 @@ void PVParallelView::PVZoomableDrawingAreaWithAxes::recompute_decorations(QPaint
 void PVParallelView::PVZoomableDrawingAreaWithAxes::draw_decorations(QPainter *painter,
                                                                      const QRectF &rect)
 {
-	draw_deco_v2(painter, rect);
+	draw_deco_v3(painter, rect);
 }
 
 /*****************************************************************************
@@ -395,6 +419,118 @@ void PVParallelView::PVZoomableDrawingAreaWithAxes::draw_deco_v2(QPainter *paint
 		screen_pos += screen_subtick_height;
 		scene_pos += scene_subtick_height;
 		++y_tick_index;
+	}
+
+	painter->restore();
+}
+
+void PVParallelView::PVZoomableDrawingAreaWithAxes::draw_deco_v3(QPainter *painter,
+                                                                 const QRectF &rect)
+{
+	int ticks_per_level = get_ticks_per_level();
+	qreal log_ticks_per_level = log(ticks_per_level);
+
+	int left = get_scene_left_margin();
+	int right = left + get_x_axis_length();
+	int top = get_scene_top_margin();
+	int bottom = top + get_y_axis_length();
+
+	QRectF rect_in_scene = map_to_scene(QRect(0, 0, 1024, 1024)).intersected(get_scene_rect());
+
+	painter->save();
+	painter->resetTransform();
+	painter->setPen(_decoration_color);
+
+	painter->drawLine(left, top, left, bottom);
+	painter->drawLine(left, bottom, right, bottom);
+
+	QFontMetrics fm = painter->fontMetrics();
+	int fm_ascent = fm.ascent();
+
+	qreal x_scale = x_zoom_to_scale(get_x_axis_zoom().get_clamped_value());
+	qreal x_level = log(rect_in_scene.width()) / log_ticks_per_level;
+	qreal scene_subtick_width = pow(ticks_per_level, (int)x_level) / ticks_per_level;
+	qreal screen_subtick_width = scene_subtick_width * x_scale;
+
+	bool need_x_subticks = (screen_subtick_width > SUBTICK_MINSIZE);
+
+	qreal scene_left = map_to_scene(QPoint(left, 0)).x();
+	int64_t x_subtick_index = ceil(scene_left / scene_subtick_width);
+	qreal scene_pos = x_subtick_index * scene_subtick_width;
+	qreal screen_pos = map_from_scene(QPointF(scene_pos, 0)).x();
+
+	// let's draw
+	while ((int)screen_pos <= right) {
+		if ((x_subtick_index % ticks_per_level) != 0) {
+			if (need_x_subticks) {
+				painter->drawLine(round(screen_pos),
+				                  bottom,
+				                  round(screen_pos),
+				                  bottom + SMALL_TICK_LENGTH);
+			}
+		} else {
+			// QString s = QString::number(scene_pos, 'f', 2);
+			QString s = get_x_value_at(scene_pos);
+			int s_len = fm.boundingRect(s).width();
+
+			painter->drawText(screen_pos - (s_len / 2),
+			                  bottom + SCALE_VALUE_OFFSET + fm_ascent,
+			                  s);
+			painter->drawLine(round(screen_pos),
+			                  bottom,
+			                  round(screen_pos),
+			                  bottom + TICK_LENGTH);
+		}
+
+		screen_pos += screen_subtick_width;
+		scene_pos += scene_subtick_width;
+		++x_subtick_index;
+	}
+
+
+	qreal y_scale = y_zoom_to_scale(get_y_axis_zoom().get_clamped_value());
+
+	qreal y_level = log(rect_in_scene.height()) / log_ticks_per_level;
+	qreal scene_subtick_height = pow(ticks_per_level, (int)y_level) / ticks_per_level;
+	qreal screen_subtick_height = scene_subtick_height * y_scale;
+
+	bool need_y_subticks = (screen_subtick_height > SUBTICK_MINSIZE);
+
+	qreal scene_top = map_to_scene(QPoint(0, top)).y();
+	int64_t y_subtick_index = ceil(scene_top / scene_subtick_height);
+
+	scene_pos = y_subtick_index * scene_subtick_height;
+	screen_pos = map_from_scene(QPointF(0, scene_pos)).y();
+
+
+	// let's draw
+	while ((int)screen_pos <= bottom) {
+		if ((y_subtick_index % ticks_per_level) != 0) {
+			if (need_y_subticks) {
+				painter->drawLine(left,
+				                  round(screen_pos),
+				                  left - SMALL_TICK_LENGTH,
+				                  round(screen_pos));
+			}
+		} else {
+			// QString s = QString::number(-scene_pos, 'f', 2);
+			QString s = get_y_value_at(-scene_pos);
+			int s_len = fm.boundingRect(s).width();
+
+			painter->drawText(left - s_len - SCALE_VALUE_OFFSET,
+			                  round(screen_pos),
+			                  s);
+
+			painter->drawLine(left,
+			                  round(screen_pos),
+			                  left - TICK_LENGTH,
+			                  round(screen_pos));
+
+		}
+
+		screen_pos += screen_subtick_height;
+		scene_pos += scene_subtick_height;
+		++y_subtick_index;
 	}
 
 	painter->restore();
--- libpvparallelview/src/include/pvparallelview/PVZoomableDrawingAreaWithAxes.h
+++ libpvparallelview/src/include/pvparallelview/PVZoomableDrawingAreaWithAxes.h
@@ -167,6 +167,7 @@ protected:
 private:
 	void draw_deco_v1(QPainter *painter, const QRectF &rect);
 	void draw_deco_v2(QPainter *painter, const QRectF &rect);
+	void draw_deco_v3(QPainter *painter, const QRectF &rect);
 
 private:
 	QColor  _decoration_color;
