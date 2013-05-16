9d07d4a39e7144fe484e9074ac41b31b996d9e76
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2013-05-16 15:15:59 +0200

    [PVScatterView] Added image transformation to get smoother animation.

--- libpvparallelview/src/include/pvparallelview/PVScatterViewImagesManager.h
+++ libpvparallelview/src/include/pvparallelview/PVScatterViewImagesManager.h
@@ -46,6 +46,48 @@ public:
 	const QImage& get_image_sel();
 	const QImage& get_image_all();
 
+public:
+	inline uint32_t last_y1_min() const { return _data_params.y1_min; }
+	inline uint32_t last_y1_max() const { return _data_params.y1_max; }
+	inline uint32_t last_y2_min() const { return _data_params.y2_min; }
+	inline uint32_t last_y2_max() const { return _data_params.y2_max; }
+	inline int last_zoom() const { return _data_params.zoom; }
+	inline double last_alpha() const { return _data_params.alpha; }
+	inline bool params_changed(
+		const uint64_t y1_min,
+		const uint64_t y1_max,
+		const uint64_t y2_min,
+		const uint64_t y2_max,
+		const int zoom,
+		const double alpha) const
+	{
+		return !(y1_min == last_y1_min() &&
+				 y1_max == last_y1_max() &&
+				 y2_min == last_y2_min() &&
+				 y2_max == last_y2_max() &&
+				 zoom == last_zoom() &&
+				 alpha == last_alpha());
+	}
+
+protected:
+	inline void set_params(
+		const uint64_t y1_min,
+		const uint64_t y1_max,
+		const uint64_t y2_min,
+		const uint64_t y2_max,
+		const int zoom,
+		const double alpha
+	)
+	{
+		_data_params.y1_min = y1_min;
+		_data_params.y1_max = y1_max;
+		_data_params.y2_min = y2_min;
+		_data_params.y2_max = y2_max;
+		_data_params.zoom = zoom;
+		_data_params.alpha = alpha;
+	}
+
+
 protected:
 	inline bool full_view() const { return (_data_params.zoom == 0) && (_data_params.alpha == 1.0); }
 
