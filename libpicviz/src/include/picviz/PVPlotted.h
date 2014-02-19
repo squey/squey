squey/squey

commit 6ffbe4bbb3bb1e10c43094eb940b2aa594e24c7f

[Picviz] code factorization for aligned rows count
PVZoneProcessing and PVPlotted duplicate code. That's finished.

libpicviz/src/include/picviz/PVPlotted.h
@@ -132,15 +132,61 @@ public:
 	// Data access
 	PVRow get_row_count() const;
 	PVCol get_column_count() const;
+
+	/**
+	 * Returns the aligned row count given a row count
+	 *
+	 * @param nrows the rows number
+	 *
+	 * @return the aligned row count corresponding to nrows
+	 */
+	static PVRow get_aligned_row_count(const PVRow nrows)
+	{
+		return ((nrows+PVROW_VECTOR_ALIGNEMENT-1)/(PVROW_VECTOR_ALIGNEMENT))*PVROW_VECTOR_ALIGNEMENT;
+	}
+
+	/**
+	 * Returns the aligned row count of this plotted
+	 *
+	 * @return the corresponding aligned row count
+	 */
 	inline PVRow get_aligned_row_count() const
 	{
-		const PVRow ret = get_row_count();
-		return ((ret+PVROW_VECTOR_ALIGNEMENT-1)/(PVROW_VECTOR_ALIGNEMENT))*PVROW_VECTOR_ALIGNEMENT;
+		return get_aligned_row_count(get_row_count());
 	}
+
 	PVSource* get_source_parent();
 	inline uint32_t const* get_column_pointer(PVCol const j) const { return &_uint_table[(size_t)j*(size_t)get_aligned_row_count()]; }
 	inline uint32_t* get_column_pointer(PVCol const j) { return &_uint_table[(size_t)j*(size_t)get_aligned_row_count()]; }
 	inline uint32_t get_value(PVRow const i, PVCol const j) const { return get_column_pointer(j)[i]; }
+
+	/**
+	 * Returns the base address of a column in a plotted's buffer
+	 *
+	 * @param plotted the plotted's base address
+	 * @param nrows the plotted's rows number
+	 * @param col the wanted column number
+	 *
+	 * @return the base address of the column
+	 */
+	static const uint32_t *get_plotted_col_addr(const uint32_t *plotted, const PVRow nrows, const PVCol col)
+	{
+		return plotted + (get_aligned_row_count(nrows) * col);
+	}
+
+	/**
+	 * Returns the base address of a column in a plotted
+	 *
+	 * @param plotted the plotted's base address
+	 * @param nrows the plotted's rows number
+	 * @param col the wanted column number
+	 *
+	 * @return the base address of the column
+	 */
+	static const uint32_t *get_plotted_col_addr(const uint_plotted_table_t &plotted, const PVRow nrows, const PVCol col)
+	{
+		return get_plotted_col_addr(&plotted.at(0), nrows, col);
+	}
 
 	void get_sub_col_minmax(plotted_sub_col_t& ret, uint32_t& min, uint32_t& max, PVSelection const& sel, PVCol col) const;
 	void get_col_minmax(PVRow& min, PVRow& max, PVSelection const& sel, PVCol col) const;

libpvparallelview/src/include/pvparallelview/PVZoneProcessing.h
@@ -25,7 +25,7 @@ public:
 		_nrows(nrows),
 		_col_a(col_a),
 		_col_b(col_b),
-		_nrows_aligned(((nrows+PVROW_VECTOR_ALIGNEMENT-1)/PVROW_VECTOR_ALIGNEMENT)*PVROW_VECTOR_ALIGNEMENT)
+		_nrows_aligned(Picviz::PVPlotted::get_aligned_row_count(nrows))
 	{ }
 
 public:
@@ -40,8 +40,15 @@ public:
 	inline PVCol& col_b() { return _col_b; }
 	inline PVRow nrows_aligned() const { return _nrows_aligned; }
 
-	inline uint32_t get_plotted_value(PVRow r, PVCol c) const { return (_plotted)[(size_t)c*(size_t)_nrows_aligned + (size_t)r]; }
-	inline uint32_t const* get_plotted_col(PVCol c) const { return &((_plotted)[(size_t)c*(size_t)_nrows_aligned]); }
+	inline uint32_t get_plotted_value(PVRow r, PVCol c) const
+	{
+		return get_plotted_col(c)[r];
+	}
+
+	inline uint32_t const* get_plotted_col(PVCol c) const
+	{
+		return Picviz::PVPlotted::get_plotted_col_addr(&_plotted.at(0), _nrows, c);
+	}
 
 	inline uint32_t const* get_plotted_col_a() const { return get_plotted_col(col_a()); }
 	inline uint32_t const* get_plotted_col_b() const { return get_plotted_col(col_b()); }

commit 742bc664895b4569a93d34b0a9d4488cc3864907

[Picviz] fix 32 bits overflow in PVPlotted
With huge datasets, parallel views high indexed zones contain garbage; stable
and "reactive" garbage, not just random'n'ghost data. In some case, the garbage
's zone pattern really look like lower indexed zones's pattern...

The plotting is stored as an huge memory block where columns are positionned
at specific addresses. To get a column's base address, a logic of aligned
addresses is used; the problem was in this aligned addresses computation...
bug introduced in commit 6ffbe4bbb3bb1e10c43094eb940b2aa594e24c7f
"[Picviz] code factorization for aligned rows count" by me...

to make things clearer, the different functions to access to columns base
address have been factorized.

One function to rule them all... muhahaha =<:-D

libpicviz/src/include/picviz/PVPlotted.h
@@ -169,8 +169,8 @@ public:
 	}
 
 	PVSource* get_source_parent();
-	inline uint32_t const* get_column_pointer(PVCol const j) const { return &_uint_table[(size_t)j*(size_t)get_aligned_row_count()]; }
-	inline uint32_t* get_column_pointer(PVCol const j) { return &_uint_table[(size_t)j*(size_t)get_aligned_row_count()]; }
+	inline uint32_t const* get_column_pointer(PVCol const j) const { return &_uint_table[get_plotted_col_offset(get_row_count(), j)]; }
+	inline uint32_t* get_column_pointer(PVCol const j) { return &_uint_table[get_plotted_col_offset(get_row_count(), j)]; }
 	inline uint32_t get_value(PVRow const i, PVCol const j) const { return get_column_pointer(j)[i]; }
 
 	/**
@@ -184,7 +184,7 @@ public:
 	 */
 	static const uint32_t *get_plotted_col_addr(const uint32_t *plotted, const PVRow nrows, const PVCol col)
 	{
-		return plotted + (get_aligned_row_count(nrows) * col);
+		return plotted + get_plotted_col_offset(nrows, col);
 	}
 
 	/**
@@ -230,6 +230,19 @@ protected:
 	virtual void child_added(PVView& child) override;
 
 	int create_table();
+
+	/**
+	 * Computes the offset between the plotting's base address and a column's base address
+	 *
+	 * @param nrows the rows number
+	 * @param colo the wanted column index
+	 *
+	 * @return the offset of the @a col in the plotted's buffer
+	 */
+	static inline size_t get_plotted_col_offset(PVRow nrows, PVCol col)
+	{
+		return (size_t)get_aligned_row_count(nrows) * (size_t)col;
+	}
 
 private:
 	PVPlotting_p _plotting;