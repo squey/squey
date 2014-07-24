42a28b4722e1af8e0900ff260a75e735be1c0fd3
Rémi Hérilier <rherilier@picviz.com>
2014-07-24 17:05:46 +0200

    [PVKernel] close ticket #298 (Fix crash at 200 000 000 lines)
    The import pipeline loads chunks as long as the source's EOF is not reached
    (since PV-I 3.2, not tested before). The mapping/plotting/views/... use the
    PICVIZ_LINES_MAX constant but not the statistics views (SV) which use the
    Nraw's internal count. So that, starting a distinct value SV will iterate over
    the NRaw (using its internal count) using a selection (which is correctly
    limited)... segfault!
    
    The ouput stage of the import pipeline now write at most PICVIZ_LINES_MAX
    events in the NRaw *and* tell the input stage it can stop loading.
    
    As there is/are still chunk(s) to process, the import does not stop immediatly
    (as when the import is interrupted by the user to load partially the source).

--- libpvkernel/src/rush/PVOutput.cpp
+++ libpvkernel/src/rush/PVOutput.cpp
@@ -12,3 +12,8 @@ void PVRush::PVOutput::operator()(PVCore::PVChunk* chunk)
 	PVLOG_WARN("(PVRush::PVOutput) default output function used !\n");
 	chunk->free();
 }
+
+void PVRush::PVOutput::set_stop_condition(bool *cond)
+{
+	_stop_cond = cond;
+}
