551a99e7ace4455c59cd7c27f3e89c5f5b51c875
Rémi Hérilier <rherilier@picviz.com>
2014-07-29 16:19:50 +0200

    [PVCore] extend QObject helpers list
    to do screenshots of views, it is useful to search for a relevant
    widget to capture: the QDocWidget around the listing. But the helper
    named PVCore::get_qobject_parent_of_type() is not suitable because it
    starts searching with the parameter's parent, not its parameter.
    
    PVCore::get_qobject_hierarchy_of_type() do the wanted job (and
    PVCore::get_qobject_parent_of_type() uses it).

--- libpvkernel/src/include/pvkernel/core/qobject_helpers.h
+++ libpvkernel/src/include/pvkernel/core/qobject_helpers.h
@@ -5,19 +5,42 @@
 
 namespace PVCore {
 
+/**
+ * Search for a widget which can be cast to T* in the family tree of a widget.
+ *
+ * @param self the widget to test
+ * @param test_self to indicate if \a self has to be checked or not
+ */
+template <typename T>
+typename std::remove_pointer<T>::type* get_qobject_hierarchy_of_type(QObject* self,
+                                                                     bool test_self = true)
+{
+	typedef typename std::remove_pointer<T>::type* pointer;
+
+	if (self == nullptr) {
+		return nullptr;
+	}
+
+	QObject* o = test_self?self:self->parent();
+	while(o) {
+		pointer o_cast = dynamic_cast<pointer>(o);
+		if (o_cast) {
+			return o_cast;
+		}
+		o = o->parent();
+	}
+	return nullptr;
+}
+
+/**
+ * Search for a widget which can be cast to T* in the ancestors of a widget.
+ *
+ * @param self the widget to test
+ */
 template <typename T>
 typename std::remove_pointer<T>::type* get_qobject_parent_of_type(QObject* self)
 {
-	typedef typename std::remove_pointer<T>::type* pointer;
-	QObject* parent = self->parent();
-	if (!parent) {
-		return nullptr;
-	}
-	pointer parent_cast = dynamic_cast<pointer>(parent);
-	if (parent_cast) {
-		return parent_cast;
-	}   
-	return get_qobject_parent_of_type<T>(parent);
+	return get_qobject_hierarchy_of_type<T>(self, false);
 }
 
 }
