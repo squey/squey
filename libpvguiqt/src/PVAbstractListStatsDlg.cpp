squey/squey

commit 27a930046cec451778b641494c6b5109a1aca844

[PVGuiQt] fix bad multiple search's initialization
In PVAbstractListStatsDlg::multiple_search(...), to parametrize the multiple
search plugins, the whole QItemSelectionModel's selected item set is used,
it also includes the "number" column's indexes which adds empty strings in
the search; but it is totally meaningless.

Now, only the selected values of the "name" column are used.

libpvguiqt/src/PVAbstractListStatsDlg.cpp
@@ -623,7 +623,7 @@ void PVGuiQt::PVAbstractListStatsDlg::multiple_search(QAction* act)
 
 	QItemSelectionModel* selection_model = _values_view->selectionModel();
 	assert(selection_model);
-	QModelIndexList list = selection_model->selection().indexes();
+	QModelIndexList list = selection_model->selectedRows(0);
 	QStringList cells;
 	for (const auto& cell : list) {
 		cells.append(cell.data().toString());

commit 264739ec50cf26c6bf68793af1d14126b375a83f

[PVGuiQt] fix i-BP's bug about empty selection when doing a "search using this values"
step to see the bug:
1/ open a stat view
2/ pick a range which produce a non empty selection
3/ do a "search for this"

The selected events set is necessarily empty.

The regression has been introduced in the commit 27a930046cec451778b641494c6b5109a1aca844
which replaces a call to
	selection_model->selection().indexes();
with a call to
	selection_model->selectedRows(0);

This seemed to be equivalent (and a little bit easier than using the
selection) but it fact it is not: in case of selection done with the mouse or
the keyboard, the two methods return the same result but if the selection is
generated from the range picker, they have different results...

Reverting the change solves the problem.

libpvguiqt/src/PVAbstractListStatsDlg.cpp
@@ -419,7 +419,7 @@ bool PVGuiQt::PVAbstractListStatsDlg::process_context_menu(QAction* act)
 
 	if (act) {
 		QStringList values;
-		for (const auto& item : _values_view->selectionModel()->selectedRows(0)) {
+		for (const auto& item : _values_view->selectionModel()->selection().indexes()) {
 			values.append(item.data().toString());
 		}
 
@@ -773,7 +773,7 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layer_with_selected_values()
 	/* as a selection is never sorted, we have to do it ourselves to have
 	 * the values in the same order than the view's one
 	 */
-	QModelIndexList indexes = _values_view->selectionModel()->selectedRows(0);
+	QModelIndexList indexes = _values_view->selectionModel()->selection().indexes();
 	qSort(indexes.begin(), indexes.end(), [](const QModelIndex& a, const QModelIndex& b) -> bool { return a.row() < b.row(); });
 
 	for(QModelIndex const & index : indexes) {
@@ -835,7 +835,7 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layers_for_selected_values()
 {
 	/* first, we have to check if there is enough free space for new layers
 	 */
-	QModelIndexList indexes = _values_view->selectionModel()->selectedRows(0);
+	QModelIndexList indexes = _values_view->selectionModel()->selection().indexes();
 
 	Picviz::PVView_sp view_sp = lib_view()->shared_from_this();
 	Picviz::PVLayerStack& ls = view_sp->get_layer_stack();