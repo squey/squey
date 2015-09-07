289cd1a62f0c4c5e83ec1af32585173832ea66ba
Pierrick Brunet <pbrunet@u.picviz>
2015-09-07 11:35:52 +0200

    Remove Useless class PVColumnIndexes
    * A typedef is enough

--- libpvkernel/src/include/pvkernel/core/PVColumnIndexes.h
+++ libpvkernel/src/include/pvkernel/core/PVColumnIndexes.h
@@ -6,25 +6,13 @@
 #ifndef __PVCORE_PVCOLUMNINDEXES_H__
 #define __PVCORE_PVCOLUMNINDEXES_H__
 
+#include <vector>
+
+#include <pvbase/types.h>
+
 namespace PVCore
 {
-
-class PVColumnIndexes {
-
-public:
-	typedef std::vector<PVCol> column_array_t;
-
-public:
-	inline PVCol operator[](size_t i) { return _indexes.at(i); }
-	inline PVCol operator[](size_t i) const { return _indexes.at(i); }
-
-	inline size_t size() const { return _indexes.size(); }
-	inline void push_back(PVCol col) { _indexes.push_back(col); }
-
-private:
-	column_array_t _indexes;
-};
-
+	using PVColumnIndexes = std::vector<PVCol>;
 }
 
 #endif // __PVCORE_PVCOLUMNINDEXES_H__
