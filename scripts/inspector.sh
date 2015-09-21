b97b688fbefddb0b4c7c73ed3a50ef5d71f4abfa
Pierrick Brunet <pbrunet@u.picviz>
2015-09-21 15:19:06 +0200

    Remove now useless files

--- scripts/inspector.sh
+++ scripts/inspector.sh
@@ -3,13 +3,9 @@
 # \file inspector.sh
 #
 # Copyright (C) Picviz Labs 2010-2012
+# Copyright (C) ESI Group INENDI 2015
 
-INSPECTOR_SOURCE_ROOT_DIR=$(/usr/bin/realpath $(dirname $0))
-ISRD=$INSPECTOR_SOURCE_ROOT_DIR
+export LD_LIBRARY_PATH=$(/usr/bin/realpath $(dirname $0)):$LD_LIBRARY_PATH
+export PATH=$(/usr/bin/realpath $(dirname $0)):$PATH
 
-PV_NOFILE=`ulimit -Hn`
-ulimit -Sn "$PV_NOFILE"
-
-cd $ISRD
-
-$ISRD/picviz-inspector
+picviz-inspector
