6deb252381691f3a4c1f65f5ebe0583e2bba07e4
Pierrick Brunet <pbrunet@u.picviz>
2015-09-28 14:30:41 +0200

    Remove OS specific bitset

--- update-copyright.sh
+++ update-copyright.sh
@@ -252,7 +252,7 @@ do
     FILENAME=`basename "$FILE"`
 
     case "$FILE" in
-	cmake/FindAPR.cmake | cmake/FindBoost.cmake | cmake/FindDUMBNET.cmake | cmake/FindHWLoc.cmake | cmake/FindICU.cmake | cmake/FindPCAP.cmake | cmake/FindPCRE.cmake | cmake/FindPkgMacros.cmake | cmake/FindTBB.cmake | cmake/FindTULIP3.cmake | libpicviz/src/include/picviz/PVBitset_gnu.h )
+	cmake/FindAPR.cmake | cmake/FindBoost.cmake | cmake/FindDUMBNET.cmake | cmake/FindHWLoc.cmake | cmake/FindICU.cmake | cmake/FindPCAP.cmake | cmake/FindPCRE.cmake | cmake/FindPkgMacros.cmake | cmake/FindTBB.cmake | cmake/FindTULIP3.cmake)
 	    # files not owned by Picviz Labs
 	    ;;
 
@@ -264,7 +264,7 @@ do
 	    # files whose format do not define any comment syntax
 	    ;;
 
-	libpicviz/src/include/picviz/PVBitset_gnu.h | libpvkernel/plugins/input_types/database/mysql_types.h | */uchardetect/* )
+	libpvkernel/plugins/input_types/database/mysql_types.h | */uchardetect/* )
 	    # files to ignore
 	    ;;
 
