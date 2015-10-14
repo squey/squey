5ce13aa78f11771312dbae124b3c783d5837985c
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2015-10-14 10:22:44 +0200

    [CMake] Find PVCop library

---
+++ cmake/FindPVCOP.cmake
@@ -0,0 +1,26 @@
+# - Try to find libpvcop
+# Once done this will define
+#  PVCOP_FOUND - System has PVCOP
+#  PVCOP_INCLUDE_DIRS - The PVCOP include directories
+#  PVCOP_LIBRARIES - The libraries needed to use PVCOP
+
+find_package(PkgConfig)
+
+find_path(PVCOP_INCLUDE_DIR pvcop.h
+          PATH_SUFFIXES pvcop)
+
+find_library(PVCOP_LIBRARY NAMES pvcop libpvcop)
+
+set(PVCOP_LIBRARIES ${PVCOP_LIBRARY} )
+set(PVCOP_INCLUDE_DIRS ${PVCOP_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set PVCOP_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(PVCOP  DEFAULT_MSG
+                                  PVCOP_LIBRARY PVCOP_INCLUDE_DIR)
+
+message(STATUS "PVCOP include dirs: ${PVCOP_INCLUDE_DIRS}")
+message(STATUS "PVCOP libraries: ${PVCOP_LIBRARIES}")
+
+mark_as_advanced(PVCOP_INCLUDE_DIR PVCOP_LIBRARY)
