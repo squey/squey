fd12c43a6e1ff75507a85fe40dd840e8a29b88ae
Pierrick Brunet <pbrunet@u.picviz>
2015-10-26 11:27:54 +0100

    [StatListing] Code factorization for ProxyModel
    * Add function separator
    * Merge StringSortProcyModel and PVSortFilterProxyModel (no other inheritance)
    * Clean up includes to keep only required ones
    * Remove ProxyModel_impl as it is not use anymore
    * Avoid double filtering on reverse sort
    * Remove reset to default ordering functions
    
    Commit done after ListingView rework as inheritance is not required anymore

--- libpvguiqt/src/PVStringSortProxyModel.cpp
+++ libpvguiqt/src/PVStringSortProxyModel.cpp
@@ -1,35 +1,419 @@
-
-#include <pvkernel/core/PVLogger.h>
+/**
+ * \file PVStringSortProxyModel.cpp
+ *
+ * Copyright (C) Picviz Labs 2010-2012
+ */
 
 #include <picviz/PVDefaultSortingFunc.h>
-
 #include <pvguiqt/PVStringSortProxyModel.h>
+#include <pvkernel/core/PVProgressBox.h>
+#include <pvkernel/core/picviz_bench.h>
 
+#include <algorithm>
+#include <cassert>
+
+#include <tbb/tick_count.h>
 #include <tbb/parallel_sort.h>
 
-PVGuiQt::PVStringSortProxyModel::PVStringSortProxyModel(QTableView* view, QObject* parent) :
-	PVSortFilterProxyModel(view, parent)
+#include <QHeaderView>
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::PVStringSortProxyModel
+ *
+ *****************************************************************************/
+
+PVGuiQt::PVStringSortProxyModel::PVStringSortProxyModel(QTableView* view, QObject* parent):
+	QAbstractProxyModel(parent),
+	_qt_lesser_f(Picviz::PVDefaultSortingFunc().qt_f_lesser()),
+	_view(view),
+	_sort_idx(-1)
 {
-	set_default_qt_order_func();
 }
 
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::init_default_sort
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::init_default_sort()
+{
+	BENCH_START(b);
 
+	int row_count = sourceModel()->rowCount();
+
+	_vec_sort_m2s.resize(row_count);
+	std::iota(_vec_sort_m2s.begin(), _vec_sort_m2s.end(), 0);
+
+	BENCH_END(b, "PVStringSortProxyModel::init_default_sort", 1, 1, row_count, sizeof(int));
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::__reverse_sort_order
+ *
+ *****************************************************************************/
+bool PVGuiQt::PVStringSortProxyModel::__reverse_sort_order(tbb::task_group_context* ctxt)
+{
+	// We have to be invariant about this variable, which may be modified by __do_sort.
+	int sort_idx = _sort_idx;
+	__do_sort(_sort_idx, (Qt::SortOrder) !_cur_order, ctxt);
+	bool changed = !ctxt->is_group_execution_cancelled();
+	if (changed) {
+		_sort_idx = sort_idx;
+	}
+
+	return changed;
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::reverse_sort_order
+ *
+ *****************************************************************************/
+bool PVGuiQt::PVStringSortProxyModel::reverse_sort_order()
+{
+	// In-place reverse of our first cache
+	QWidget* parent_ = dynamic_cast<QWidget*>(QObject::parent());
+	PVCore::PVProgressBox* box = new PVCore::PVProgressBox(tr("Reverse sorting order..."), parent_);
+	box->set_enable_cancel(true);
+	tbb::task_group_context ctxt;
+	bool changed = PVCore::PVProgressBox::progress([&](){ __reverse_sort_order(&ctxt); }, ctxt, box);
+
+	return changed;
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::do_filter
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::do_filter()
+{
+	BENCH_START(b);
+	vec_indexes_t tmp;
+	filter_source_indexes(_vec_sort_m2s, tmp);
+	if (tmp.size() == _vec_filtered_m2s.size()) {
+		emit layoutAboutToBeChanged();
+		_vec_filtered_m2s = tmp;
+		emit layoutChanged();
+	}
+	else {
+		beginResetModel();
+		_vec_filtered_m2s = tmp;
+		endResetModel();
+	}
+	BENCH_END(b, "PVStringSortProxyModel::do_filter", 1, 1, 1, 1);
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::__do_sort
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::__do_sort(int column, Qt::SortOrder order, tbb::task_group_context* ctxt)
+{
+	tbb::tick_count start = tbb::tick_count::now();
+	init_default_sort();
+	sort_indexes(column, order, _vec_sort_m2s, ctxt);
+	tbb::tick_count end = tbb::tick_count::now();
+	PVLOG_INFO("Sorting took %0.4f seconds.\n", (end-start).seconds());
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::do_sort
+ *
+ *****************************************************************************/
+bool PVGuiQt::PVStringSortProxyModel::do_sort(int column, Qt::SortOrder order)
+{
+	assert(column >= 0 && column < sourceModel()->columnCount());
+	QWidget* parent_ = dynamic_cast<QWidget*>(QObject::parent());
+	PVCore::PVProgressBox* box = new PVCore::PVProgressBox(tr("Sorting..."), parent_);
+	box->set_enable_cancel(true);
+	tbb::task_group_context ctxt;
+	bool changed = PVCore::PVProgressBox::progress([&](){ __do_sort(column, order, &ctxt);}, ctxt, box);
+
+	return changed;
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::sort
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::sort(int column, Qt::SortOrder order)
+{
+	bool changed = false;
+
+	// This path could be call only from Qt (not checked)
+	if (column == -1) {
+		init_default_sort();
+		do_filter();
+		return;
+	}
+
+	if (_sort_idx == column && _cur_order != order) {
+		changed = reverse_sort_order();
+	}
+	else {
+		changed = do_sort(column, order);
+	}
+
+	if (changed) {
+		do_filter();
+		_sort_idx = column;
+		_cur_order = order;
+	} else {
+		_view->horizontalHeader()->blockSignals(true);
+		_view->horizontalHeader()->setSortIndicator(_sort_idx, _cur_order);
+		_view->horizontalHeader()->blockSignals(false);
+	}
+
+	_view->horizontalHeader()->setSortIndicatorShown(true);
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::reprocess_source
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::reprocess_source()
+{
+	init_default_sort();
+
+	do_filter();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::invalidate_sort
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::invalidate_sort()
+{
+	// Force a sort if suitable
+	if (_sort_idx >= 0 && _sort_idx < sourceModel()->columnCount()) {
+		do_sort(_sort_idx, _cur_order);
+		do_filter();
+	}
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::invalidate_filter
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::invalidate_filter()
+{
+	// Force a computation of the filter
+	do_filter();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::invalidate_all
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::invalidate_all()
+{
+	// Force a sort if suitable
+	if (sourceModel() != NULL && sourceModel()->rowCount() != (int)_vec_sort_m2s.size()) {
+		// Size has changed, recreate a default sort array.
+		int sort_idx = _sort_idx;
+		init_default_sort();
+		_sort_idx = sort_idx;
+	}
+	if (_sort_idx >= 0 && _sort_idx < sourceModel()->columnCount()) {
+		do_sort(_sort_idx, _cur_order);
+	}
+	// And reprocess the filter everytime
+	do_filter();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::mapFromSource
+ *
+ *****************************************************************************/
+QModelIndex PVGuiQt::PVStringSortProxyModel::mapFromSource(QModelIndex const& src_idx) const
+{
+	// source to proxy
+	if (!src_idx.isValid()) {
+		return QModelIndex();
+	}
+
+	// Reverse search of the original index to save in the source index
+	int search_src_row = src_idx.row();
+	for (int proxy_row = 0; proxy_row < (int)_vec_filtered_m2s.size(); proxy_row++) {
+		int src_row = _vec_filtered_m2s[proxy_row];
+		if (src_row == search_src_row) {
+			return index(proxy_row, src_idx.column(), QModelIndex());
+		}
+	}
+
+	return QModelIndex();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::mapToSource
+ *
+ *****************************************************************************/
+QModelIndex PVGuiQt::PVStringSortProxyModel::mapToSource(QModelIndex const& src_idx) const
+{
+	// proxy to source
+	if (!src_idx.isValid()) {
+		return QModelIndex();
+	}
+
+	if (src_idx.row() == 0 && _vec_filtered_m2s.size() == 0) {
+		// Special case where no lines are displayed but we still want header information !!
+		// This function is called by QAbstractProxyModel::headerData to find out the good column.
+		// TODO: we should use the axis combination in the proxy, and not in the model.
+		return sourceModel()->index(0, src_idx.column(), QModelIndex());
+	}
+
+	if (src_idx.row() >= (int)_vec_filtered_m2s.size()) {
+		return QModelIndex();
+	}
+
+	return sourceModel()->index(_vec_filtered_m2s[src_idx.row()], src_idx.column(), QModelIndex());
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::setSourceModel
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::setSourceModel(QAbstractItemModel* model)
+{
+	beginResetModel();
+
+	QAbstractItemModel* prev_model = sourceModel();
+	if (prev_model) {
+		disconnect(prev_model, SIGNAL(layoutAboutToBeChanged()), this, SLOT(src_layout_about_changed()));
+		disconnect(prev_model, SIGNAL(layoutChanged()), this, SLOT(src_layout_changed()));
+		disconnect(prev_model, SIGNAL(modelAboutToBeReset()), this, SLOT(src_model_about_reset()));
+		disconnect(prev_model, SIGNAL(modelReset()), this, SLOT(src_model_reset()));
+	}
+
+	QAbstractProxyModel::setSourceModel(model);
+	init_default_sort();
+	do_filter();
+
+	connect(model, SIGNAL(layoutAboutToBeChanged()), this, SLOT(src_layout_about_changed()));
+	connect(model, SIGNAL(layoutChanged()), this, SLOT(src_layout_changed()));
+	connect(model, SIGNAL(modelAboutToBeReset()), this,SLOT(src_model_about_reset()));
+	connect(model, SIGNAL(modelReset()), this, SLOT(src_model_reset()));
+
+	endResetModel();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::rowCount
+ *
+ *****************************************************************************/
+int PVGuiQt::PVStringSortProxyModel::rowCount(const QModelIndex& parent) const
+{
+	if (parent.isValid()) {
+		return 0;
+	}
+	return _vec_filtered_m2s.size();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::columnCount
+ *
+ *****************************************************************************/
+int PVGuiQt::PVStringSortProxyModel::columnCount(const QModelIndex& parent) const
+{
+	return sourceModel()->columnCount(parent);
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::index
+ *
+ *****************************************************************************/
+QModelIndex PVGuiQt::PVStringSortProxyModel::index(int row, int col, const QModelIndex&) const
+{
+	if (parent.isValid()) {
+		return QModelIndex();
+	}
+
+	return createIndex(row, col);
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::parent
+ *
+ *****************************************************************************/
+QModelIndex PVGuiQt::PVStringSortProxyModel::parent(const QModelIndex& /*idx*/) const
+{
+	return QModelIndex();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::src_layout_about_changed
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::src_layout_about_changed()
+{
+	emit layoutAboutToBeChanged();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::src_layout_changed
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::src_layout_changed()
+{
+	reprocess_source();
+	emit layoutChanged();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::src_model_about_reset
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::src_model_about_reset()
+{
+	beginResetModel();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::src_model_reset
+ *
+ *****************************************************************************/
+void PVGuiQt::PVStringSortProxyModel::src_model_reset()
+{
+	reprocess_source();
+	endResetModel();
+}
+
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::set_qt_order_func
+ *
+ *****************************************************************************/
 void PVGuiQt::PVStringSortProxyModel::set_qt_order_func(const Picviz::PVQtSortingFunc_flesser& lt_t)
 {
 	_qt_lesser_f = lt_t;
 }
 
-void PVGuiQt::PVStringSortProxyModel::set_default_qt_order_func()
-{
-	Picviz::PVDefaultSortingFunc sf;
-	set_qt_order_func(sf.qt_f_lesser());
-}
-
-bool PVGuiQt::PVStringSortProxyModel::is_equal(const QModelIndex& left, const QModelIndex& right) const
-{
-	return sourceModel()->data(left).toString() == sourceModel()->data(right).toString();
-}
-
+/******************************************************************************
+ *
+ * PVGuiQt::PVStringSortProxyModel::sort_indexes
+ *
+ *****************************************************************************/
 void PVGuiQt::PVStringSortProxyModel::sort_indexes(int column, Qt::SortOrder order, vec_indexes_t& vec_idxes, tbb::task_group_context* /*ctxt*/)
 {
 	switch (column) {
--- libpvguiqt/src/include/pvguiqt/PVStringSortProxyModel.h
+++ libpvguiqt/src/include/pvguiqt/PVStringSortProxyModel.h
@@ -1,22 +1,38 @@
 /**
- * \file PVStringSortProxyModel.h
+ * \file PVSortFilterProxyModel.h
  *
  * Copyright (C) Picviz Labs 2010-2012
  */
 
-#ifndef PVGUIQT_PVSTRINGSORTPROXYMODEL_H
-#define PVGUIQT_PVSTRINGSORTPROXYMODEL_H
+#ifndef PVGUIQT_PVFILTERPROXYMODEL_H
+#define PVGUIQT_PVFILTERPROXYMODEL_H
 
+#include <QAbstractProxyModel>
+#include <QModelIndex>
+#include <QVector>
+#include <QTableView>
+
+#include <pvkernel/core/PVSharedBuffer.h>
 #include <picviz/PVSortingFunc.h>
 
-#include <pvguiqt/PVSortFilterProxyModel.h>
+#include <boost/date_time/time_duration.hpp>
+
+#include "tbb/task.h"
 
 namespace PVGuiQt {
 
-class PVStringSortProxyModel: public PVSortFilterProxyModel
+class PVStringSortProxyModel: public QAbstractProxyModel
 {
+	Q_OBJECT
+
 public:
+	typedef QVector<int> vec_indexes_t;
+
+public:
+	// Public interface
 	PVStringSortProxyModel(QTableView* view, QObject* parent = nullptr);
+
+	inline vec_indexes_t const& get_proxy_indexes() const { return _vec_filtered_m2s; }
 
 	/**
 	 * RH: the parameter has no constness because GCC seems to have a
@@ -26,16 +42,71 @@ public:
 
 	void set_default_qt_order_func();
 
+	// Helper functions for derived classes
 protected:
-	bool less_than(const QModelIndex& /*left*/, const QModelIndex& /*right*/) const override { return false; }
+	void invalidate_sort();
+	void invalidate_filter();
+	void invalidate_all();
 
-	bool is_equal(const QModelIndex& left, const QModelIndex &right) const override;
+	void sort_indexes(int column, Qt::SortOrder order, vec_indexes_t& vec_idxes, tbb::task_group_context* ctxt);
+	void filter_source_indexes(vec_indexes_t const& src_idxes_in, vec_indexes_t& src_idxes_out)
+	{
+		src_idxes_out = src_idxes_in;
+	}
 
-	void sort_indexes(int column, Qt::SortOrder order, vec_indexes_t& vec_idxes, tbb::task_group_context* ctxt = NULL) override;
-	void filter_source_indexes(vec_indexes_t const& src_idxes_in, vec_indexes_t& src_idxes_out) override { src_idxes_out = src_idxes_in; }
+	// Function from QAbstractProxyModel to implement
+public:
+	QModelIndex mapFromSource(QModelIndex const& src_idx) const override;
+	QModelIndex mapToSource(QModelIndex const& proxy_idx) const override;
+	void setSourceModel(QAbstractItemModel* midel) override;
+
+	// Function from QAbstractItemModel to implement
+public:
+	/** Sort the model content on the given column
+	 * 
+	 * @param[in] column : Column to use for sorting
+	 * @param[in] order : Sorting order to use
+	 *
+	 * @note : This function is a Qt required function to make sortFilter work
+	 */
+	void sort(int column, Qt::SortOrder order) override;
+	int rowCount(const QModelIndex& parent) const override;
+	int columnCount(const QModelIndex& parent) const override;
+	QModelIndex index(int row, int col, const QModelIndex&) const override;
+	QModelIndex parent(const QModelIndex& idx) const override;
+
+signals:
+	void sort_cancelled_for_column(int column);
 
 private:
+	bool reverse_sort_order();
+	bool do_sort(int column, Qt::SortOrder order);
+	void do_filter();
+	void init_default_sort();
+	void reprocess_source();
+	void __do_sort(int column, Qt::SortOrder order, tbb::task_group_context* ctxt);
+	bool __reverse_sort_order(tbb::task_group_context* ctxt);
 	mutable Picviz::PVQtSortingFunc_flesser _qt_lesser_f;
+
+private slots:
+	void src_layout_about_changed();
+	void src_layout_changed();
+	void src_model_about_reset();
+	void src_model_reset();
+
+protected:
+	/* View and model are fusionned here because Qt require a sort methods with
+	 * a specific signature. Because of this constraint, we can't return the
+	 * result of the sort function (success or fail) and we have to handle the
+	 * view modification in the sort function.
+	 */
+	QTableView* _view; //!< View representation of the model.
+
+private:
+	vec_indexes_t _vec_sort_m2s; //!< map-to-source indexes after sorting
+	vec_indexes_t _vec_filtered_m2s; //!< map-to-source indexes after filtering
+	int _sort_idx; //!< Index of the column used to perform sorting
+	Qt::SortOrder _cur_order; //!< Sorting ordering
 };
 
 }
