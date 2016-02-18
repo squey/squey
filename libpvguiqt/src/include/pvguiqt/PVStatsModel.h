squey/squey

commit d0c0d2fd1f9dd2b3257067be21920e1ba678b7ad

Make sure stats export keep sorting order.

libpvguiqt/src/include/pvguiqt/PVStatsModel.h
@@ -77,7 +77,7 @@ public:
 		value.replace(quote, escaped_quote);
 		value = quote + value + quote + sep;
 
-		double occurence_count = QString::fromStdString(_col2.at(row)).toDouble();
+		double occurence_count = QString::fromStdString(_col2.at(row_pos_to_index(row))).toDouble();
 
 		double ratio = occurence_count / max_count();
 		switch(_format) {

commit 335d2feb29343e90cc76e0990582ca07722ef5b7

[PVGuiQt] fix bug when copying to clipboard in PVStatsModel
The copied values are correct but not their related statistics.

The bug has been introduced in commit d0c0d2fd1f9dd2b3257067be21920e1ba678b7ad
which does not use the same index to read data from the two pvcop::db::array.

The problem comes from the fact that the method PVStatsModel::export_line(...)
has to be used with indices in the PVAbstractTableModel internal selection
and not with indices of cells displayed in the QAbstractItemView. The use of
the method PVAbstractTableModel::row_pos_to_index(...) was also erroneous.

libpvguiqt/src/include/pvguiqt/PVStatsModel.h
@@ -82,7 +82,7 @@ public:
 		value.replace(quote, escaped_quote);
 		value = quote + value + quote;
 
-		double occurence_count = QString::fromStdString(_col2.at(row_pos_to_index(row))).toDouble();
+		double occurence_count = QString::fromStdString(_col2.at(row)).toDouble();
 
 		double ratio = occurence_count / max_count();
 		if ((_format & ValueFormat::Count) == ValueFormat::Count) {