2aec2018c4041536cb10dda3b1afc294e9344905
Pierrick Brunet <pbrunet@u.picviz>
2016-03-01 10:16:41 +0100

    Do not create Unused folder.

--- libinendi/tests/export_selection_to_mineset.cpp
+++ libinendi/tests/export_selection_to_mineset.cpp
@@ -17,11 +17,6 @@ int main(int argc, char** argv)
 
 	pvtest::TestEnv env(argv[1], argv[2]);
 
-	QDir nraw_dir(QString::fromStdString(PVRush::PVNraw::default_tmp_path));
-	if (!nraw_dir.exists()){
-		nraw_dir.mkdir(QString::fromStdString(PVRush::PVNraw::default_tmp_path));
-	}
-
 	std::string dataset_url = Inendi::PVMineset::import_dataset(*env.view);
 	Inendi::PVMineset::delete_dataset(dataset_url);
 }
