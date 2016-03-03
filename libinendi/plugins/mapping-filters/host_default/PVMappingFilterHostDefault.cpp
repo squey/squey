060f2b64c0675933b8d2adf984e0333e867a0ebc
Pierrick Brunet <pbrunet@u.picviz>
2016-03-03 10:17:38 +0100

    Improve host_default mapping.
    * Readability only and add a test.

--- libinendi/plugins/mapping-filters/host_default/PVMappingFilterHostDefault.cpp
+++ libinendi/plugins/mapping-filters/host_default/PVMappingFilterHostDefault.cpp
@@ -2,108 +2,58 @@
  * @file
  *
  * @copyright (C) Picviz Labs 2010-March 2015
- * @copyright (C) ESI Group INENDI April 2015-2015
+ * @copyright (C) ESI Group INENDI April 2015-2016
  */
 
 #include "PVMappingFilterHostDefault.h"
-#include <inendi/limits.h>
-#include <pvkernel/core/PVUnicodeString.h>
-#include <pvkernel/core/PVUnicodeString16.h>
-#include <pvkernel/core/PVStringUtils.h>
+
 #include <pvkernel/core/network.h>
 
-#include <QVector>
-#include <QByteArray>
-
 #include <algorithm>
-#include <tbb/concurrent_vector.h>
 
 #include <pvkernel/core/dumbnet.h>
 
-static uint32_t compute_str16_factor(PVCore::PVUnicodeString16 const& str)
+/**
+ * Return a values which sort strings based on theirs 4 first chars.
+ */
+static uint32_t compute_str_factor(const char* str, size_t len)
 {
-	char b1_c = 0;
-	char b2_c = 0;
-	char b3_c = 0;
-	char b4_c = 0;
-
-	const size_t len = str.len();
-	PVCore::PVUnicodeString16::utf_char const* const buf = str.buffer();
-	if (len >= 1) {
-		b1_c = QChar(buf[0]).toLatin1();
-		if (len >= 2) {
-			b2_c = QChar(buf[1]).toLatin1();
-			if (len >= 3) {
-				b3_c = QChar(buf[2]).toLatin1();
-				if (len >= 4) {
-					b4_c = QChar(buf[3]).toLatin1();
-				}
-			}
-		}
+	uint32_t res = *reinterpret_cast<const uint32_t*>(str);
+	switch(len) {
+		case 0:
+			return 0;
+		case 1:
+			return res & 0xFF000000;
+		case 2:
+			return res & 0xFFFF0000;
+		case 3:
+			return res & 0xFFFFFF00;
+		default:
+			return res;
 	}
-
-	return ((uint32_t)(b4_c) << 0)  | ((uint32_t)(b3_c) << 8) |
-	       ((uint32_t)(b2_c) << 16) | ((uint32_t)(b1_c) << 24);
 }
 
-static uint32_t compute_str_factor(PVCore::PVUnicodeString const& str)
+
+Inendi::PVMappingFilterHostDefault::PVMappingFilterHostDefault():
+	PVMappingFilter()
 {
-	char b1_c = 0;
-	char b2_c = 0;
-	char b3_c = 0;
-	char b4_c = 0;
-
-	const size_t len = str.len();
-	PVCore::PVUnicodeString::utf_char const* const buf = str.buffer();
-	// TODO: check for UTF8 real chars!
-	if (len >= 1) {
-		b1_c = buf[0];
-		if (len >= 2) {
-			b2_c = buf[1];
-			if (len >= 3) {
-				b3_c = buf[2];
-				if (len >= 4) {
-					b4_c = buf[3];
-				}
-			}
-		}
-	}
-
-	return ((uint32_t)(b4_c) << 0)  | ((uint32_t)(b3_c) << 8) |
-	       ((uint32_t)(b2_c) << 16) | ((uint32_t)(b1_c) << 24);
+	INIT_FILTER_NOPARAM(PVMappingFilterHostDefault);
 }
 
-
-Inendi::PVMappingFilterHostDefault::PVMappingFilterHostDefault(PVCore::PVArgumentList const& args):
-	PVMappingFilter(),
-	_case_sensitive(false) // This will be changed by set_args anyway
-{
-	INIT_FILTER(PVMappingFilterHostDefault, args);
-}
-
-DEFAULT_ARGS_FILTER(Inendi::PVMappingFilterHostDefault)
-{
-	PVCore::PVArgumentList args;
-	args[PVCore::PVArgumentKey("convert-domain-lowercase", "Convert domain strings to lower case")].setValue<bool>(true);
-	return args;
-}
-
-void Inendi::PVMappingFilterHostDefault::set_args(PVCore::PVArgumentList const& args)
-{
-	Inendi::PVMappingFilter::set_args(args);
-	_case_sensitive = !args.at("convert-domain-lowercase").toBool();
-}
-
-Inendi::PVMappingFilter::decimal_storage_type Inendi::host_mapping::process_utf8(const char* buf, size_t size, PVMappingFilter*)
+/**
+ * Ip values are between 0 and 2**31 while string values are between 2**31 and 2**32
+ */
+Inendi::PVMappingFilter::decimal_storage_type Inendi::PVMappingFilterHostDefault::process_cell(const char* buf, size_t size)
 {
 	uint32_t ret;
 	if (PVCore::Network::ipv4_aton(buf, size, ret)) {
 		// That goes to the first half of the space
+		// Two consecutive value may have the same mapping value.
 		ret >>= 1;
 	}
 	else {
 		// Take the first four characters
-		ret = compute_str_factor(PVCore::PVUnicodeString(buf, size));
+		ret = compute_str_factor(buf, size);
 		// That goes to the other half!
 		ret = (ret >> 1) | 0x80000000;
 	}
@@ -113,23 +63,4 @@ Inendi::PVMappingFilter::decimal_storage_type Inendi::host_mapping::process_utf8
 	return ret_ds;
 }
 
-Inendi::PVMappingFilter::decimal_storage_type Inendi::host_mapping::process_utf16(const uint16_t* buf, size_t size, PVMappingFilter*)
-{
-	uint32_t ret;
-	if (PVCore::Network::ipv4_a16ton(buf, size, ret)) {
-		// That goes to the first half of the space
-		ret >>= 1;
-	}
-	else {
-		// Take the first four characters
-		ret = compute_str16_factor(PVCore::PVUnicodeString16(buf, size));
-		// That goes to the other half!
-		ret = (ret >> 1) | 0x80000000;
-	}
-
-	Inendi::PVMappingFilter::decimal_storage_type ret_ds;
-	ret_ds.storage_as_uint() = ret;
-	return ret_ds;
-}
-
-IMPL_FILTER(Inendi::PVMappingFilterHostDefault)
+IMPL_FILTER_NOPARAM(Inendi::PVMappingFilterHostDefault)
--- libinendi/plugins/mapping-filters/host_default/PVMappingFilterHostDefault.h
+++ libinendi/plugins/mapping-filters/host_default/PVMappingFilterHostDefault.h
@@ -2,7 +2,7 @@
  * @file
  *
  * @copyright (C) Picviz Labs 2011-March 2015
- * @copyright (C) ESI Group INENDI April 2015-2015
+ * @copyright (C) ESI Group INENDI April 2015-2016
  */
 
 #ifndef PVFILTER_PVMAPPINGFILTERHOSTDEFAULT_H
@@ -13,30 +13,31 @@
 
 namespace Inendi {
 
-struct host_mapping
-{
-	static Inendi::PVMappingFilter::decimal_storage_type process_utf8(const char* buf, size_t size, PVMappingFilter* m);
-	static Inendi::PVMappingFilter::decimal_storage_type process_utf16(uint16_t const* buf, size_t size, PVMappingFilter* m);
-};
-
+/**
+ * Host mapping work on string but separate Ip from String.
+ * 
+ * Ip are in 0 to 2**31 and string are in 2*µ31 to 2**32.
+ */
 class PVMappingFilterHostDefault: public PVMappingFilter
 {
 public:
-	PVMappingFilterHostDefault(PVCore::PVArgumentList const& args = PVMappingFilterHostDefault::default_args());
+	PVMappingFilterHostDefault();
 
 public:
-	Inendi::PVMappingFilter::decimal_storage_type process_cell(const char* buf, size_t size) override
-	{
-		return host_mapping::process_utf8(buf, size, this);
-	}
+	/**
+	 * This mapping apply cell by cell.
+	 *
+	 * It compute mapping value.
+	 */
+	Inendi::PVMappingFilter::decimal_storage_type process_cell(const char* buf, size_t size) override;
+
+	/**
+	 * Meta information from this plugin.
+	 */
 	QString get_human_name() const override { return QString("Default"); }
-	void set_args(PVCore::PVArgumentList const& args) override;
 	PVCore::DecimalType get_decimal_type() const override { return PVCore::UnsignedIntegerType; }
 
-private:
-	bool _case_sensitive;
-	
-	CLASS_FILTER(PVMappingFilterHostDefault)
+	CLASS_FILTER_NOPARAM(PVMappingFilterHostDefault)
 };
 
 }
