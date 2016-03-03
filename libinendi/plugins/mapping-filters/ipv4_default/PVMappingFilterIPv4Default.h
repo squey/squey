4f229c4d2c18866550351ec366dcacb3da860e66
Pierrick Brunet <pbrunet@u.picviz>
2016-03-03 14:58:00 +0100

    Rework IPV4 mapping
    * Use direct NRaw value.

--- libinendi/plugins/mapping-filters/ipv4_default/PVMappingFilterIPv4Default.h
+++ libinendi/plugins/mapping-filters/ipv4_default/PVMappingFilterIPv4Default.h
@@ -5,31 +5,46 @@
  * @copyright (C) ESI Group INENDI April 2015-2015
  */
 
-#ifndef PVFILTER_PVMAPPINGFILTERIPV4DEFAULT_H
-#define PVFILTER_PVMAPPINGFILTERIPV4DEFAULT_H
+#ifndef PVFILTER_PVMAPPINGFILTERIPV4_DEFAULT_H
+#define PVFILTER_PVMAPPINGFILTERIPV4_DEFAULT_H
 
 #include <pvkernel/core/general.h>
 #include <inendi/PVMappingFilter.h>
 
 namespace Inendi {
 
-struct ipv4_mapping
-{
-	static Inendi::PVMappingFilter::decimal_storage_type process_utf8(const char* buf, size_t size, PVMappingFilter* m);
-	static Inendi::PVMappingFilter::decimal_storage_type process_utf16(uint16_t const* buf, size_t size, PVMappingFilter* m);
-};
+class PVMappingFilterIPv4Default;
 
+/**
+ * IPv4 mapping. It keeps unsigned integer values.
+ */
 class PVMappingFilterIPv4Default: public PVMappingFilter
 {
-public:
-	Inendi::PVMappingFilter::decimal_storage_type process_cell(const char* buf, size_t size) override
-	{
-		return ipv4_mapping::process_utf8(buf, size, this);
-	}
-	QString get_human_name() const { return QString("Default"); }
-	PVCore::DecimalType get_decimal_type() const override { return PVCore::UnsignedIntegerType; }
+	public:
+		PVMappingFilterIPv4Default();
 
-	CLASS_FILTER(PVMappingFilterIPv4Default)
+		/**
+		 * Copy NRaw values (real unsigned integers value) as mapping value.
+		 */
+		decimal_storage_type* operator()(PVCol const col, PVRush::PVNraw const& nraw) override {
+			auto array = nraw.collection().column(col);
+
+			for(size_t row=0; row< array.size(); row++) {
+				Inendi::PVMappingFilter::decimal_storage_type ds;
+				ds.storage_as_int() = array.to_core_array<uint32_t>()[row];
+				_dest[row] = ds;
+			}
+
+			return _dest;
+		}
+
+		/**
+		 * Metainformation for this plugin.
+		 */
+		QString get_human_name() const override { return QString("Default"); }
+		PVCore::DecimalType get_decimal_type() const override { return PVCore::UnsignedIntegerType; }
+
+	CLASS_FILTER_NOPARAM(PVMappingFilterIPv4Default)
 };
 
 }
