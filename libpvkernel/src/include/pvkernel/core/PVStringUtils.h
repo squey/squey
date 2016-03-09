55c7fa028032d1a979e85a5d5838afed6ea90a10
Pierrick Brunet <pbrunet@u.picviz>
2016-03-09 16:15:27 +0100

    Improve string_default mapping
    * use int_log2 to improve performances
    * Add documentation about what is done.

--- libpvkernel/src/include/pvkernel/core/PVStringUtils.h
+++ libpvkernel/src/include/pvkernel/core/PVStringUtils.h
@@ -8,97 +8,102 @@
 #ifndef PVCORE_STRINGUTILS_H
 #define PVCORE_STRINGUTILS_H
 
-/* 16105 is the value corresponding to the arbitrary string:
- * "The competent programmer is fully aware of the limited size of his own skull. He therefore approaches his task with full humility, and 
- * avoids clever tricks like the plague."
- * Used by compte_str_factor
- */
-#define STRING_MAX_YVAL 16105
-
-#include <QChar>
-
 #include <pvkernel/core/PVUnicodeString.h>
+#include <cmath>
 
 namespace PVCore {
 
 class PVStringUtils {
 public:
 
-	static inline uint32_t compute_str_factor(PVCore::PVUnicodeString const& str, bool /*case_sensitive*/)
+	static inline uint32_t compute_str_factor(PVCore::PVUnicodeString const& str, bool case_sensitive)
 	{
 		const uint8_t* u8_buf = (const uint8_t*) str.buffer();
 		size_t size = str.size();
 
-		return _compute_str_factor<uint8_t, 8>(u8_buf, size, false);
+		return _compute_str_factor(u8_buf, size, case_sensitive);
 	};
 
 private:
-	template <typename T, size_t char_bits>
-	static inline uint32_t _compute_str_factor(T const* buf, size_t size, bool case_sensitive = true)
+	static uint8_t int_log2(uint16_t v) {
+//https://graphics.stanford.edu/~seander/bithacks.html#IntegerLogLookup
+		uint32_t shift = (v > 0xFF  ) << 3;
+		uint8_t r = shift;
+		v >>= shift;
+
+		shift = (v > 0xF   ) << 2;
+		r |= shift;
+		v >>= shift;
+
+		shift = (v > 0x3   ) << 1;
+		r |= shift;
+		v >>= shift;
+
+		r |= (v >> 1);
+
+		return r;
+	}
+
+	static inline uint32_t _compute_str_factor(uint8_t const* buf, size_t size, bool case_sensitive = true)
 	{
 		if (size < 1) {
 			return 0;
 		}
 
 		// -------------------------------------------------------------------
-		// |  a (4)  |  b (0..12)  |      c (9)        |      d (7..19)      | 32 bits
+		// |  a (4)  |  b (0..12)  |      c (8)        |      d (8..20)      | 32 bits
 		// -------------------------------------------------------------------
 		// a : log2(length)
-		// b : linear splitting between 'a' according to the length
-		// c : sum of the first 2 bytes
+		// b : linear splitting between 'a' according to the length of remainder of the log2 computing.
+		// c : the first bytes
 		// d : weak bits of the sum of the remaining bytes
 
-		size_t max_size = std::min(size, 4096UL);
+		assert(size <= 4096UL && "PVCOP give smaller str, string size would be too big");
 
-		size_t a = log2(max_size);
-		size_t a_prime = std::min(a, 9UL);
-		uint32_t factor = (a+1) << (32-4); // +1 to separate 1 length strings from 0 length strings
 
-		size_t b = (max_size - (1 << a));
-		factor = factor | (b << (32-4-a));
+		// Compute "a" and set it in the first 4 bits of factor
+		uint8_t shift = 32 - 4;
+		const uint8_t a = PVStringUtils::int_log2(size);
+		uint32_t factor = (a+1) << shift; // +1 to separate 1 length strings from 0 length strings
 
-		const T& c = buf[0];
-		size_t d_bits = 32-4-a-char_bits;
-		factor = factor | (char_value(c) << d_bits);
+		// Compute "b" and set it in the shortest number of bits that may contains it after "a"
+		// The shortest number of bits is "a"
+		shift -= a;
+		size_t b = (size - (1 << a));
+		factor = factor | (b << shift);
+
+		// Set the first bytes in "c"
+		shift -= 8;
+		uint8_t c = buf[0];
+		factor = factor | (c << shift);
+
+		// Compute the sum of remaining bytes. Truncate it on the remaining bytes (truncate strong bits) and set it in "d".
+		// "d" size depend on "b" size.
+		size_t max_remaining_size = std::min(size, 1UL << (32-4-a-8)) - 1;
+
+		if(max_remaining_size == 0) {
+			// Nothing more to sum.
+			return factor;
+		}
 
 		size_t d = 0;
-		size_t max_remaining_size = std::min(size, (size_t) 1 << (32-4-a_prime-char_bits-char_bits)) -1;
+
 		if (case_sensitive) {
-			for (size_t i = 0; i < max_remaining_size; i++) {
-				const T& c = buf[i+1];
-				d += char_value(c);
-			}
+			d = std::accumulate(buf + 1, buf + 1 + max_remaining_size, 0, std::plus<uint32_t>());
 		}
 		else {
-			for (size_t i = 0; i < max_remaining_size; i++) {
-				T c = to_lower(buf[i+1]);
-				d += char_value(c);
-			}
+			d = std::accumulate(buf + 1, buf + 1 + max_remaining_size, 0,
+								[&](uint8_t a, uint8_t b) { return std::tolower(a) + std::tolower(b); });
 		}
-		size_t shift = std::max(0UL, (d_bits) - (size_t) ceil(log2(max_remaining_size * (1 << char_bits))));
+
+//		size_t bits_in_sum = 8 + int_log2(max_remaining_size);
+//		if(bits_in_sum != (size_t) ceil(log2(max_remaining_size << 8)))
+//			std::cout << "Fail : " << bits_in_sum << "/" << (size_t) ceil(log2(max_remaining_size << 8)) << std::endl;
+		size_t d_bits = shift;
+		shift = std::max(0UL, shift - (size_t) ceil(log2(max_remaining_size << 8)));
 		factor = factor | ((d & ((1 << d_bits)-1)) << shift);
 
 		return factor;
-	}
-
-	static inline uint32_t char_value(uint16_t c)
-	{
-		return (c >> 8) + (c & 0x00FF);
-	}
-
-	static inline uint32_t char_value(uint8_t c)
-	{
-		return c;
-	}
-
-	static inline uint16_t to_lower(uint16_t c)
-	{
-		return QChar::toLower(c);
-	}
-
-	static inline uint16_t to_lower(uint8_t c)
-	{
-		return c;
 	}
 };
 
