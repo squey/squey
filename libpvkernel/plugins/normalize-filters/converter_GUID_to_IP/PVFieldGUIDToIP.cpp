2bdb8db56669f1ff69a749e1556c2e5c0d24a2b2
Pierrick Brunet <pbrunet@u.picviz>
2016-04-12 15:47:31 +0200

    Re-enable GUID to IP conversion.

--- libpvkernel/plugins/normalize-filters/converter_GUID_to_IP/PVFieldGUIDToIP.cpp
+++ libpvkernel/plugins/normalize-filters/converter_GUID_to_IP/PVFieldGUIDToIP.cpp
@@ -2,7 +2,7 @@
  * @file
  *
  * @copyright (C) Picviz Labs 2013-March 2015
- * @copyright (C) ESI Group INENDI April 2015-2015
+ * @copyright (C) ESI Group INENDI April 2015-2016
  */
 
 #include "PVFieldGUIDToIP.h"
@@ -42,52 +42,35 @@ PVCore::PVField& PVFilter::PVFieldGUIDToIP::one_to_one(PVCore::PVField& field)
 	// IPv4=88.190.253.248
 	// IPv6=58be:fdf8:400c:0c00:0000:0000:0000:0071
 
-	constexpr int field_max_len = 38;
-	constexpr size_t ipv4_hexa_len = 8;
+	constexpr int field_max_len = 38; // With bracet
 	constexpr size_t ipv4_dec_max_len = 15;
 	constexpr size_t ipv6_hexa_len = 32+7;
 
-	PVCore::PVArgumentList args = FilterT::get_args();
-
-	QString field_str;
-	field.get_qstr(field_str);
-	int start = 0;
-	if (field_str.size() == field_max_len) {
-		start++;
+	char* txt = field.begin();
+	if (field.size() == field_max_len) {
+		// Skip bracet
+		txt++;
 	}
 
-	if (!_ipv6) { //ipv4
-		char dec_ipv4[ipv4_dec_max_len];
-
+	if (not _ipv6) { //ipv4
 		unsigned int a, b, c, d;
-		if (sscanf(field_str.mid(start, ipv4_hexa_len).toStdString().c_str(), "%2x%2x%2x%2x", &a, &b, &c, &d) == 4) {
-			snprintf(dec_ipv4, ipv4_dec_max_len+1, "%u.%u.%u.%u", a, b, c, d);
-
-			size_t ip_dec_utf16_len = strlen(dec_ipv4)*2;
-
-			const ushort* dec_ipv4_utf16 = QString::fromLatin1(dec_ipv4).utf16();
-			field.allocate_new(ip_dec_utf16_len);
-			memcpy(field.begin(), dec_ipv4_utf16, ip_dec_utf16_len);
-			field.set_end(field.begin() + ip_dec_utf16_len);
-		}
-		else {
+		if (sscanf(txt, "%2x%2x%2x%2x", &a, &b, &c, &d) == 4) {
+			field.allocate_new(ipv4_dec_max_len + 1);
+			int end = snprintf(field.begin(), ipv4_dec_max_len + 1, "%u.%u.%u.%u", a, b, c, d);
+			field.set_end(field.begin() + end);
+		} else {
+			// Invalid GUID Format
 			field.set_invalid();
 			field.elt_parent()->set_invalid();
 		}
-	}
-	else { //ipv6
-		constexpr size_t ipv6_hexa_utf16_len = ipv6_hexa_len*2;
-		char ipv6_hexa[ipv6_hexa_len];
+	} else { //ipv6
 		unsigned int a, b, c, d, e, f, g, h;
 
-		if (sscanf(field_str.mid(start, ipv6_hexa_len).toStdString().c_str(), "%4X%4X-%4X-%4X-%4X-%4X%4X%4X", &a, &b, &c, &d, &e, &f, &g, &h) == 8) {
-			snprintf(ipv6_hexa, ipv6_hexa_len+1, "%04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x", a, b, c, d, e, f, g, h);
-			const ushort* ipv6_hexa_utf16 = QString::fromLatin1(ipv6_hexa).utf16();
-			field.allocate_new(ipv6_hexa_utf16_len);
-			memcpy(field.begin(), ipv6_hexa_utf16, ipv6_hexa_utf16_len);
-			field.set_end(field.begin() + ipv6_hexa_utf16_len);
-		}
-		else {
+		if (sscanf(txt, "%4X%4X-%4X-%4X-%4X-%4X%4X%4X", &a, &b, &c, &d, &e, &f, &g, &h) == 8) {
+			field.allocate_new(ipv6_hexa_len + 1);
+			int end = snprintf(field.begin(), ipv6_hexa_len + 1, "%04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x", a, b, c, d, e, f, g, h);
+			field.set_end(field.begin() + end);
+		} else {
 			field.set_invalid();
 			field.elt_parent()->set_invalid();
 		}
