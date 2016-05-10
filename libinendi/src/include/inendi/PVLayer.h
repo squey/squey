e3a4bde680b7f5e3b04e12423bf039bdab93c989
Pierrick Brunet <pbrunet@u.picviz>
2016-05-10 06:59:43 +0200

    Remove unused macro.

--- libinendi/src/include/inendi/PVLayer.h
+++ libinendi/src/include/inendi/PVLayer.h
@@ -17,14 +17,9 @@
 #include <inendi/PVSelection.h>
 #include <inendi/PVLayer_types.h>
 
-//#include <tbb/cache_aligned_allocator.h>
-
 #include <vector>
 
 #define INENDI_LAYER_NAME_MAXLEN 1000
-
-#define INENDI_LAYER_ARCHIVE_EXT "pvl"
-#define INENDI_LAYER_ARCHIVE_FILTER "INENDI layer-stack files (*." INENDI_LAYER_ARCHIVE_EXT ")"
 
 namespace Inendi
 {
