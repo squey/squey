a4774883e757d4d761fbe7c6ee82ff1820ac6ea5
Pierrick Brunet <pbrunet@u.picviz>
2016-05-18 16:05:44 +0200

    Remove dummy files for test as everything can be done in C++.

--- whatismissing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# @file
-#
-# @copyright (C) Picviz Labs 2010-March 2015
-# @copyright (C) ESI Group INENDI April 2015-2015
-
-git status |grep -v "tests/rush/Trush_" |grep -v "tests/core/Tcore_" |grep -v ".cmake" |grep -v "*~" |grep -v "*.cxx" |grep -v "*.so"
