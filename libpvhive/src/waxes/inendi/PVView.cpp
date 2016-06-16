8b613f6838c9e856111b96d835cbe13615190439
Pierrick Brunet <pbrunet@u.picviz>
2016-06-16 10:23:06 +0200

    No observer for post_filter_layer.

--- libpvhive/src/waxes/inendi/PVView.cpp
+++ libpvhive/src/waxes/inendi/PVView.cpp
@@ -38,10 +38,8 @@ IMPL_WAX(Inendi::PVView::process_from_selection, view, args)
 	    call_object_default<Inendi::PVView, FUNC(Inendi::PVView::process_from_selection)>(view,
 	                                                                                      args);
 	if (v) {
-		refresh_observers(&v->get_post_filter_layer());
 		refresh_observers(&v->get_output_layer());
 	}
-	refresh_observers(&view->get_post_filter_layer());
 	refresh_observers(&view->get_output_layer());
 
 	return v;
@@ -53,10 +51,8 @@ IMPL_WAX(Inendi::PVView::process_from_layer_stack, view, args)
 	    call_object_default<Inendi::PVView, FUNC(Inendi::PVView::process_from_layer_stack)>(view,
 	                                                                                        args);
 	if (v) {
-		refresh_observers(&v->get_post_filter_layer());
 		refresh_observers(&v->get_output_layer());
 	}
-	refresh_observers(&view->get_post_filter_layer());
 	refresh_observers(&view->get_output_layer());
 
 	return v;
