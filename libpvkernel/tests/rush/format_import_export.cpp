cb7bc96aaec292cad06d809fe90dbc3537d1598a
Pierrick Brunet <pbrunet@u.picviz>
2016-07-18 10:21:35 +0200

    Add more tests for the aggregator.

--- libpvkernel/tests/rush/format_import_export.cpp
+++ libpvkernel/tests/rush/format_import_export.cpp
@@ -8,6 +8,8 @@
 #include <pvkernel/core/inendi_assert.h>
 #include <pvkernel/core/PVUtils.h>
 
+#include <sys/stat.h>
+
 #ifdef INSPECTOR_BENCH
 constexpr size_t DUPL = 10000;
 #else
@@ -17,19 +19,32 @@ constexpr size_t DUPL = 1;
 int main(int argc, char** argv)
 {
 	if (argc < 4) {
-		std::cerr << "Usage: " << argv[0] << " input_file output_file format" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " input_file output_file format [begin] [extra_input]"
+		          << std::endl;
 		return 1;
 	}
 
 	const char* input_file = argv[1];
 	const char* output_file = argv[2];
 	const char* format = argv[3];
+	size_t begin = 0;
+	if (argc > 4) {
+		errno = 0;
+		begin = std::strtoul(argv[4], nullptr, 10);
+		if (errno == ERANGE) {
+			throw std::runtime_error("Invalid input for begin value");
+		}
+	}
+	std::string extra_input;
+	if (argc > 5) {
+		extra_input = argv[5];
+	}
 
-	pvtest::TestEnv env(input_file, format, DUPL);
+	pvtest::TestEnv env(input_file, format, DUPL, extra_input);
 
 	auto start = std::chrono::system_clock::now();
 
-	env.load_data();
+	env.load_data(begin);
 
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> diff = end - start;
