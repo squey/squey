87df625690cace2a2e1378ce0a350ff86b3d5486
Pierrick Brunet <pbrunet@u.picviz>
2016-07-22 12:41:36 +0200

    Build FIeld inplace for CSVSplitter.

--- libpvkernel/plugins/normalize-filters/splitter_csv/PVFieldSplitterCSV.h
+++ libpvkernel/plugins/normalize-filters/splitter_csv/PVFieldSplitterCSV.h
@@ -23,16 +23,9 @@ class PVFieldSplitterCSV : public PVFieldsFilter<one_to_many>
   public:
 	PVCore::list_fields::size_type one_to_many(PVCore::list_fields& l,
 	                                           PVCore::list_fields::iterator it_ins,
-	                                           PVCore::PVField const& field);
+	                                           PVCore::PVField& field);
 
-	PVCore::list_fields::size_type one_to_many(PVCore::list_fields& l,
-	                                           PVCore::list_fields::iterator it_ins,
-	                                           PVCore::PVField& field)
-	{
-		return one_to_many(l, it_ins, (PVCore::PVField const&)field);
-	}
-
-	bool guess(list_guess_result_t& res, PVCore::PVField const& in_field);
+	bool guess(list_guess_result_t& res, PVCore::PVField& in_field);
 
 	void set_args(PVCore::PVArgumentList const& args);
 
