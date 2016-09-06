squey/squey

commit c2c2ddae688c2b366b8a3fdff35a2b32ea07f24d

Use sigc instead of Hive on layer_stack update.

gui-qt/src/PVMainWindow.cpp
@@ -381,10 +381,10 @@ void PVInspector::PVMainWindow::commit_selection_to_new_layer(Inendi::PVView* in
 	PVHive::get().register_actor(view_sp, actor);
 
 	if (should_hide_layers) {
-		actor.call<FUNC(Inendi::PVView::hide_layers)>();
+		view_sp->hide_layers();
 	}
 
-	actor.call<FUNC(Inendi::PVView::add_new_layer)>(name);
+	view_sp->add_new_layer();
 	Inendi::PVLayer& layer = view_sp->get_current_layer();
 
 	// We need to configure the layer
@@ -415,10 +415,10 @@ void PVInspector::PVMainWindow::move_selection_to_new_layer(Inendi::PVView* inen
 	if (!name.isEmpty()) {
 
 		if (should_hide_layers) {
-			actor.call<FUNC(Inendi::PVView::hide_layers)>();
+			view_sp->hide_layers();
 		}
 
-		actor.call<FUNC(Inendi::PVView::add_new_layer)>(name);
+		view_sp->add_new_layer();
 		Inendi::PVLayer& new_layer = inendi_view->get_current_layer();
 
 		/* We set it's selection to the final selection */

gui-qt/src/PVLayerStackEventFilter.cpp
@@ -1,53 +1,0 @@
-/**
- * @file
- *
- * @copyright (C) Picviz Labs 2009-March 2015
- * @copyright (C) ESI Group INENDI April 2015-2015
- */
-
-#include <QtWidgets>
-#include <QEvent>
-
-#include <PVLayerStackView.h>
-#include <PVMainWindow.h>
-
-#include <PVLayerStackEventFilter.h>
-
-/******************************************************************************
- *
- * PVInspector::PVLayerStackEventFilter::PVLayerStackEventFilter
- *
- *****************************************************************************/
-PVInspector::PVLayerStackEventFilter::PVLayerStackEventFilter(PVMainWindow* mw,
-                                                              PVLayerStackView* parent)
-    : QObject(parent)
-{
-	PVLOG_DEBUG("PVInspector::PVLayerStackEventFilter::%s\n", __FUNCTION__);
-
-	main_window = mw;
-	layer_stack_view = parent;
-}
-
-/******************************************************************************
- *
- * PVInspector::PVLayerStackEventFilter::eventFilter
- *
- *****************************************************************************/
-bool PVInspector::PVLayerStackEventFilter::eventFilter(QObject* /*watched*/, QEvent* event)
-{
-	PVLOG_DEBUG("PVInspector::PVLayerStackEventFilter::%s : with an event of type %d\n",
-	            __FUNCTION__, event->type());
-
-	if (event->type() == QEvent::HoverMove) {
-		layer_stack_view->mouse_hover_layer_index =
-		    layer_stack_view->rowAt(((QMouseEvent*)event)->x());
-		if (layer_stack_view->mouse_hover_layer_index !=
-		    layer_stack_view->last_mouse_hover_layer_index) {
-			layer_stack_view->last_mouse_hover_layer_index =
-			    layer_stack_view->mouse_hover_layer_index;
-			layer_stack_view->viewport()->update();
-		}
-		return true;
-	}
-	return false;
-}

libinendi/src/PVLayerStack.cpp
@@ -60,7 +60,7 @@ Inendi::PVLayer* Inendi::PVLayerStack::append_new_layer(PVRow row_count,
 	} else {
 		layer_name = name;
 	}
-	return append_layer(PVLayer(layer_name, row_count));
+	return append_layer(PVLayer(layer_name, &row_count));
 }
 
 /******************************************************************************

libinendi/src/PVView.cpp
@@ -92,6 +92,7 @@ Inendi::PVView::~PVView()
  *****************************************************************************/
 void Inendi::PVView::reset_layers()
 {
+	_layer_stack_about_to_refresh.emit();
 	PVRow row_count = get_row_count();
 
 	// This function remove all the layers and add the default one with all events
@@ -111,6 +112,7 @@ void Inendi::PVView::reset_layers()
 	post_filter_layer.reset_to_full_and_default_color(row_count);
 	layer_stack_output_layer.reset_to_full_and_default_color(row_count);
 	output_layer.reset_to_full_and_default_color(row_count);
+	_layer_stack_refreshed.emit();
 }
 
 /******************************************************************************
@@ -120,32 +122,41 @@ void Inendi::PVView::reset_layers()
  *****************************************************************************/
 void Inendi::PVView::add_new_layer(QString name)
 {
-	Inendi::PVLayer* layer = layer_stack.append_new_layer(get_row_count(), name);
-	layer->compute_selectable_count(get_row_count());
+	_layer_stack_about_to_refresh.emit();
+	size_t row_count = get_row_count();
+	Inendi::PVLayer* layer = layer_stack.append_new_layer(row_count, name);
+	layer->compute_selectable_count(row_count);
 
+	_layer_stack_refreshed.emit();
 	_update_current_min_max.emit();
 }
 
 void Inendi::PVView::delete_selected_layer()
 {
+	_layer_stack_about_to_refresh.emit();
 	layer_stack.delete_selected_layer();
 
+	_layer_stack_refreshed.emit();
 	_update_current_min_max.emit();
 }
 
 void Inendi::PVView::delete_layer_n(int idx)
 {
+	_layer_stack_about_to_refresh.emit();
 	layer_stack.delete_by_index(idx);
 
+	_layer_stack_refreshed.emit();
 	_update_current_min_max.emit();
 }
 
 void Inendi::PVView::duplicate_selected_layer(const QString& name)
 {
+	_layer_stack_about_to_refresh.emit();
 	PVLayer* new_layer = layer_stack.duplicate_selected_layer(name);
 	compute_layer_min_max(*new_layer);
 	new_layer->compute_selectable_count(get_row_count());
 
+	_layer_stack_refreshed.emit();
 	_update_current_min_max.emit();
 }
 
@@ -719,11 +730,12 @@ void Inendi::PVView::set_floating_selection(PVSelection& selection)
  * Inendi::PVView::set_layer_stack_layer_n_name
  *
  *****************************************************************************/
-int Inendi::PVView::set_layer_stack_layer_n_name(int n, QString const& name)
+void Inendi::PVView::set_layer_stack_layer_n_name(int n, QString const& name)
 {
+	_layer_stack_about_to_refresh.emit();
 	PVLayer& layer = layer_stack.get_layer_n(n);
 	layer.set_name(name);
-	return 0;
+	_layer_stack_refreshed.emit();
 }
 
 /******************************************************************************
@@ -733,8 +745,10 @@ int Inendi::PVView::set_layer_stack_layer_n_name(int n, QString const& name)
  *****************************************************************************/
 void Inendi::PVView::set_layer_stack_selected_layer_index(int index)
 {
+	_layer_stack_about_to_refresh.emit();
 	layer_stack.set_selected_layer_index(index);
 
+	_layer_stack_refreshed.emit();
 	_update_current_min_max.emit();
 }
 
@@ -764,8 +778,9 @@ void Inendi::PVView::set_selection_view(PVSelection const& sel)
  * Inendi::PVView::toggle_layer_stack_layer_n_visible_state
  *
  *****************************************************************************/
-int Inendi::PVView::toggle_layer_stack_layer_n_visible_state(int n)
+void Inendi::PVView::toggle_layer_stack_layer_n_visible_state(int n)
 {
+	_layer_stack_about_to_refresh.emit();
 	PVLayer& layer = layer_stack.get_layer_n(n);
 
 	if (layer.get_visible()) {
@@ -773,7 +788,7 @@ int Inendi::PVView::toggle_layer_stack_layer_n_visible_state(int n)
 	} else {
 		layer.set_visible(1);
 	}
-	return 0;
+	_layer_stack_refreshed.emit();
 }
 
 /******************************************************************************
@@ -782,8 +797,10 @@ int Inendi::PVView::toggle_layer_stack_layer_n_visible_state(int n)
 
 void Inendi::PVView::move_selected_layer_to(int new_index)
 {
+	_layer_stack_about_to_refresh.emit();
 	get_layer_stack().move_selected_layer_to(new_index);
 
+	_layer_stack_refreshed.emit();
 	_update_current_min_max.emit();
 }
 

libinendi/src/include/inendi/PVView.h
@@ -116,7 +116,12 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>,
 	int get_layer_stack_layer_n_visible_state(int n) const;
 	PVLayer& get_layer_stack_output_layer();
 	PVLayer const& get_layer_stack_output_layer() const { return layer_stack_output_layer; }
-	void hide_layers() { layer_stack.hide_layers(); }
+	void hide_layers()
+	{
+		_layer_stack_about_to_refresh.emit();
+		layer_stack.hide_layers();
+		_layer_stack_refreshed.emit();
+	}
 
 	PVCol get_active_axis() const
 	{
@@ -182,7 +187,7 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>,
 
 	void set_color_on_active_layer(const PVCore::PVHSVColor c);
 
-	int set_layer_stack_layer_n_name(int n, QString const& name);
+	void set_layer_stack_layer_n_name(int n, QString const& name);
 
 	void set_layer_stack_selected_layer_index(int index);
 
@@ -191,7 +196,7 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>,
 	void set_selection_from_layer(PVLayer const& layer);
 	void set_selection_view(PVSelection const& sel);
 
-	int toggle_layer_stack_layer_n_visible_state(int n);
+	void toggle_layer_stack_layer_n_visible_state(int n);
 	void move_selected_layer_to(int new_index);
 
 	void select_all_nonzb_lines();
@@ -382,6 +387,8 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>,
 	sigc::signal<void> _axis_combination_updated;
 	sigc::signal<void> _axis_combination_about_to_update;
 	sigc::signal<void> _update_current_min_max;
+	sigc::signal<void> _layer_stack_about_to_refresh;
+	sigc::signal<void> _layer_stack_refreshed;
 
   protected:
 	/*! \brief PVView's specific axes combination

libpvguiqt/src/PVAbstractListStatsDlg.cpp
@@ -766,10 +766,10 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layer_with_selected_values()
 
 	multiple_search(_msearch_action_for_layer_creation, sl, false);
 
-	actor.call<FUNC(Inendi::PVView::add_new_layer)>(text);
+	view_sp->add_new_layer(text);
 	Inendi::PVLayer& layer = view_sp->get_layer_stack().get_selected_layer();
 	int ls_index = view_sp->get_layer_stack().get_selected_layer_index();
-	actor.call<FUNC(Inendi::PVView::toggle_layer_stack_layer_n_visible_state)>(ls_index);
+	view_sp->toggle_layer_stack_layer_n_visible_state(ls_index);
 
 	// We need to configure the layer
 	view_sp->commit_selection_to_layer(layer);
@@ -787,7 +787,7 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layer_with_selected_values()
 			insert_pos = old_selected_layer_index;
 			++old_selected_layer_index;
 		}
-		actor.call<FUNC(Inendi::PVView::move_selected_layer_to)>(insert_pos);
+		view_sp->move_selected_layer_to(insert_pos);
 	}
 
 	ls.set_selected_layer_index(old_selected_layer_index);
@@ -878,10 +878,10 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layers_for_selected_values()
 		sl.append(s);
 		multiple_search(_msearch_action_for_layer_creation, sl, false);
 
-		actor.call<FUNC(Inendi::PVView::add_new_layer)>(layer_name);
+		view_sp->add_new_layer(layer_name);
 		Inendi::PVLayer& layer = view_sp->get_layer_stack().get_selected_layer();
 		int ls_index = view_sp->get_layer_stack().get_selected_layer_index();
-		actor.call<FUNC(Inendi::PVView::toggle_layer_stack_layer_n_visible_state)>(ls_index);
+		view_sp->toggle_layer_stack_layer_n_visible_state(ls_index);
 
 		// We need to configure the layer
 		view_sp->commit_selection_to_layer(layer);
@@ -897,7 +897,7 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layers_for_selected_values()
 				insert_pos = old_selected_layer_index;
 				++old_selected_layer_index;
 			}
-			actor.call<FUNC(Inendi::PVView::move_selected_layer_to)>(insert_pos);
+			view_sp->move_selected_layer_to(insert_pos);
 		}
 
 		ls.set_selected_layer_index(old_selected_layer_index);

libpvguiqt/src/PVLayerStackModel.cpp
@@ -23,22 +23,17 @@ PVGuiQt::PVLayerStackModel::PVLayerStackModel(Inendi::PVView_sp& lib_view, QObje
     , _lib_view(*lib_view)
     , select_brush(QColor(255, 240, 200))
     , unselect_brush(QColor(180, 180, 180))
-    , _obs(this)
-    , _ls_valid(true)
 {
 	PVLOG_DEBUG("PVGuiQt::PVLayerStackModel::%s : Creating object\n", __FUNCTION__);
 
 	select_font.setBold(true);
 
 	PVHive::get().register_actor(lib_view, _actor);
-	PVHive::get().register_observer(
-	    lib_view, [=](Inendi::PVView& view) { return &view.get_layer_stack(); }, _obs);
 
-	_obs.connect_about_to_be_deleted(
-	    this, SLOT(layer_stack_about_to_be_deleted(PVHive::PVObserverBase*)));
-	_obs.connect_about_to_be_refreshed(
-	    this, SLOT(layer_stack_about_to_be_refreshed(PVHive::PVObserverBase*)));
-	_obs.connect_refresh(this, SLOT(layer_stack_refreshed(PVHive::PVObserverBase*)));
+	lib_view->_layer_stack_about_to_refresh.connect(
+	    sigc::mem_fun(this, &PVGuiQt::PVLayerStackModel::layer_stack_about_to_be_refreshed));
+	lib_view->_layer_stack_refreshed.connect(
+	    sigc::mem_fun(this, &PVGuiQt::PVLayerStackModel::layer_stack_refreshed));
 }
 
 /******************************************************************************
@@ -84,21 +79,6 @@ QVariant PVGuiQt::PVLayerStackModel::data(const QModelIndex& index, int role) co
 			return QBrush(QColor(205, 139, 204));
 		}
 		break;
-	/*
-	        if (parent_widget && parent_widget->get_layer_stack_widget() &&
-	   parent_widget->get_layer_stack_widget()->get_layer_stack_view()) {
-	                PVLayerStackView *layer_stack_view =
-	   parent_widget->get_layer_stack_widget()->get_layer_stack_view();
-	                if (lib_layer_stack().get_selected_layer_index() == lib_index)
-	   {
-	                        peturn QBrush(QColor(205,139,204));
-	                }
-	                if (layer_stack_view->mouse_hover_layer_index == index.row())
-	   {
-	                        return QBrush(QColor(200,200,200));
-	                }
-	                return QBrush(QColor(255,255,255));
-	        }*/
 
 	case (Qt::DisplayRole):
 		switch (index.column()) {
@@ -190,10 +170,6 @@ QVariant PVGuiQt::PVLayerStackModel::headerData(int /*section*/,
  *****************************************************************************/
 int PVGuiQt::PVLayerStackModel::rowCount(const QModelIndex& /*index*/) const
 {
-	if (!_ls_valid) {
-		return 0;
-	}
-
 	return lib_layer_stack().get_layer_count();
 }
 
@@ -212,26 +188,17 @@ bool PVGuiQt::PVLayerStackModel::setData(const QModelIndex& index, const QVarian
 	case (Qt::EditRole):
 		switch (index.column()) {
 		case 0:
-			_actor.call<FUNC(Inendi::PVView::toggle_layer_stack_layer_n_visible_state)>(lib_index);
+			lib_view().toggle_layer_stack_layer_n_visible_state(lib_index);
 			_actor.call<FUNC(Inendi::PVView::process_from_layer_stack)>();
 			return true;
 
 		case 1:
-			_actor.call<FUNC(Inendi::PVView::set_layer_stack_layer_n_name)>(lib_index,
-			                                                                value.toString());
+			lib_view().set_layer_stack_layer_n_name(lib_index, value.toString());
 			return true;
 
 		default:
 			return QAbstractTableModel::setData(index, value, role);
 		}
-
-	case (PVCustomQtRoles::RoleSetSelectedItem): {
-		const bool is_sel = value.toBool();
-		if (is_sel) {
-			_actor.call<FUNC(Inendi::PVView::set_layer_stack_selected_layer_index)>(lib_index);
-		}
-		return true;
-	}
 
 	default:
 		return QAbstractTableModel::setData(index, value, role);
@@ -240,14 +207,7 @@ bool PVGuiQt::PVLayerStackModel::setData(const QModelIndex& index, const QVarian
 	return false;
 }
 
-void PVGuiQt::PVLayerStackModel::layer_stack_about_to_be_deleted(PVHive::PVObserverBase* /*o*/)
-{
-	beginResetModel();
-	_ls_valid = false;
-	endResetModel();
-}
-
-void PVGuiQt::PVLayerStackModel::layer_stack_about_to_be_refreshed(PVHive::PVObserverBase* /*o*/)
+void PVGuiQt::PVLayerStackModel::layer_stack_about_to_be_refreshed()
 {
 	beginResetModel();
 }
@@ -260,14 +220,14 @@ void PVGuiQt::PVLayerStackModel::reset_layer_colors(const int idx)
 	_actor.call<FUNC(Inendi::PVView::process_from_layer_stack)>();
 }
 
-void PVGuiQt::PVLayerStackModel::layer_stack_refreshed(PVHive::PVObserverBase* /*o*/)
+void PVGuiQt::PVLayerStackModel::layer_stack_refreshed()
 {
 	endResetModel();
 }
 
 void PVGuiQt::PVLayerStackModel::add_new_layer(QString name)
 {
-	_actor.call<FUNC(Inendi::PVView::add_new_layer)>(name);
+	_lib_view.add_new_layer(name);
 	Inendi::PVLayer& layer = lib_layer_stack().get_layer_n(rowCount() - 1);
 	layer.reset_to_full_and_default_color(layer.get_selection().count());
 	_actor.call<FUNC(Inendi::PVView::process_from_layer_stack)>();
@@ -295,7 +255,7 @@ void PVGuiQt::PVLayerStackModel::delete_selected_layer()
 		return;
 	}
 
-	_actor.call<FUNC(Inendi::PVView::delete_selected_layer)>();
+	_lib_view.delete_selected_layer();
 	_actor.call<FUNC(Inendi::PVView::process_from_layer_stack)>();
 }
 
@@ -315,6 +275,6 @@ void PVGuiQt::PVLayerStackModel::delete_layer_n(const int idx)
 		return;
 	}
 
-	_actor.call<FUNC(Inendi::PVView::delete_layer_n)>(idx);
+	_lib_view.delete_layer_n(idx);
 	_actor.call<FUNC(Inendi::PVView::process_from_layer_stack)>();
 }

libpvguiqt/src/PVLayerStackView.cpp
@@ -42,25 +42,12 @@ PVGuiQt::PVLayerStackView::PVLayerStackView(QWidget* parent) : QTableView(parent
 	verticalHeader()->hide();
 	horizontalHeader()->hide();
 
-// viewport()->setMouseTracking(true);
-// viewport()->setAttribute(Qt::WA_Hover, true);
-
-#if 0
-	// We use a delegate to render the Icons 
-	layer_stack_delegate = new PVLayerStackDelegate(mw, this);
-	setItemDelegate(layer_stack_delegate);
-
-	layer_stack_event_filter = new PVLayerStackEventFilter(mw, this);
-	viewport()->installEventFilter(layer_stack_event_filter);
-
-	mouse_hover_layer_index = -1;
-	last_mouse_hover_layer_index = -1;
-#endif
-
-	connect(this, SIGNAL(clicked(QModelIndex const&)), this,
-	        SLOT(layer_clicked(QModelIndex const&)));
-	connect(this, SIGNAL(doubleClicked(QModelIndex const&)), this,
-	        SLOT(layer_double_clicked(QModelIndex const&)));
+	connect(this, &QTableView::clicked, [this](QModelIndex const& idx) {
+		int layer_count = ls_model()->lib_view().get_layer_stack().get_layer_count();
+		/* We create and store the true index of the layer in the lib */
+		int lib_index = layer_count - 1 - idx.row();
+		ls_model()->lib_view().set_layer_stack_selected_layer_index(lib_index);
+	});
 
 	// Context menu
 	connect(this, SIGNAL(customContextMenuRequested(const QPoint&)), this,
@@ -114,8 +101,6 @@ void PVGuiQt::PVLayerStackView::leaveEvent(QEvent* /*event*/)
 {
 	PVLOG_DEBUG("PVGuiQt::PVLayerStackView::%s\n", __FUNCTION__);
 
-	// mouse_hover_layer_index = -1;
-	// last_mouse_hover_layer_index = -1;
 	viewport()->update();
 	clearFocus();
 }
@@ -221,18 +206,4 @@ void PVGuiQt::PVLayerStackView::export_layer_selection(int model_idx)
 void PVGuiQt::PVLayerStackView::reset_layer_colors(int layer_idx)
 {
 	ls_model()->reset_layer_colors(layer_idx);
-}
-
-void PVGuiQt::PVLayerStackView::layer_clicked(QModelIndex const& idx)
-{
-	if (!idx.isValid()) {
-		// Qt says it's only called when idx is valid, but still..
-		return;
-	}
-
-	ls_model()->setData(idx, QVariant(true), PVCustomQtRoles::RoleSetSelectedItem);
-}
-
-void PVGuiQt::PVLayerStackView::layer_double_clicked(QModelIndex const&)
-{
 }

commit 6e5c02b90d0ecfef20553fbc6036d64d90d19280

[GuiQt] get back use of 'name' when creating a new layer from the current selection
The regression had been introduced in commit c2c2ddae688c2b366b8a3fdff35a2b32ea07f24d
which created a new layer without using the requested name.

close #344

gui-qt/src/PVMainWindow.cpp
@@ -347,7 +347,7 @@ void PVInspector::PVMainWindow::commit_selection_to_new_layer(Inendi::PVView* in
 		inendi_view->hide_layers();
 	}
 
-	inendi_view->add_new_layer();
+	inendi_view->add_new_layer(name);
 	Inendi::PVLayer& layer = inendi_view->get_current_layer();
 
 	// We need to configure the layer