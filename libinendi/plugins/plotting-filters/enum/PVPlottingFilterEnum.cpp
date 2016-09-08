eb518f268bd63d7928ae42a73bc39a42e363fcdb
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2016-09-08 16:49:23 +0200

    Move mapping uniform code to plotting enum

--- libinendi/plugins/plotting-filters/enum/PVPlottingFilterEnum.cpp
+++ libinendi/plugins/plotting-filters/enum/PVPlottingFilterEnum.cpp
@@ -7,21 +7,61 @@
 
 #include "PVPlottingFilterEnum.h"
 
+#include <limits>
+
+#include <fstream>
+
+#include <pvkernel/core/inendi_bench.h>
+
+using plotting_t = uint32_t;
+
 void Inendi::PVPlottingFilterEnum::
 operator()(pvcop::db::array const& mapped, pvcop::db::array const&, uint32_t* dest)
 {
-	assert(dest);
+	BENCH_START(mapping);
 
-	pvcop::db::groups group;
+	BENCH_START(group);
+
+	pvcop::db::groups groups;
 	pvcop::db::extents extents;
+	mapped.group(groups, extents);
 
-	mapped.group(group, extents);
+	BENCH_END(group, "PVPlottingFilterEnum::group", mapped.size(),
+	          pvcop::db::type_traits(mapped.type()).get_size(), mapped.size(), sizeof(plotting_t));
 
-	auto& core_group = group.to_core_array();
+	auto& core_extents = extents.to_core_array();
 
-	// -1 as we count "number of space between values", not "values"
-	double extend_factor = std::numeric_limits<uint32_t>::max() / ((double)extents.size() - 1);
-	for (size_t row = 0; row < mapped.size(); row++) {
-		dest[row] = extend_factor * core_group[row];
+	BENCH_START(sort);
+
+	// Sort extents
+	mapped.parallel_sort(extents);
+	pvcop::db::indexes indexes = pvcop::db::indexes::parallel_sort(extents);
+	auto& sorted_extents = indexes.to_core_array();
+	indexes.parallel_sort(extents);
+
+	BENCH_END(sort, "PVPlottingFilterEnum::sort", mapped.size(),
+	          pvcop::db::type_traits(mapped.type()).get_size(), mapped.size(), sizeof(plotting_t));
+
+	auto& core_groups = groups.to_core_array();
+
+	BENCH_START(fill_mapping);
+
+	if (extents.size() == 1) {
+		const plotting_t default_value = std::numeric_limits<plotting_t>::max() / 2;
+		std::fill_n(dest, mapped.size(), default_value);
+	} else {
+		// -1 as we count "number of space between values", not "values"
+		const double extend_factor =
+		    std::numeric_limits<plotting_t>::max() / ((double)extents.size() - 1);
+#pragma omp parallel for
+		for (size_t row = 0; row < mapped.size(); row++) {
+			dest[row] = ~uint32_t(extend_factor * sorted_extents[core_groups[row]]);
+		}
 	}
+
+	BENCH_END(fill_mapping, "PVPlottingFilterEnum::fill_mapping", mapped.size(),
+	          pvcop::db::type_traits(mapped.type()).get_size(), mapped.size(), sizeof(plotting_t));
+
+	BENCH_END(mapping, "PVPlottingFilterEnum::operator()", mapped.size(),
+	          pvcop::db::type_traits(mapped.type()).get_size(), mapped.size(), sizeof(plotting_t));
 }
