squey/squey

commit a5e5c712ba0d12c30aa34878bab1b5c70a4b128a

get rid of floating and volatil selection.
* These selection are confusing as they are a globals state not always
properly set. Removing them cost some extra selection allocation but
avoid unconsistent global state.

gui-qt/src/PVMainWindow.cpp
@@ -363,8 +363,6 @@ void PVInspector::PVMainWindow::commit_selection_to_new_layer(Inendi::PVView* in
 	inendi_view->commit_selection_to_layer(layer);
 	inendi_view->update_current_layer_min_max();
 	inendi_view->compute_selectable_count(layer);
-	// and to update the layer-stack
-	inendi_view->process_layer_stack();
 }
 
 /******************************************************************************
@@ -401,7 +399,7 @@ void PVInspector::PVMainWindow::move_selection_to_new_layer(Inendi::PVView* inen
 		// do not forget to update the current layer
 		inendi_view->compute_selectable_count(current_layer);
 
-		inendi_view->process_layer_stack();
+		inendi_view->process_layer_stack(inendi_view->get_real_output_selection());
 	}
 }
 

libinendi/src/PVStateMachine.cpp
@@ -14,8 +14,6 @@
  *****************************************************************************/
 Inendi::PVStateMachine::PVStateMachine()
 {
-	square_area_mode = AREA_MODE_OFF;
-
 	listing_unselected_visible = false;
 	listing_zombie_visible = false;
 }

libinendi/src/PVView.cpp
@@ -35,12 +35,10 @@ PVCore::PVHSVColor Inendi::PVView::_default_zombie_line_properties(HSV_COLOR_BLA
  *****************************************************************************/
 Inendi::PVView::PVView(PVPlotted& plotted)
     : PVCore::PVDataTreeChild<PVPlotted, PVView>(plotted)
-    , floating_selection(get_row_count())
     , post_filter_layer("post_filter_layer", get_row_count())
     , layer_stack_output_layer("view_layer_stack_output_layer", get_row_count())
     , output_layer("output_layer", get_row_count())
     , layer_stack(get_row_count())
-    , volatile_selection(get_row_count())
     , _axes_combination(get_parent<PVSource>().get_format())
     , _view_id(get_parent<PVRoot>().get_new_view_id())
     , _active_axis(0)
@@ -70,7 +68,8 @@ Inendi::PVView::PVView(PVPlotted& plotted)
 
 	_layer_stack_refreshed.emit();
 
-	process_layer_stack();
+	post_filter_layer.get_selection().select_all();
+	process_layer_stack(post_filter_layer.get_selection());
 }
 
 /******************************************************************************
@@ -151,29 +150,6 @@ void Inendi::PVView::commit_selection_to_layer(PVLayer& new_layer)
 	new_layer.get_selection() = post_filter_layer.get_selection();
 	output_layer.get_lines_properties().A2B_copy_restricted_by_selection(
 	    new_layer.get_lines_properties(), new_layer.get_selection());
-}
-
-void Inendi::PVView::commit_volatile_in_floating_selection()
-{
-	switch (_state_machine.get_square_area_mode()) {
-	case Inendi::PVStateMachine::AREA_MODE_ADD_VOLATILE:
-		floating_selection |= volatile_selection;
-		break;
-
-	case Inendi::PVStateMachine::AREA_MODE_INTERSECT_VOLATILE:
-		floating_selection &= volatile_selection;
-		break;
-
-	case Inendi::PVStateMachine::AREA_MODE_SET_WITH_VOLATILE:
-		floating_selection = volatile_selection;
-		break;
-
-	case Inendi::PVStateMachine::AREA_MODE_SUBSTRACT_VOLATILE:
-		floating_selection -= volatile_selection;
-		break;
-
-	case Inendi::PVStateMachine::AREA_MODE_OFF:;
-	}
 }
 
 /******************************************************************************
@@ -409,12 +385,12 @@ void Inendi::PVView::process_correlation()
  * Inendi::PVView::process_layer_stack
  *
  *****************************************************************************/
-void Inendi::PVView::process_layer_stack()
+void Inendi::PVView::process_layer_stack(Inendi::PVSelection const& sel)
 {
 	layer_stack.process(layer_stack_output_layer, get_row_count());
 	_update_layer_stack_output_layer.emit();
 
-	process_post_filter_layer();
+	process_post_filter_layer(sel);
 }
 
 /******************************************************************************
@@ -422,34 +398,11 @@ void Inendi::PVView::process_layer_stack()
  * Inendi::PVView::process_post_filter_layer
  *
  *****************************************************************************/
-void Inendi::PVView::process_post_filter_layer()
+void Inendi::PVView::process_post_filter_layer(Inendi::PVSelection const& to_use)
 {
-	/* We treat the selection according to specific SQUARE_AREA_SELECTION mode */
-	switch (_state_machine.get_square_area_mode()) {
-	case Inendi::PVStateMachine::AREA_MODE_SET_WITH_VOLATILE:
-		post_filter_layer.get_selection() = volatile_selection;
-		break;
-
-	case Inendi::PVStateMachine::AREA_MODE_ADD_VOLATILE:
-		post_filter_layer.get_selection() = floating_selection | volatile_selection;
-		break;
-
-	case Inendi::PVStateMachine::AREA_MODE_SUBSTRACT_VOLATILE:
-		post_filter_layer.get_selection().AB_sub(floating_selection, volatile_selection);
-		break;
-
-	case Inendi::PVStateMachine::AREA_MODE_INTERSECT_VOLATILE:
-		post_filter_layer.get_selection() = floating_selection & volatile_selection;
-		break;
-
-	default:
-		post_filter_layer.get_selection() = layer_stack_output_layer.get_selection();
-		break;
-	}
-
 	/* We cut the resulting selection with what is available in the layer_stack */
 	/* We are in ALL edit_mode */
-	post_filter_layer.get_selection() &= layer_stack_output_layer.get_selection();
+	post_filter_layer.get_selection() = layer_stack_output_layer.get_selection() & to_use;
 
 	/* We simply copy the lines_properties */
 	post_filter_layer.get_lines_properties() = layer_stack_output_layer.get_lines_properties();
@@ -520,7 +473,7 @@ void Inendi::PVView::set_color_on_active_layer(const PVCore::PVHSVColor c)
 	PVLayer& active_layer = layer_stack.get_selected_layer();
 
 	active_layer.get_lines_properties().selection_set_color(get_real_output_selection(), c);
-	process_layer_stack();
+	process_layer_stack(get_real_output_selection());
 }
 
 /******************************************************************************
@@ -567,9 +520,7 @@ void Inendi::PVView::set_selection_from_layer(PVLayer const& layer)
  *****************************************************************************/
 void Inendi::PVView::set_selection_view(PVSelection const& sel)
 {
-	_state_machine.set_square_area_mode(Inendi::PVStateMachine::AREA_MODE_SET_WITH_VOLATILE);
-	volatile_selection = sel;
-	process_post_filter_layer();
+	process_post_filter_layer(sel);
 }
 
 /******************************************************************************
@@ -605,17 +556,16 @@ void Inendi::PVView::move_selected_layer_to(int new_index)
 
 void Inendi::PVView::select_all()
 {
-	_state_machine.set_square_area_mode(Inendi::PVStateMachine::AREA_MODE_SET_WITH_VOLATILE);
-	volatile_selection.select_all();
-	process_post_filter_layer();
+	Inendi::PVSelection sel(get_row_count());
+	sel.select_all();
+	process_post_filter_layer(sel);
 }
 
 void Inendi::PVView::select_none()
 {
-	// Set square area mode w/ volatile
-	_state_machine.set_square_area_mode(Inendi::PVStateMachine::AREA_MODE_SET_WITH_VOLATILE);
-	volatile_selection.select_none();
-	process_post_filter_layer();
+	Inendi::PVSelection sel(get_row_count());
+	sel.select_none();
+	process_post_filter_layer(sel);
 }
 
 std::string Inendi::PVView::get_name() const

libinendi/src/include/inendi/PVStateMachine.h
@@ -25,22 +25,6 @@ class PVStateMachine
 	bool view_unselected_zombie_visible = true;
 
   public:
-	/** Square Area Modes
-	 * helps to identify the selection mode
-	 * that is done with the square area
-	 */
-	enum SquareAreaModes {
-		AREA_MODE_OFF, /**< No selection area */
-		AREA_MODE_SET_WITH_VOLATILE,
-		AREA_MODE_ADD_VOLATILE,
-		AREA_MODE_SUBSTRACT_VOLATILE,
-		AREA_MODE_INTERSECT_VOLATILE
-	};
-
-  private:
-	SquareAreaModes square_area_mode;
-
-  public:
 	PVStateMachine();
 
 	// listing state management
@@ -64,8 +48,6 @@ class PVStateMachine
 
 	QString get_string();
 
-	SquareAreaModes get_square_area_mode() const { return square_area_mode; }
-
 	void set_listing_unselected_visible(bool visible) { listing_unselected_visible = visible; }
 
 	void set_listing_zombie_visible(bool visible) { listing_zombie_visible = visible; }
@@ -74,8 +56,6 @@ class PVStateMachine
 	{
 		view_unselected_zombie_visible = visible;
 	}
-
-	void set_square_area_mode(SquareAreaModes mode) { square_area_mode = mode; }
 
 	void toggle_listing_unselected_visibility()
 	{

libinendi/src/include/inendi/PVView.h
@@ -44,9 +44,6 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>
 	~PVView();
 
   public:
-	inline PVSelection& get_floating_selection() { return floating_selection; }
-	inline PVSelection& get_volatile_selection() { return volatile_selection; }
-
 	// Proxy functions for PVHive
 	bool move_axis_to_new_position(PVCol index_source, PVCol index_dest)
 	{
@@ -110,8 +107,6 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>
 		assert(_active_axis < get_column_count());
 		return _active_axis;
 	}
-	PVStateMachine& get_state_machine() { return _state_machine; }
-	PVStateMachine const& get_state_machine() const { return _state_machine; }
 
 	PVAxesCombination const& get_axes_combination() const { return _axes_combination; }
 	void set_axes_combination_list_id(PVAxesCombination::columns_indexes_t const& idxes,
@@ -211,7 +206,7 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>
 	 *
 	 * * Save data in layer_stack_output_layer.
 	 */
-	void process_layer_stack();
+	void process_layer_stack(Inendi::PVSelection const& sel);
 
 	/**
 	 * Set correct selection to post_filter_layer.
@@ -219,7 +214,7 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>
 	 * * Copy color (FIXME : done every time, should be done only once).
 	 * * Merge selection with layer_stack selection.
 	 */
-	void process_post_filter_layer();
+	void process_post_filter_layer(Inendi::PVSelection const& sel);
 
 	/**
 	 * Compute output layer from post_filter_layer data.
@@ -244,8 +239,6 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>
 	 *
 	 */
 	std::string get_data(PVRow row, PVCol column) const;
-
-	void commit_volatile_in_floating_selection();
 
 	/***********
 	 * FILTERS
@@ -285,13 +278,6 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>
 	PVRow get_plotted_col_max_row(PVCol const combined_col) const;
 
   public:
-	// State machine
-	inline void set_square_area_mode(PVStateMachine::SquareAreaModes mode)
-	{
-		_state_machine.set_square_area_mode(mode);
-	}
-
-  public:
 	void serialize_write(PVCore::PVSerializeObject& so);
 	static Inendi::PVView& serialize_read(PVCore::PVSerializeObject& so, Inendi::PVPlotted& parent);
 
@@ -319,15 +305,12 @@ class PVView : public PVCore::PVDataTreeChild<PVPlotted, PVView>
 	sigc::signal<void> _about_to_be_delete;
 
   protected:
-	PVSelection floating_selection; //!< This is the current selection
 	PVLayer
 	    post_filter_layer; //!< Contains selection and color lines for in progress view computation.
 	PVLayer layer_stack_output_layer; //!< Layer grouping every information from the layer stack
 	PVLayer output_layer;             //!< This is the shown layer.
 	PVLayerStack layer_stack;
 	PVStateMachine _state_machine;
-	PVSelection volatile_selection; //!< It is the selection currently computed. It will be flush in
-	                                // floating_selection once it is completed.
 
 	/*! \brief PVView's specific axes combination
 	 *  It is originaly copied from the parent's PVSource, and then become specific

libpvguiqt/src/PVAbstractListStatsDlg.cpp
@@ -770,8 +770,6 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layer_with_selected_values()
 	lib_view()->commit_selection_to_layer(layer);
 	lib_view()->update_current_layer_min_max();
 	lib_view()->compute_selectable_count(layer);
-	// and to update the layer-stack
-	lib_view()->process_layer_stack();
 
 	if (mode != PVWidgets::PVLayerNamingPatternDialog::ON_TOP) {
 		int insert_pos;
@@ -787,9 +785,7 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layer_with_selected_values()
 
 	ls.set_selected_layer_index(old_selected_layer_index);
 
-	lib_view()->get_volatile_selection() = old_sel;
-	lib_view()->commit_volatile_in_floating_selection();
-	lib_view()->process_post_filter_layer();
+	lib_view()->process_layer_stack(old_sel);
 }
 
 /******************************************************************************
@@ -888,16 +884,13 @@ void PVGuiQt::PVAbstractListStatsDlg::create_layers_for_selected_values()
 			}
 			lib_view()->move_selected_layer_to(insert_pos);
 		}
-
-		ls.set_selected_layer_index(old_selected_layer_index);
-		lib_view()->get_volatile_selection() = old_sel;
-		lib_view()->commit_volatile_in_floating_selection();
-		lib_view()->process_post_filter_layer();
 		++offset;
 	});
 
+	ls.set_selected_layer_index(old_selected_layer_index);
+
 	// we can update the layer-stack once all layers have been created
-	lib_view()->process_layer_stack();
+	lib_view()->process_layer_stack(old_sel);
 }
 
 /******************************************************************************

libpvguiqt/src/PVLayerFilterProcessWidget.cpp
@@ -181,7 +181,9 @@ void PVGuiQt::PVLayerFilterProcessWidget::reject()
 	*_args_widget->get_args() = _args_org;
 
 	// Update everything
-	_view->process_layer_stack();
+	Inendi::PVSelection sel(_view->get_row_count());
+	sel.select_all();
+	_view->process_layer_stack(sel);
 
 	QDialog::reject();
 }
@@ -206,10 +208,7 @@ void PVGuiQt::PVLayerFilterProcessWidget::save_Slot()
 	    current_selected_layer.get_lines_properties(),
 	    _view->get_post_filter_layer().get_selection());
 
-	_view->get_volatile_selection() = _view->get_post_filter_layer().get_selection();
-	_view->set_square_area_mode(Inendi::PVStateMachine::AREA_MODE_SET_WITH_VOLATILE);
-
-	_view->process_layer_stack();
+	_view->process_layer_stack(_view->get_post_filter_layer().get_selection());
 
 	// Save last used filter
 	_view->set_last_used_filter(_filter_p->registered_name());

libpvguiqt/src/PVLayerStackModel.cpp
@@ -185,7 +185,7 @@ bool PVGuiQt::PVLayerStackModel::setData(const QModelIndex& index, const QVarian
 		switch (index.column()) {
 		case 0:
 			lib_view().toggle_layer_stack_layer_n_visible_state(lib_index);
-			lib_view().process_layer_stack();
+			lib_view().process_layer_stack(lib_view().get_real_output_selection());
 			return true;
 
 		case 1:
@@ -213,7 +213,7 @@ void PVGuiQt::PVLayerStackModel::reset_layer_colors(const int idx)
 	Inendi::PVLayerStack& layerstack = lib_layer_stack();
 	Inendi::PVLayer& layer = layerstack.get_layer_n(lib_index_from_model_index(idx));
 	layer.reset_to_default_color();
-	lib_view().process_layer_stack();
+	lib_view().process_layer_stack(lib_view().get_real_output_selection());
 }
 
 void PVGuiQt::PVLayerStackModel::layer_stack_refreshed()
@@ -226,14 +226,14 @@ void PVGuiQt::PVLayerStackModel::add_new_layer(QString name)
 	_lib_view.add_new_layer(name);
 	Inendi::PVLayer& layer = lib_layer_stack().get_layer_n(rowCount() - 1);
 	layer.reset_to_full_and_default_color();
-	lib_view().process_layer_stack();
+	lib_view().process_layer_stack(lib_view().get_real_output_selection());
 }
 
 void PVGuiQt::PVLayerStackModel::move_selected_layer_up()
 {
 	beginResetModel();
 	lib_view().move_selected_layer_up();
-	lib_view().process_layer_stack();
+	lib_view().process_layer_stack(lib_view().get_real_output_selection());
 	endResetModel();
 }
 
@@ -241,7 +241,7 @@ void PVGuiQt::PVLayerStackModel::move_selected_layer_down()
 {
 	beginResetModel();
 	lib_view().move_selected_layer_down();
-	lib_view().process_layer_stack();
+	lib_view().process_layer_stack(lib_view().get_real_output_selection());
 	endResetModel();
 }
 
@@ -253,14 +253,14 @@ void PVGuiQt::PVLayerStackModel::delete_selected_layer()
 
 	_lib_view.delete_selected_layer();
 
-	lib_view().process_layer_stack();
+	lib_view().process_layer_stack(lib_view().get_real_output_selection());
 }
 
 void PVGuiQt::PVLayerStackModel::duplicate_selected_layer(const QString& name)
 {
 	beginResetModel();
 	lib_view().duplicate_selected_layer(name);
-	lib_view().process_layer_stack();
+	lib_view().process_layer_stack(lib_view().get_real_output_selection());
 	endResetModel();
 }
 
@@ -273,5 +273,5 @@ void PVGuiQt::PVLayerStackModel::delete_layer_n(const int idx)
 	}
 
 	_lib_view.delete_layer_n(idx);
-	lib_view().process_layer_stack();
+	lib_view().process_layer_stack(lib_view().get_real_output_selection());
 }

commit 3f3ba5d0145582ab839e08817bc10d3b59c52e39

[GuiQt] revive layerstack reprocessing when creating a new layer from current selection
regression had been introduced in commit a5e5c712ba0d12c30aa34878bab1b5c70a4b128a

close #373

gui-qt/src/PVMainWindow.cpp
@@ -342,6 +342,7 @@ void PVInspector::PVMainWindow::commit_selection_to_new_layer(Inendi::PVView* in
 	inendi_view->commit_selection_to_layer(layer);
 	inendi_view->update_current_layer_min_max();
 	inendi_view->compute_selectable_count(layer);
+	inendi_view->process_layer_stack(inendi_view->get_real_output_selection());
 }
 
 /******************************************************************************