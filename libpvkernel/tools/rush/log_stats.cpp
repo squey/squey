33ab640a636b8f24d89479d13bfdcf7586c6a41d
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2016-09-16 10:44:40 +0200

    [logstats] Ignore unselected axes to speedup import process

--- libpvkernel/tools/rush/log_stats.cpp
+++ libpvkernel/tools/rush/log_stats.cpp
@@ -4,23 +4,13 @@
  * @copyright (C) ESI Group INENDI 2016
  */
 
-#include "../../tests/rush/common.h"
-
-#include <pvkernel/core/PVDirectory.h>
-#include <pvkernel/rush/PVUtils.h>
-
-#include <pvcop/db/algo.h>
+#include "log_stats.h"
 
 #include <boost/program_options.hpp>
 #include <boost/tokenizer.hpp>
 
 #include <iomanip>
 #include <sstream>
-
-struct integers_list {
-	using integers_t = std::vector<int32_t>;
-	integers_t values;
-};
 
 void validate(boost::any& v, std::vector<std::string> const& options, integers_list*, int32_t)
 {
@@ -46,14 +36,6 @@ void validate(boost::any& v, std::vector<std::string> const& options, integers_l
 }
 
 namespace po = boost::program_options;
-
-struct cmd_options {
-	std::vector<std::string> log_files;
-	std::string format_file;
-	integers_list columns_index;
-	bool extended_stats;
-	size_t max_stats_rows;
-};
 
 cmd_options parse_options(int argc, char** argv)
 {
@@ -102,68 +84,6 @@ cmd_options parse_options(int argc, char** argv)
 	}
 
 	return opts;
-}
-
-void run_stats(cmd_options opts)
-{
-	std::string tmp_collector_path;
-
-	{
-		pvtest::TestEnv env(opts.log_files, opts.format_file);
-		env.load_data();
-
-		const PVRush::PVFormat format = env._format;
-		const pvcop::collection& collection = env._nraw.collection();
-		tmp_collector_path = collection.rootdir();
-		size_t row_count = pvcop::core::algo::bit_count(env._nraw.valid_rows_sel());
-
-		pvlogger::info() << row_count << " rows / " << collection.row_count() << std::endl;
-
-		pvcop::db::array distinct_values;
-		pvcop::db::array distinct_values_count;
-
-		if (opts.columns_index.values.empty()) {
-			opts.columns_index.values.resize(collection.column_count());
-			std::iota(opts.columns_index.values.begin(), opts.columns_index.values.end(), 0);
-		}
-
-		for (size_t col : opts.columns_index.values) {
-			const pvcop::db::array& column = collection.column(col);
-			pvcop::db::algo::distinct(column, distinct_values, distinct_values_count,
-			                          env._nraw.valid_rows_sel());
-
-			std::stringstream stream;
-			stream << format.get_axes()[col].get_name().toStdString() << " : "
-			       << distinct_values.size();
-
-			if (distinct_values.size() > 1) {
-				std::cout << '\t' << stream.str() << std::endl;
-
-				if (opts.extended_stats) {
-					pvcop::db::indexes indexes =
-					    pvcop::db::indexes::parallel_sort(distinct_values_count);
-					const auto& sorted = indexes.to_core_array();
-
-					const auto& counts = distinct_values_count.to_core_array<uint64_t>();
-
-					for (size_t row = 0; row < std::min(opts.max_stats_rows, indexes.size());
-					     row++) {
-						size_t sorted_row = sorted[sorted.size() - row - 1];
-						std::cout << PVRush::PVUtils::safe_export(distinct_values.at(sorted_row),
-						                                          ",", "\"")
-						          << "," << counts[sorted_row] << "," << std::setprecision(2)
-						          << std::fixed << (double)counts[sorted_row] / row_count * 100
-						          << "%" << std::endl;
-					}
-				}
-			} else {
-				std::cout << stream.str() << " (" << distinct_values.at(0) << ")" << std::endl;
-			}
-		}
-	}
-
-	// Cleanup temporary nraw directory
-	PVCore::PVDirectory::remove_rec(tmp_collector_path.c_str());
 }
 
 int main(int argc, char** argv)
