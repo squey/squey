6f7540f28c962d5cfaf38d067fea6b8e9b40f243
Pierrick Brunet <pbrunet@u.picviz>
2016-09-22 09:37:35 +0200

    Add parentheses around macro

--- libpvkernel/src/include/pvkernel/core/inendi_bench.h
+++ libpvkernel/src/include/pvkernel/core/inendi_bench.h
@@ -21,14 +21,14 @@
 #define BENCH_SHOW(var, desc, nelts_start, size_elt_start, nelts_end, size_elt_end)                \
 	{                                                                                              \
 		double time = (__bench_end_##var - __bench_start_##var).seconds();                         \
-		double size_in_mb = (double)(nelts_start * size_elt_start) / (1024.0 * 1024.0);            \
-		double size_out_mb = (double)(nelts_end * size_elt_end) / (1024.0 * 1024.0);               \
+		double size_in_mb = (double)((nelts_start) * (size_elt_start)) / (1024.0 * 1024.0);        \
+		double size_out_mb = (double)((nelts_end) * (size_elt_end)) / (1024.0 * 1024.0);           \
 		double bw_in = size_in_mb / time;                                                          \
 		double bw_out = size_out_mb / time;                                                        \
-		std::cout << desc << ": in " << time * 1000 << " ms. Input (#/size/BW): " << nelts_start   \
-		          << "/" << size_in_mb << " MB/" << bw_in                                          \
-		          << " MB/s | Output (#/size/BW): " << nelts_end << "/" << size_out_mb << " MB/"   \
-		          << bw_out << " MB/s" << std::endl;                                               \
+		std::cout << (desc) << ": in " << time * 1000                                              \
+		          << " ms. Input (#/size/BW): " << (nelts_start) << "/" << size_in_mb << " MB/"    \
+		          << bw_in << " MB/s | Output (#/size/BW): " << (nelts_end) << "/" << size_out_mb  \
+		          << " MB/" << bw_out << " MB/s" << std::endl;                                     \
 	}
 
 #define BENCH_END(var, desc, nelts_start, size_elt_start, nelts_end, size_elt_end)                 \
@@ -44,7 +44,7 @@
 #define BENCH_STAT_IN_BW(var, nelts_in, size_elt_in)                                               \
 	{                                                                                              \
 		double time = (__bench_end_##var - __bench_start_##var).seconds();                         \
-		double size_in_mb = (double)(nelts_in * size_elt_in) / (1024.0 * 1024.0);                  \
+		double size_in_mb = (double)((nelts_in) * (size_elt_in)) / (1024.0 * 1024.0);              \
 		double bw_in = size_in_mb / time;                                                          \
 		PV_STAT_MEM_BW(#var "_in_bw", bw_in);                                                      \
 	}
@@ -52,7 +52,7 @@
 #define BENCH_STAT_OUT_BW(var, nelts_out, size_elt_out)                                            \
 	{                                                                                              \
 		double time = (__bench_end_##var - __bench_start_##var).seconds();                         \
-		double size_out_mb = (double)(nelts_out * size_elt_out) / (1024.0 * 1024.0);               \
+		double size_out_mb = (double)((nelts_out) * (size_elt_out)) / (1024.0 * 1024.0);           \
 		double bw_out = size_out_mb / time;                                                        \
 		PV_STAT_MEM_BW(#var "_out_bw", bw_out);                                                    \
 	}
@@ -78,8 +78,8 @@
 	PVCore::PVMemory::get_memory_usage(__mem_bench_##var_vm_end, __mem_bench_##var_rss_end);       \
 	double diff_vm_mb = (__mem_bench_##var_vm_end - __mem_bench_##var_vm) / (1024.0);              \
 	double diff_rss_mb = (__mem_bench_##var_rss_end - __mem_bench_##var_rss) / (1024.0);           \
-	std::cout << desc << ": memory footprint is: VM=" << diff_vm_mb << " MB / RES=" << diff_rss_mb \
-	          << " MB" << std::endl;
+	std::cout << (desc) << ": memory footprint is: VM=" << diff_vm_mb                              \
+	          << " MB / RES=" << diff_rss_mb << " MB" << std::endl;
 
 #else
 
@@ -105,7 +105,7 @@
 #define CHECK(v) __CHECK(v, __FILE__, __LINE__)
 #define __CHECK(v, F, L)                                                                           \
 	if (!(v)) {                                                                                    \
-		std::cerr << F << ":" << L << " :" << #v << " isn't valid." << std::endl;                  \
+		std::cerr << (F) << ":" << (L) << " :" << #v << " isn't valid." << std::endl;              \
 	}
 
 #endif
