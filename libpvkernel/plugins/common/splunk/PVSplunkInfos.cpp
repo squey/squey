1485f9ce7e8d637b3cee53fd584fbaba58b5145c
Rémi Hérilier <remi.herilier@esi-group.com>
2016-10-06 11:52:54 +0200

    [Splunk] remove credentials serialization from investigation
    close #439

--- libpvkernel/plugins/common/splunk/PVSplunkInfos.cpp
+++ libpvkernel/plugins/common/splunk/PVSplunkInfos.cpp
@@ -11,8 +11,6 @@ void PVRush::PVSplunkInfos::serialize_write(PVCore::PVSerializeObject& so) const
 {
 	so.attribute_write("host", _host);
 	so.attribute_write("port", _port);
-	so.attribute_write("login", _login);
-	so.attribute_write("password", _password);
 	so.attribute_write("splunk_index", _splunk_index);
 	so.attribute_write("splunk_host", _splunk_host);
 	so.attribute_write("splunk_sourcetype", _splunk_sourcetype);
@@ -23,8 +21,6 @@ PVRush::PVSplunkInfos PVRush::PVSplunkInfos::serialize_read(PVCore::PVSerializeO
 	PVRush::PVSplunkInfos infos;
 	infos._host = so.attribute_read<QString>("host");
 	infos._port = so.attribute_read<uint16_t>("port");
-	infos._login = so.attribute_read<QString>("login");
-	infos._password = so.attribute_read<QString>("password");
 	infos._splunk_index = so.attribute_read<QString>("splunk_index");
 	infos._splunk_host = so.attribute_read<QString>("splunk_host");
 	infos._splunk_sourcetype = so.attribute_read<QString>("splunk_sourcetype");
