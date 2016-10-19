e82703490cf978de46e8c4700f33f4de8da1566a
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2016-10-19 12:23:07 +0200

    Extract time formats from binary to configuration file

--- libpvkernel/src/include/pvkernel/rush/PVTypesDiscoveryOutput.h
+++ libpvkernel/src/include/pvkernel/rush/PVTypesDiscoveryOutput.h
@@ -14,8 +14,10 @@
 #include <pvkernel/filter/PVFilterFunction.h> // for CLASS_FILTER_NONREG
 
 #include <pvbase/types.h> // for PVRow
+#include <pvbase/general.h>
 
 #include <cassert> // for assert
+#include <unordered_set>
 
 #include <pvcop/formatter_desc_list.h>
 
@@ -33,6 +35,8 @@ namespace PVRush
 class PVTypesDiscoveryOutput : public PVRush::PVOutput
 {
   public:
+	using autodet_type_t =
+	    std::vector<std::pair<std::pair<std::string, std::string>, std::vector<std::string>>>;
 	using type_desc_t = std::pair<std::string, std::string>;
 
   public:
@@ -44,6 +48,8 @@ class PVTypesDiscoveryOutput : public PVRush::PVOutput
 
   public:
 	type_desc_t type_desc(PVCol col) const;
+	static void append_time_formats(const std::unordered_set<std::string>& time_formats);
+	static std::unordered_set<std::string> supported_time_formats();
 
   protected:
 	void prepare_load(const PVRush::PVFormat&) override;
@@ -55,6 +61,7 @@ class PVTypesDiscoveryOutput : public PVRush::PVOutput
 	using matching_formatters_t = std::vector<std::vector<bool>>;
 	using types_desc_t = std::vector<type_desc_t>;
 
+	autodet_type_t _types;
 	std::vector<pvcop::types::formatter_interface*> _formatters;
 	types_desc_t _types_desc;
 	matching_formatters_t _matching_formatters;
--- libpvkernel/src/rush/PVTypesDiscoveryOutput.cpp
+++ libpvkernel/src/rush/PVTypesDiscoveryOutput.cpp
@@ -17,8 +17,10 @@
 #include <pvcop/types/factory.h>
 #include <boost/utility/string_ref.hpp>
 
+static constexpr const char* USER_TIME_FORMATS_FILENAME = "user_time_formats.ini";
+
 // clang-format off
-static const std::vector<std::pair<std::pair<std::string, std::string>, std::vector<std::string>>> TYPES {{
+static const PVRush::PVTypesDiscoveryOutput::autodet_type_t TYPES {{
 //   {{ formatter, parameters}, { excluded_formatter_1, excluded_formatter_2, ...}}
      {{"number_uint32", ""},    { "number_float", "number_double", "time" }},
      {{"number_int32",  ""},    { "number_float", "number_double", "time" }},
@@ -30,28 +32,69 @@ static const std::vector<std::pair<std::pair<std::string, std::string>, std::vec
      {{"ipv4",          ""},    {}},
      {{"ipv6",          ""},    {}},
      {{"mac_address",   ""},    {}},
+}};
 
-     {{"time", "yyyy.MMMM.dd H:mm"}, {}},
-     {{"time", "yyyy-M-d h:mm"}, {}},
-     {{"time", "yyyy-M-d H:m:s"}, {}},
-     {{"time", "yyyy-M-d H:m:ss.S"}, {}},
-     {{"time", "yyyy/MM/dd HH:mm:ss Z"}, {}},
-     {{"time", "yyyy/M/d"}, {}},
-     {{"time", "MMM d H:m:s"}, {}},
-     {{"time", "MMM d yyyy H:m:s"}, {}},
-     {{"time", "eee MMM d H:m:ss yyyy"}, {}},
-     {{"time", "dMMMyyyy H:m:s"}, {}},
-     {{"time", "dMMMyyyy"}, {}},
-     {{"time", "d/M/yy H:m:s"}, {}},
-     {{"time", "H:m:s"}, {}},
-     {{"time", "H%m%s"}, {}},
-     {{"time", "d/M/yyyy H:m:s.S"}, {}},
-     {{"time", "yy H:m:s.S"}, {}},
-     {{"time", "yy H%m%s.S"}, {}},
-     {{"time", "yy-M-d H:mm:ss.SSS"}, {}},
-     {{"time", "yy-M-d H:mm:ss.SSS V"}, {}}
+static const std::vector<std::string> SUPPLIED_TIMES_FORMATS {{
+	"yyyy.MMMM.dd H:mm",
+	"yyyy-M-d h:mm",
+	"yyyy-M-d H:m:s",
+	"yyyy-M-d H:m:ss.S",
+	"yyyy/MM/dd HH:mm:ss Z",
+	"yyyy/M/d",
+	"MMM d H:m:s",
+	"MMM d yyyy H:m:s",
+	"eee MMM d H:m:ss yyyy",
+	"dMMMyyyy H:m:s",
+	"dMMMyyyy",
+	"d/M/yy H:m:s",
+	"H:m:s",
+	"H%m%s",
+	"d/M/yyyy H:m:s.S",
+	"yy H:m:s.S",
+	"yy H%m%s.S",
+	"yy-M-d H:mm:ss.SSS",
+	"yy-M-d H:mm:ss.SSS V",
 }};
 // clang-format on
+
+static std::string get_user_time_formats_path()
+{
+	return std::string(getenv("HOME")) + "/" + INENDI_INSPECTOR_CONFDIR + "/" +
+	       USER_TIME_FORMATS_FILENAME;
+}
+
+static PVRush::PVTypesDiscoveryOutput::autodet_type_t supported_types()
+{
+	PVRush::PVTypesDiscoveryOutput::autodet_type_t types = TYPES;
+
+	// supplied time formats
+	for (const std::string& suplied_time_format : SUPPLIED_TIMES_FORMATS) {
+		types.push_back({{"time", suplied_time_format}, {}});
+	}
+
+	// user time formats (update file if needed to remove potential duplications)
+	std::string updated_user_time_formats;
+	const std::unordered_set<std::string> supplied_times_formats(SUPPLIED_TIMES_FORMATS.begin(),
+	                                                             SUPPLIED_TIMES_FORMATS.end());
+	std::ifstream in_f(get_user_time_formats_path());
+	bool user_time_formats_file_needs_update = false;
+	for (std::string user_time_format; getline(in_f, user_time_format);) {
+		auto it = supplied_times_formats.find(user_time_format);
+		bool duplicated_time_format = it != supplied_times_formats.end();
+		user_time_formats_file_needs_update |= duplicated_time_format;
+		if (not duplicated_time_format) {
+			types.push_back({{"time", user_time_format}, {}});
+			updated_user_time_formats += user_time_format + "\n";
+		}
+	}
+	if (user_time_formats_file_needs_update) {
+		in_f.close();
+		std::ofstream out_f(get_user_time_formats_path());
+		out_f << updated_user_time_formats;
+	}
+
+	return types;
+}
 
 static pvcop::formatter_desc get_formatter_desc(const std::string& type,
                                                 const std::string& type_format)
@@ -63,11 +106,42 @@ static pvcop::formatter_desc get_formatter_desc(const std::string& type,
 	return {type, type_format};
 }
 
+void PVRush::PVTypesDiscoveryOutput::append_time_formats(
+    const std::unordered_set<std::string>& time_formats)
+{
+	std::ofstream f;
+	f.open(get_user_time_formats_path(), std::ios_base::app | std::ios_base::out);
+	for (const std::string& time_format : time_formats) {
+		f << time_format << std::endl;
+	}
+}
+
+std::unordered_set<std::string> PVRush::PVTypesDiscoveryOutput::supported_time_formats()
+{
+	std::unordered_set<std::string> supported_time_formats;
+
+	// supplied time formats
+	for (const std::string& suplied_time_format : SUPPLIED_TIMES_FORMATS) {
+		supported_time_formats.emplace(suplied_time_format);
+	}
+
+	// user time formats
+	std::string time_format;
+	std::ifstream f(get_user_time_formats_path());
+	for (std::string supported_time_format; getline(f, supported_time_format);) {
+		supported_time_formats.emplace(supported_time_format);
+	}
+	f.close();
+
+	return supported_time_formats;
+}
+
 void PVRush::PVTypesDiscoveryOutput::prepare_load(const PVRush::PVFormat& format)
 {
+	_types = supported_types();
 	_column_count = format.get_storage_format().size();
 
-	for (const auto& type : TYPES) {
+	for (const auto& type : _types) {
 		pvcop::formatter_desc fd = get_formatter_desc(type.first.first, type.first.second);
 		_formatters.emplace_back(pvcop::types::factory::create(fd.name(), fd.parameters()));
 	}
@@ -109,7 +183,7 @@ void PVRush::PVTypesDiscoveryOutput::operator()(PVCore::PVChunk* chunk)
 					 */
 					if (local_row == 0 && matching_formatters[col][idx]) {
 						for (size_t i = 0; i < _formatters.size(); i++) {
-							for (const std::string& excl_fmt : TYPES[idx].second) {
+							for (const std::string& excl_fmt : _types[idx].second) {
 								if (strcmp(_formatters[i]->name(), excl_fmt.c_str()) == 0) {
 									matching_formatters[col][i] = false;
 								}
@@ -147,7 +221,7 @@ void PVRush::PVTypesDiscoveryOutput::job_has_finished(const PVControllerJob::inv
 		bool formatter_found = false;
 		for (size_t idx = 0; idx < _formatters.size(); idx++) {
 			if (_matching_formatters[col][idx]) {
-				_types_desc.emplace_back(TYPES[idx].first.first, TYPES[idx].first.second);
+				_types_desc.emplace_back(_types[idx].first.first, _types[idx].first.second);
 				formatter_found = true;
 				break;
 			}
