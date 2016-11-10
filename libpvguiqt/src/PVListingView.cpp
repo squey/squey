squey/squey

commit 8556fe37dd5530bab9d5678671e0db65f7a59efe

Allow 'search for this value' to take into account every selected values

libpvguiqt/src/PVListingView.cpp
@@ -371,7 +371,16 @@ void PVGuiQt::PVListingView::show_ctxt_menu(const QPoint& pos)
 	// to the menu's actions.
 	_ctxt_row = listing_model()->rowIndex(idx_click);
 	_ctxt_col = idx_click.column(); // This is the *combined* axis index
-	_ctxt_v = listing_model()->data(idx_click, Qt::DisplayRole).toString();
+
+	const Inendi::PVSource& src = _view.get_parent<Inendi::PVSource>();
+
+	QStringList l;
+	for (PVRow line : listing_model()->shown_lines()) {
+		if (listing_model()->current_selection().get_line_fast(line)) {
+			l << QString::fromStdString(src.get_input_value(line, _ctxt_col));
+		}
+	}
+	_ctxt_v = l.join("\n");
 
 	// Show the menu at the given pos
 	QAction* act_sel = _ctxt_menu.exec(QCursor::pos());

commit 16675d888d884a00f1ee9d05671c4de14477776a

Fix value copying after an axes combination change
This regression was introduced in commit 8556fe37dd5530bab9d5678671e0db65f7a59efe
This also affect multiple-search which rely on this feature...

libpvguiqt/src/PVListingView.cpp
@@ -373,13 +373,14 @@ void PVGuiQt::PVListingView::show_ctxt_menu(const QPoint& pos)
 	// to the menu's actions.
 	_ctxt_row = listing_model()->rowIndex(idx_click);
 	_ctxt_col = idx_click.column(); // This is the *combined* axis index
+	PVCol col = _view.get_axes_combination().get_nraw_axis(_ctxt_col);
 
 	const Inendi::PVSource& src = _view.get_parent<Inendi::PVSource>();
 
 	QStringList l;
 	for (PVRow line : listing_model()->shown_lines()) {
 		if (listing_model()->current_selection().get_line_fast(line)) {
-			l << QString::fromStdString(src.get_input_value(line, _ctxt_col));
+			l << QString::fromStdString(src.get_input_value(line, col));
 		}
 	}
 	_ctxt_v = l.join("\n");