squey/squey

commit 4b4cdbe2fb22d243ca245599bfedaca5e96a590e

[PVPlotted] Used pvcop::db::array as underlying storage

libpvparallelview/src/include/pvparallelview/PVZonesManager.h
@@ -87,27 +87,20 @@ class PVZonesManager : public QObject
 	inline PVCol get_number_cols() const { return _ncols; }
 	inline PVRow get_row_count() const { return _nrows; }
 
-	inline Inendi::PVPlotted::uint_plotted_table_t const& get_uint_plotted() const
-	{
-		assert(_uint_plotted);
-		return *_uint_plotted;
-	}
+	inline Inendi::PVPlotted const& get_plotted() const { return _plotted; }
 
   public:
 	inline PVZoneProcessing get_zone_processing(PVZoneID const z) const
 	{
-		return {get_row_count(), Inendi::PVPlotted::get_plotted_col_addr(
-		                             *_uint_plotted, get_row_count(), _axes_comb[z]),
-		        Inendi::PVPlotted::get_plotted_col_addr(*_uint_plotted, get_row_count(),
-		                                                _axes_comb[z + 1])};
+		return {get_row_count(), _plotted.get_column_pointer(z),
+		        _plotted.get_column_pointer(z + 1)};
 	}
 
   protected:
-	Inendi::PVPlotted::uint_plotted_table_t const* _uint_plotted =
-	    nullptr;                   // FIXME : This is a duplication, it should get it from view
-	PVRow _nrows = 0;              // FIXME : This is a duplication, it should get it from view
-	PVCol _ncols = 0;              // FIXME : This is a duplication, it should get it from view
-	std::vector<PVCol> _axes_comb; // FIXME : This is a duplication, it should get it from view
+	const Inendi::PVPlotted& _plotted; // FIXME : This is a duplication, it should get it from view
+	PVRow _nrows = 0;                  // FIXME : This is a duplication, it should get it from view
+	PVCol _ncols = 0;                  // FIXME : This is a duplication, it should get it from view
+	std::vector<PVCol> _axes_comb;     // FIXME : This is a duplication, it should get it from view
 	// _axes_comb is copied to handle update once the axes_combination have been update in the view.
 	std::vector<PVZone> _zones;
 };

libinendi/plugins/plotting-filters/default/PVPlottingFilterMinmax.cpp
@@ -14,14 +14,14 @@
 template <class T>
 static void compute_minmax_plotting(pvcop::db::array const& mapped,
                                     pvcop::db::array const& minmax,
-                                    uint32_t* dest)
+                                    pvcop::core::array<uint32_t>& dest)
 {
 	auto& mm = minmax.to_core_array<T>();
 	double ymin = (double)mm[0];
 	double ymax = (double)mm[1];
 
 	if (ymin == ymax) {
-		std::fill_n(dest, mapped.size(), 0x80000000);
+		std::fill_n(dest.begin(), mapped.size(), 0x80000000);
 		return;
 	}
 	assert(ymax > ymin);
@@ -37,8 +37,9 @@ static void compute_minmax_plotting(pvcop::db::array const& mapped,
 	}
 }
 
-void Inendi::PVPlottingFilterMinmax::
-operator()(pvcop::db::array const& mapped, pvcop::db::array const& minmax, uint32_t* dest)
+void Inendi::PVPlottingFilterMinmax::operator()(pvcop::db::array const& mapped,
+                                                pvcop::db::array const& minmax,
+                                                pvcop::core::array<uint32_t>& dest)
 {
 	assert(dest);
 

libinendi/plugins/plotting-filters/default/PVPlottingFilterMinmax.h
@@ -18,7 +18,7 @@ class PVPlottingFilterMinmax : public PVPlottingFilter
   public:
 	void operator()(pvcop::db::array const& mapped,
 	                pvcop::db::array const& minmax,
-	                uint32_t* dest) override;
+	                pvcop::core::array<uint32_t>& dest) override;
 	QString get_human_name() const override { return QString("Min/max"); }
 
 	std::set<plotting_capability> list_usable_type() const override { return {}; }

libinendi/plugins/plotting-filters/enum/PVPlottingFilterEnum.cpp
@@ -15,8 +15,9 @@
 
 using plotting_t = uint32_t;
 
-void Inendi::PVPlottingFilterEnum::
-operator()(pvcop::db::array const& mapped, pvcop::db::array const&, uint32_t* dest)
+void Inendi::PVPlottingFilterEnum::operator()(pvcop::db::array const& mapped,
+                                              pvcop::db::array const&,
+                                              pvcop::core::array<uint32_t>& dest)
 {
 	pvcop::db::groups groups;
 	pvcop::db::extents extents;
@@ -31,7 +32,7 @@ operator()(pvcop::db::array const& mapped, pvcop::db::array const&, uint32_t* de
 
 	if (extents.size() == 1) {
 		const plotting_t default_value = std::numeric_limits<plotting_t>::max() / 2;
-		std::fill_n(dest, mapped.size(), default_value);
+		std::fill_n(dest.begin(), mapped.size(), default_value);
 	} else {
 		// -1 as we count "number of space between values", not "values"
 		const double extend_factor =

libinendi/plugins/plotting-filters/enum/PVPlottingFilterEnum.h
@@ -18,7 +18,7 @@ class PVPlottingFilterEnum : public PVPlottingFilter
   public:
 	void operator()(pvcop::db::array const& mapped,
 	                pvcop::db::array const& minmax,
-	                uint32_t* dest) override;
+	                pvcop::core::array<uint32_t>& dest) override;
 	QString get_human_name() const override { return QString("Uniform"); }
 
 	std::set<plotting_capability> list_usable_type() const override { return {}; }

libinendi/plugins/plotting-filters/log_minmax/PVPlottingFilterLogMinmax.cpp
@@ -12,15 +12,16 @@
 #include <omp.h>
 
 template <class T>
-static void
-compute_log_plotting(pvcop::db::array const& mapped, pvcop::db::array const& minmax, uint32_t* dest)
+static void compute_log_plotting(pvcop::db::array const& mapped,
+                                 pvcop::db::array const& minmax,
+                                 pvcop::core::array<uint32_t>& dest)
 {
 	auto& mm = minmax.to_core_array<T>();
 	double ymin = (double)mm[0];
 	double ymax = (double)mm[1];
 
 	if (ymin == ymax) {
-		std::fill_n(dest, mapped.size(), 1UL << 31);
+		std::fill_n(dest.begin(), mapped.size(), 1UL << 31);
 		return;
 	}
 
@@ -41,8 +42,9 @@ compute_log_plotting(pvcop::db::array const& mapped, pvcop::db::array const& min
 	}
 }
 
-void Inendi::PVPlottingFilterLogMinmax::
-operator()(pvcop::db::array const& mapped, pvcop::db::array const& minmax, uint32_t* dest)
+void Inendi::PVPlottingFilterLogMinmax::operator()(pvcop::db::array const& mapped,
+                                                   pvcop::db::array const& minmax,
+                                                   pvcop::core::array<uint32_t>& dest)
 {
 	assert(dest);
 

libinendi/plugins/plotting-filters/log_minmax/PVPlottingFilterLogMinmax.h
@@ -18,7 +18,7 @@ class PVPlottingFilterLogMinmax : public PVPlottingFilter
   public:
 	void operator()(pvcop::db::array const& mapped,
 	                pvcop::db::array const& minmax,
-	                uint32_t* dest) override;
+	                pvcop::core::array<uint32_t>& dest) override;
 	QString get_human_name() const override { return QString("Logarithmic min/max"); }
 
 	std::set<plotting_capability> list_usable_type() const override { return {}; }

libinendi/plugins/plotting-filters/port/PVPlottingFilterPort.cpp
@@ -8,7 +8,8 @@
 #include "PVPlottingFilterPort.h"
 
 template <class T>
-static void compute_port_plotting(pvcop::db::array const& mapped, uint32_t* dest)
+static void compute_port_plotting(pvcop::db::array const& mapped,
+                                  pvcop::core::array<uint32_t>& dest)
 {
 	auto& values = mapped.to_core_array<T>();
 
@@ -30,8 +31,9 @@ static void compute_port_plotting(pvcop::db::array const& mapped, uint32_t* dest
 	}
 }
 
-void Inendi::PVPlottingFilterPort::
-operator()(pvcop::db::array const& mapped, pvcop::db::array const&, uint32_t* dest)
+void Inendi::PVPlottingFilterPort::operator()(pvcop::db::array const& mapped,
+                                              pvcop::db::array const&,
+                                              pvcop::core::array<uint32_t>& dest)
 {
 	// FIXME : We may inform user if minmax is not in 65535 - 0
 	assert(dest);

commit fb66e9ecfc188d7556355375a9556b929b5c76ae

[PVParallelView] fix wrong column retrieval in PVZonesManager
When changing the axes combination of a PVView, the zones were wrong (good
name but bad plotted/mapped/column).

The regression comes from the commit 4b4cdbe2fb22d243ca245599bfedaca5e96a590e
which removed the use of the axes combination indirection to get the right
plotted array.

close issue #491

libpvparallelview/src/include/pvparallelview/PVZonesManager.h
@@ -92,8 +92,8 @@ class PVZonesManager : public QObject
   public:
 	inline PVZoneProcessing get_zone_processing(PVZoneID const z) const
 	{
-		return {get_row_count(), _plotted.get_column_pointer(PVCol(z)),
-		        _plotted.get_column_pointer(PVCol(z + 1))};
+		return {get_row_count(), _plotted.get_column_pointer(_axes_comb[PVCol(z)]),
+		        _plotted.get_column_pointer(_axes_comb[PVCol(z + 1)])};
 	}
 
   protected: