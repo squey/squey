squey/squey

commit 394bb1232bddcf9eeb650e7639e915acee8ac1f6

[PVCorrelationEngine] Unregister proper view on PVView deletion

libinendi/src/PVCorrelationEngine.cpp
@@ -27,7 +27,12 @@ void Inendi::PVCorrelationEngine::add(const PVCorrelation& c)
 
 void Inendi::PVCorrelationEngine::remove(const Inendi::PVView* view)
 {
-	_correlations.erase(view);
+	for (const auto& c : _correlations) {
+		if (view == c.second.view1 or view == c.second.view2) {
+			_correlations.erase(c.first);
+			break;
+		}
+	}
 }
 
 bool Inendi::PVCorrelationEngine::exists(const PVCorrelation& c) const

commit 6055e4de809b7c4be41de6058a167f46f282c989

Fix regression preventing reciproque correlations
Introduced in commit 394bb1232bddcf9eeb650e7639e915acee8ac1f6
while fixing ticket #507

libinendi/src/PVCorrelationEngine.cpp
@@ -25,10 +25,21 @@ void Inendi::PVCorrelationEngine::add(const PVCorrelation& c)
 	_correlations.emplace(c.view1, c);
 }
 
-void Inendi::PVCorrelationEngine::remove(const Inendi::PVView* view)
+void Inendi::PVCorrelationEngine::remove(const Inendi::PVView* view, bool both_ways /*= false*/)
 {
 	for (const auto& c : _correlations) {
-		if (view == c.second.view1 or view == c.second.view2) {
+		if (view == c.second.view1) {
+			_correlations.erase(c.first);
+			break;
+		}
+	}
+
+	if (not both_ways) {
+		return;
+	}
+
+	for (const auto& c : _correlations) {
+		if (view == c.second.view2) {
 			_correlations.erase(c.first);
 			break;
 		}

libinendi/src/PVView.cpp
@@ -119,7 +119,7 @@ Inendi::PVView::~PVView()
 	PVLOG_DEBUG("In PVView destructor: 0x%x\n", this);
 
 	// remove correlation
-	get_parent<Inendi::PVRoot>().correlations().remove(this);
+	get_parent<Inendi::PVRoot>().correlations().remove(this, true /*both_ways*/);
 
 #ifdef WITH_MINESET
 	for (const std::string& mineset_dataset : _mineset_datasets) {

libinendi/src/include/inendi/PVCorrelationEngine.h
@@ -48,9 +48,10 @@ class PVCorrelationEngine
 	/**
 	 * Deactivate an existing correlation
 	 *
-	 * @param view1 the origin view
+	 * @param view the origin view1
+	 * @param both_ways specify if view2 is also removed from correlations
 	 */
-	void remove(const Inendi::PVView* view1);
+	void remove(const Inendi::PVView* view1, bool both_ways = false);
 
 	/**
 	 * Return the associated correlation for a given view