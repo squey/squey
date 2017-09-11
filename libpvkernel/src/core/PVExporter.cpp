9cc272cf4d9ab805edb4bdd9ad0a5a1b51465603
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2017-09-11 11:01:41 +0200

    Create export API to allow inputs to configure custom exporter

--- libpvkernel/src/core/PVExporter.cpp
+++ libpvkernel/src/core/PVExporter.cpp
@@ -25,20 +25,20 @@
 
 #include <tbb/pipeline.h>
 
-const std::string PVCore::PVExporter::default_sep_char = ",";
-const std::string PVCore::PVExporter::default_quote_char = "\"";
+const size_t PVCore::PVCSVExporter::STEP_COUNT = 2000;
+const std::string PVCore::PVCSVExporter::default_sep_char = ",";
+const std::string PVCore::PVCSVExporter::default_quote_char = "\"";
 
-PVCore::PVExporter::PVExporter(const std::string& file_path,
-                               const PVCore::PVSelBitField& sel,
-                               const PVCore::PVColumnIndexes& column_indexes,
-                               PVRow total_row_count,
-                               const export_func& f,
-                               const std::string& sep_char /* = default_sep_char */,
-                               const std::string& quote_char, /* = default_quote_char */
-                               const std::string& header      /* = std::string() */
-                               )
-    : _file_path(file_path)
-    , _sel(sel)
+PVCore::PVCSVExporter::PVCSVExporter(const std::string& file_path,
+                                     const PVCore::PVSelBitField& sel,
+                                     const PVCore::PVColumnIndexes& column_indexes,
+                                     PVRow total_row_count,
+                                     const export_func& f,
+                                     const std::string& sep_char /* = default_sep_char */,
+                                     const std::string& quote_char, /* = default_quote_char */
+                                     const std::string& header      /* = std::string() */
+                                     )
+    : PVExporterBase(file_path, sel)
     , _column_indexes(column_indexes)
     , _total_row_count(total_row_count)
     , _f(f)
@@ -53,71 +53,82 @@ PVCore::PVExporter::PVExporter(const std::string& file_path,
 	}
 }
 
-size_t PVCore::PVExporter::export_rows(PVRow sc /* = 0 */)
+void PVCore::PVCSVExporter::export_rows()
 {
-	PVRow step_count =
-	    std::min((sc ? sc : _total_row_count), _total_row_count - _exported_row_count);
+	size_t exported_row_count = 0;
 
-	int thread_index = -1;
-	const size_t thread_count = std::thread::hardware_concurrency();
-	const size_t range = step_count / thread_count;
+	while (exported_row_count < _total_row_count) {
 
-	tbb::parallel_pipeline(
-	    thread_count /* = max_number_of_live_token */,
-	    tbb::make_filter<void, std::pair<size_t, size_t>>(
-	        tbb::filter::serial_in_order,
-	        [&](tbb::flow_control& fc) -> std::pair<size_t, size_t> {
-		        if ((size_t)++thread_index == thread_count) {
-			        fc.stop();
-			        return {};
-		        }
+		if (_canceled) {
+			_compressor.cancel();
+			std::remove(_file_path.c_str());
+			break;
+		}
 
-		        const size_t begin_index = _exported_row_count + (thread_index * range);
-		        const size_t len = (size_t)thread_index == (thread_count - 1)
-		                               ? step_count - ((thread_count - 1) * range)
-		                               : range;
-		        const size_t end_index = begin_index + len;
+		size_t step_count = std::min(STEP_COUNT, _total_row_count - exported_row_count);
 
-		        return std::make_pair(begin_index, end_index);
-		    }) &
-	        tbb::make_filter<std::pair<size_t, size_t>, std::string>(
-	            tbb::filter::parallel,
-	            [&](const std::pair<size_t, size_t>& range) -> std::string {
+		int thread_index = -1;
+		const size_t thread_count = std::thread::hardware_concurrency();
+		const size_t range = step_count / thread_count;
 
-		            const size_t begin_index = range.first;
-		            const size_t end_index = range.second;
+		tbb::parallel_pipeline(
+		    thread_count /* = max_number_of_live_token */,
+		    tbb::make_filter<void, std::pair<size_t, size_t>>(
+		        tbb::filter::serial_in_order,
+		        [&](tbb::flow_control& fc) -> std::pair<size_t, size_t> {
+			        if ((size_t)++thread_index == thread_count) {
+				        fc.stop();
+				        return {};
+			        }
 
-		            std::string content;
-		            for (PVRow row_index = begin_index; row_index < end_index; row_index++) {
+			        const size_t begin_index = exported_row_count + (thread_index * range);
+			        const size_t len = (size_t)thread_index == (thread_count - 1)
+			                               ? step_count - ((thread_count - 1) * range)
+			                               : range;
+			        const size_t end_index = begin_index + len;
 
-			            if (!_sel.get_line_fast(row_index)) {
-				            continue;
+			        return std::make_pair(begin_index, end_index);
+			    }) &
+		        tbb::make_filter<std::pair<size_t, size_t>, std::string>(
+		            tbb::filter::parallel,
+		            [&](const std::pair<size_t, size_t>& range) -> std::string {
+
+			            const size_t begin_index = range.first;
+			            const size_t end_index = range.second;
+
+			            std::string content;
+			            for (PVRow row_index = begin_index; row_index < end_index; row_index++) {
+
+				            if (!_sel.get_line_fast(row_index)) {
+					            continue;
+				            }
+
+				            content +=
+				                _f(row_index, _column_indexes, _sep_char, _quote_char) + "\n";
 			            }
 
-			            content += _f(row_index, _column_indexes, _sep_char, _quote_char) + "\n";
-		            }
+			            return content;
 
-		            return content;
+			        }) &
+		        tbb::make_filter<std::string, void>(
+		            tbb::filter::serial_in_order, [&](const std::string& content) {
+			            try {
+				            _compressor.write(content);
+			            } catch (const PVCore::PVStreamingCompressorError& e) {
+				            throw PVExportError(e.what());
+			            }
+			        }));
 
-		        }) &
-	        tbb::make_filter<std::string, void>(
-	            tbb::filter::serial_in_order, [&](const std::string& content) {
-		            try {
-			            _compressor.write(content);
-		            } catch (const PVCore::PVStreamingCompressorError& e) {
-			            throw PVExportError(e.what());
-		            }
-		        }));
+		exported_row_count += step_count;
 
-	return _exported_row_count += step_count;
-}
+		if (_f_progress) {
+			_f_progress(exported_row_count);
+		}
+	}
 
-void PVCore::PVExporter::cancel()
-{
-	_compressor.cancel();
-}
-
-void PVCore::PVExporter::wait_finished()
-{
 	_compressor.wait_finished();
+
+	if (_f_progress) {
+		_f_progress(_total_row_count);
+	}
 }
