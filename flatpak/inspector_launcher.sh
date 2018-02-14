e1c39c92dfc58d85af4d021e82c90a505f03ed03
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2018-02-14 13:51:35 +0100

    set OCL_ICD_VENDORS path to /etc/opencl_vendors
    This is needed as
    https://github.com/flatpak/freedesktop-sdk-images/pull/78
    is now merged, /etc/OpenCL is not writable at runtime anymore

--- flatpak/inspector_launcher.sh
+++ flatpak/inspector_launcher.sh
@@ -2,13 +2,15 @@
 
 inendi-lsinit &> /dev/null
 
+export OCL_ICD_VENDORS=/etc/opencl_vendors
+mkdir -p $OCL_ICD_VENDORS
+
 NVIDIA_VERSION_NAME=`ls /usr/lib/GL|grep nvidia-*|sed -e "s/nvidia-//"`
 NVIDIA_VERSION=`echo $NVIDIA_VERSION_NAME | sed -rn 's/([[:digit:]]+)-([[:digit:]]+)/\1\.\2/p'`
 
-mkdir -p /etc/OpenCL/vendors
-echo "/app/lib/libpocl.so" > /etc/OpenCL/vendors/pocl.icd
+echo "/app/lib/libpocl.so" > $OCL_ICD_VENDORS/pocl.icd
 if [ -n "$NVIDIA_VERSION" ]; then
-	echo "/usr/lib/GL/nvidia-$NVIDIA_VERSION_NAME/lib/libnvidia-opencl.so.$NVIDIA_VERSION" > /etc/OpenCL/vendors/nvidia.icd
+	echo "/usr/lib/GL/nvidia-$NVIDIA_VERSION_NAME/lib/libnvidia-opencl.so.$NVIDIA_VERSION" > $OCL_ICD_VENDORS/nvidia.icd
 fi
 
 inendi-inspector
