29b192723eb78fd1c5c80134550641945fd8e296
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2018-02-27 13:10:24 +0100

    Add absolue dir to "build" and "repo" folders

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-flatpak-builder build $DIR/manifest.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --repo=repo
+flatpak-builder $DIR/build $DIR/manifest.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --repo=$DIR/repo
 if [ $? -eq 0 ]; then
 	flatpak update --user com.esi_inendi.Inspector
-	flatpak-builder build $DIR/pcap-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --repo=repo
+	flatpak-builder $DIR/build $DIR/pcap-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --repo=$DIR/repo
 fi
