f1070179b3fa7d43496d868cd30a4420b78b3e3e
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2018-03-14 15:46:44 +0100

    Fix test

--- libpvguiqt/tests/sort_filter.cpp
+++ libpvguiqt/tests/sort_filter.cpp
@@ -1,6 +1,8 @@
 #include <pvkernel/core/inendi_assert.h>
 #include <pvguiqt/PVSortFilter.h>
 
+#include <pvcop/core/selected_array.h>
+
 #include <random>
 #include <chrono>
 #include <iostream>
@@ -10,6 +12,15 @@ constexpr size_t SIZE = 200000000;
 #else
 constexpr size_t SIZE = 10000;
 #endif
+
+static bool is_sorted(const PVGuiQt::PVSortFilter& sf,
+                      const pvcop::core::selected_array<pvcop::db::index_t>& sorted_sel_array)
+{
+	return std::is_sorted(std::begin(sorted_sel_array), std::end(sorted_sel_array),
+	                      [&](pvcop::db::index_t i1, pvcop::db::index_t i2) {
+		                      return sf.row_pos_from_index(i1) < sf.row_pos_from_index(i2);
+		                  });
+}
 
 int main()
 {
@@ -21,6 +32,7 @@ int main()
 
 	// Shuffle values as it is the worse but normal case)
 	auto& sort = sf.sorting().to_core_array();
+	const auto& sorted_sel_array = pvcop::core::make_selected_array(sort, sel);
 	std::mt19937 g; // Init from seed to keep same random ordering
 	std::shuffle(sort.begin(), sort.end(), g);
 
@@ -32,7 +44,7 @@ int main()
 	PV_VALID(sf.size(), SIZE / 2);
 
 #ifndef INSPECTOR_BENCH
-	PV_VALID(sf.row_pos_to_index(5), (PVRow)7157);
+	PV_ASSERT_VALID(not is_sorted(sf, sorted_sel_array));
 #endif
 
 	start = std::chrono::steady_clock::now();
@@ -41,7 +53,7 @@ int main()
 	diff += (end - start);
 
 #ifndef INSPECTOR_BENCH
-	PV_VALID(sf.row_pos_to_index(10), (PVRow)7157);
+	PV_ASSERT_VALID(is_sorted(sf, sorted_sel_array));
 #endif
 
 	std::cout << diff.count();
