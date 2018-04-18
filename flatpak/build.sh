30b2af0ed58fe4233ac0482e60865dbde341c27a
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2018-04-18 16:12:37 +0200

    Fix pcap-inspector deployment
    Improve the way flatpak remotes are handled

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -6,10 +6,12 @@ source ./subs_vars.sh $@
 
 if [ $EXPORT_BUILD = true ]; then
     flatpak-builder $DIR/build $DIR/inendi-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --repo=$REPO_DIR || exit 1
-    flatpak update --user com.esi_inendi.Inspector || exit 2
-    flatpak-builder $DIR/build $DIR/pcap-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --repo=$REPO_DIR || exit 3
+    flatpak remote-add --user --if-not-exists --no-gpg-verify `basename "$REPO_DIR"` "$REPO_DIR" || exit 2
+    flatpak install --user `basename "$REPO_DIR"` com.esi_inendi.Inspector || exit 3
+    flatpak update --user com.esi_inendi.Inspector || exit 4
+    flatpak-builder $DIR/build $DIR/pcap-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --repo=$REPO_DIR || exit 5
 else
-    flatpak-builder $DIR/build $DIR/inendi-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --build-only || exit 4
+    flatpak-builder $DIR/build $DIR/inendi-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --build-only || exit 6
 fi
 
 if [ ! -z "$UPLOAD_URL" -a ! -z "$REPO_DIR" ]; then
