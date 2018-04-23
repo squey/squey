9d14d25a4f87e594732e04b211953efe8af8ff89
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2018-04-23 10:40:28 +0200

    Name flatpak branch the same as git branch to avoid build conflicts

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source ./subs_vars.sh $@
 
 if [ $EXPORT_BUILD = true ]; then
-    flatpak-builder $DIR/build $DIR/inendi-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --repo=$REPO_DIR || exit 1
+    flatpak-builder $DIR/build $DIR/inendi-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache --force-clean --delete-build-dirs --repo=$REPO_DIR || exit 1
     flatpak remote-add --user --if-not-exists --no-gpg-verify `basename "$REPO_DIR"` "$REPO_DIR" || exit 2
-    flatpak install --user `basename "$REPO_DIR"` com.esi_inendi.Inspector || exit 3
-    flatpak update --user com.esi_inendi.Inspector || exit 4
-    flatpak-builder $DIR/build $DIR/pcap-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --repo=$REPO_DIR || exit 5
+    flatpak install --user `basename "$REPO_DIR"` com.esi_inendi.Inspector/x86_64/$BRANCH_NAME
+    flatpak update --user com.esi_inendi.Inspector/x86_64/$BRANCH_NAME || exit 3
+    flatpak-builder $DIR/build $DIR/pcap-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache --force-clean --delete-build-dirs --repo=$REPO_DIR || exit 4
 else
-    flatpak-builder $DIR/build $DIR/inendi-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache  --force-clean --delete-build-dirs --build-only || exit 6
+    flatpak-builder $DIR/build $DIR/inendi-inspector.json --gpg-sign=3C88A1109C7272D88C1DA28ABEEF7E7DF6D0F465 --gpg-homedir=$DIR/gnupg --ccache --force-clean --delete-build-dirs --build-only || exit 5
 fi
 
 if [ ! -z "$UPLOAD_URL" -a ! -z "$REPO_DIR" ]; then
