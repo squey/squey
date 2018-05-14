a4a279d10368d9948080aefb512ad2c6192db49f
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2018-05-14 16:53:42 +0200

    Properly delete license object with RAII

--- gui-qt/src/main.cpp
+++ gui-qt/src/main.cpp
@@ -147,12 +147,11 @@ int run_inspector(QApplication& app, int argc, char* argv[])
 		}
 	}
 
-	Inendi::Utils::License::RAII_InitLicense license_manager(
-	    inendi_license_path.toStdString().c_str());
-
 	bool license_ok = false;
 	do {
 		try {
+			Inendi::Utils::License::RAII_InitLicense license_manager(
+			    inendi_license_path.toStdString().c_str());
 			Inendi::Utils::License::RAII_LicenseFeature full_program_license(
 			    INENDI_LICENSE_PREFIX, INENDI_LICENSE_FEATURE);
 			license_ok = true;
@@ -172,6 +171,8 @@ int run_inspector(QApplication& app, int argc, char* argv[])
 			}
 		}
 	} while (not license_ok);
+	Inendi::Utils::License::RAII_InitLicense license_manager(
+	    inendi_license_path.toStdString().c_str());
 	Inendi::Utils::License::RAII_LicenseFeature full_program_license(INENDI_LICENSE_PREFIX,
 	                                                                 INENDI_LICENSE_FEATURE);
 
