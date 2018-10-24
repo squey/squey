7082a25f5722a16a571d3643e8abfd1957d3d1df
Jean-Baptiste Leonesio <jbleonesio@picviz.com>
2018-10-24 15:59:29 +0200

    Windows 10 installer various fixes
    * Fix "Keep configuration" when uninstalling
    * Fix removing "wsl" temporary file during installation
    * Remove unecessary ubuntu packages

--- nsis/resources/install_inspector.sh
+++ nsis/resources/install_inspector.sh
@@ -5,9 +5,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+apt-get -y remove --purge snap* git* vim* lxd* *openssh* man* krb5* lsof htop ed nano mlocate rsync parted ntfs-3g xfsprogs btrfs-progs dosfstools pciutils cloud-init tcpdump geoip-database gawk lvm2 iproute2 rsyslog strace keyboard-configuration shared-mime-info command-not-found-data busybox* groff-base language-selector-common iptables zerofree ubuntu-advantage-tools tmux telnet screen
+apt-get -y autoremove
 apt-get update
 apt-get -y dist-upgrade
-apt-get -y install wget libappstream-glib8 libjson-glib-1.0-0 libgpgme11
+apt-get -y install sed wget libappstream-glib8 libjson-glib-1.0-0 libgpgme11 xdg-desktop-portal
+apt-get clean
 wget https://repo.esi-inendi.com/wsl/ostree_2099.4-1_amd64.deb
 wget https://repo.esi-inendi.com/wsl/flatpak_1.99-1_amd64.deb
 dpkg -i ostree*.deb && rm -f ostree*.deb
