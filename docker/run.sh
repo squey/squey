2f6ff7ef2cabab927a426e864af09c3f0e5731df
Jean-Baptiste Leonesio <jean-baptiste@leonesio.fr>
2022-03-22 09:53:57 +0100

    Fix docker install

--- docker/run.sh
+++ docker/run.sh
@@ -13,4 +13,4 @@ then
     source resources/check_nvidia-docker.sh
 fi
 
-${DOCKER} ${DOCKER_OPTS} run --privileged ${NVIDIA_DOCKER_RUNTIME} --rm --name inendi-inspector -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 443:443 -d inendi/inspector
+${DOCKER} ${DOCKER_OPTS} run --privileged ${NVIDIA_DOCKER_RUNTIME} --name inendi-inspector -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 443:443 -d --restart unless-stopped inendi/inspector
