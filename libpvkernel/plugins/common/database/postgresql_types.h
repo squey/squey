b3f0493bbfad5f6324ace7c11ccff3819b1eab98
Jean-Baptiste Leonesio <jean-baptiste@leonesio.fr>
2022-04-12 12:45:53 +0200

    Enable Qt Postgresql support

---
+++ libpvkernel/plugins/common/database/postgresql_types.h
@@ -0,0 +1,31 @@
+#ifndef __PVKERNEL_POSTGRESQL_TYPES_H__
+#define __PVKERNEL_POSTGRESQL_TYPES_H__
+
+// types defines in qtbase/src/plugins/sqldrivers/psql/qsql_psql.cpp
+
+#define QBOOLOID 16
+#define QINT8OID 20
+#define QINT2OID 21
+#define QINT4OID 23
+#define QNUMERICOID 1700
+#define QFLOAT4OID 700
+#define QFLOAT8OID 701
+#define QABSTIMEOID 702
+#define QRELTIMEOID 703
+#define QDATEOID 1082
+#define QTIMEOID 1083
+#define QTIMETZOID 1266
+#define QTIMESTAMPOID 1114
+#define QTIMESTAMPTZOID 1184
+#define QOIDOID 2278
+#define QBYTEAOID 17
+#define QREGPROCOID 24
+#define QXIDOID 28
+#define QCIDOID 29
+
+#define QBITOID 1560
+#define QVARBITOID 1562
+
+#define VARHDRSZ 4
+
+#endif // __PVKERNEL_POSTGRESQL_TYPES_H__
