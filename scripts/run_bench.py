abde9026eb4292c440161605a66feca8a6240975
Jean-Baptiste Leonesio <jean-baptiste+inspector@leonesio.fr>
2023-02-17 17:22:23 +0100

    Fix CWE-78
    https://cwe.mitre.org/data/definitions/78.html

--- scripts/run_bench.py
+++ scripts/run_bench.py
@@ -18,13 +18,13 @@ CODESPEED_URL = 'http://codespeed.srv.picviz/'
 
 def get_git_hash():
     """ Get hash of the current git HEAD. """
-    return subprocess.check_output("git rev-parse HEAD", shell=True)
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD'], shell=False)
 
 
 def get_git_branch():
     """ Get the current git branch. """
-    return subprocess.check_output("git rev-parse --abbrev-ref HEAD",
-                                   shell=True).strip()
+    return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+                                   shell=False).strip()
 
 
 def run(program_name, num_run):
