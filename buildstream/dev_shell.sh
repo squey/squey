dc54b5b083984741e3b6e49ce4237c3c7199a1b9
Jean-Baptiste Leonesio <jean-baptiste+inspector@leonesio.fr>
2023-02-20 13:04:37 +0100

    Automatically source python virtual environment if ".venv" directory exists

--- buildstream/dev_shell.sh
+++ buildstream/dev_shell.sh
@@ -7,6 +7,11 @@ xhost +
 source .common.sh
 scripts/cuda-devices &> /dev/null || true
 
+if [ -d ".venv" ]; then
+    echo "Activating python virtual environment"
+    source .venv/bin/activate
+fi
+
 open_workspace "workspace_dev"
 
 bst shell $MOUNT_OPTS --build inendi-inspector.bst
