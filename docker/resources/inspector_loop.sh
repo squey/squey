6ff9e0e43b4b6d4040390b3ce72d1d61debcf7a5
Jean-Baptiste Leonesio <jean-baptiste+inspector@leonesio.fr>
2023-02-28 15:43:07 +0100

    Fix default branch ("stable" instead of "master")

--- docker/resources/inspector_loop.sh
+++ docker/resources/inspector_loop.sh
@@ -3,7 +3,7 @@
 while true; do
     # Run INENDI Inspector with "--devel" option to be able to generate crash reports
     branch=`cat /opt/inendi/inspector/current_branch.txt`
-    [[ -z "$branch" ]] && b="master" || b="$branch"
+    [[ -z "$branch" ]] && b="stable" || b="$branch"
     PYTHON_VERSION=`flatpak run --command="python3" com.gitlab.inendi.Inspector//$b -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`
     output=`flatpak run --devel --env="PYTHONPATH=/opt/inendi/inspector/python${PYTHON_VERSION}/site-packages" com.gitlab.inendi.Inspector//$b 2>&1`
     ret=$?
