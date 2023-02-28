b7f863c5c7920dc3f559d2e682208496e7165a35
Jean-Baptiste Leonesio <jean-baptiste+inspector@leonesio.fr>
2023-02-28 15:42:06 +0100

    Handle system and user flatpak installations

--- docker/download_files.sh
+++ docker/download_files.sh
@@ -12,23 +12,33 @@ then
     command -v flatpak &> /dev/null || { echo >&2 "'flatpak' executable is required to execute this script."; exit 1; }
     command -v wget &> /dev/null || { echo >&2 "'wget' executable is required to execute this script."; exit 1; }
 
-    flatpak remote-add --user --if-not-exists flathub "${FLATHUB_REPO}"
-    flatpak remote-add --user --if-not-exists --no-gpg-verify inendi_tmp https://inendi.gitlab.io/flatpak/
+    if [[ `flatpak remote-list |grep flathub |grep -v -q system` == 1 ]]; then
+        flatpak remote-add --user --if-not-exists flathub "${FLATHUB_REPO}"
+    fi
+
+    FLATPAK_SYSTEM_REPO_DIR="/var/lib/flatpak/repo"
+    FLATPAK_USER_REPO_DIR="~/.local/share/flatpak/repo"
 
     # Export Freedesktop runtime bundle
     echo "[1/4] Exporting Flatpak runtime bundle ..."
-    flatpak info "$RUNTIME_NAME//$RUNTIME_BRANCH" &> /dev/null
+    flatpak_install_type=$(flatpak info "$RUNTIME_NAME//$RUNTIME_BRANCH" | grep "Installation: " | cut -d " " -f 2)
     runtime_not_installed=$?
-    if [ $runtime_not_installed -eq 1 ]
-    then
-        flatpak install --user -y flathub "$RUNTIME_NAME//$RUNTIME_BRANCH"  &> /dev/null
+    if [ $flatpak_install_type == "user" ]; then
+        USER_OPT="--user"
+        FLATPAK_REPO_DIR="$FLATPAK_USER_REPO_DIR"
     else
-        flatpak update --user -y "$RUNTIME_NAME//$RUNTIME_BRANCH" &> /dev/null
+        FLATPAK_REPO_DIR="$FLATPAK_SYSTEM_REPO_DIR"
     fi
-    flatpak build-bundle --runtime ~/.local/share/flatpak/repo "${DATA_PATH}/runtime.flatpak" "$RUNTIME_NAME" "$RUNTIME_BRANCH"
     if [ $runtime_not_installed -eq 1 ]
     then
-        flatpak uninstall --user -y "$RUNTIME_NAME//$RUNTIME_BRANCH" &> /dev/null
+        flatpak install $USER_OPT -y flathub "$RUNTIME_NAME//$RUNTIME_BRANCH"  &> /dev/null
+    else
+        flatpak update $USER_OPT -y "$RUNTIME_NAME//$RUNTIME_BRANCH" &> /dev/null
+    fi
+    flatpak build-bundle --runtime $FLATPAK_REPO_DIR "${DATA_PATH}/runtime.flatpak" "$RUNTIME_NAME" "$RUNTIME_BRANCH"
+    if [ $runtime_not_installed -eq 1 ]
+    then
+        flatpak uninstall $USER_OPT -y "$RUNTIME_NAME//$RUNTIME_BRANCH" &> /dev/null
     fi
 
     # Export Freedesktop Sdk bundle
@@ -37,14 +47,14 @@ then
     sdk_not_installed=$?
     if [ $sdk_not_installed -eq 1 ]
     then
-        flatpak install --user -y flathub "$SDK_NAME//$RUNTIME_BRANCH"  &> /dev/null
+        flatpak install $USER_OPT -y flathub "$SDK_NAME//$RUNTIME_BRANCH"  &> /dev/null
     else
-        flatpak update --user -y "$SDK_NAME//$RUNTIME_BRANCH" &> /dev/null
+        flatpak update $USER_OPT -y "$SDK_NAME//$RUNTIME_BRANCH" &> /dev/null
     fi
-    flatpak build-bundle --runtime ~/.local/share/flatpak/repo "${DATA_PATH}/sdk.flatpak" "$SDK_NAME" "$RUNTIME_BRANCH"
+    flatpak build-bundle --runtime $FLATPAK_REPO_DIR "${DATA_PATH}/sdk.flatpak" "$SDK_NAME" "$RUNTIME_BRANCH"
     if [ $sdk_not_installed -eq 1 ]
     then
-        flatpak uninstall --user -y "$SDK_NAME//$RUNTIME_BRANCH" &> /dev/null
+        flatpak uninstall $USER_OPT -y "$SDK_NAME//$RUNTIME_BRANCH" &> /dev/null
     fi
 
     # Export NVIDIA drivers bundle
@@ -53,14 +63,14 @@ then
         echo "[2/4] Exporting NVIDIA drivers bundle ..."
         if [ $runtime_not_installed -eq 1 ]
         then
-            flatpak install --user -y flathub "$DRIVERS_NAME//$DRIVERS_BRANCH" &> /dev/null
+            flatpak install $USER_OPT -y flathub "$DRIVERS_NAME//$DRIVERS_BRANCH" &> /dev/null
         else
-            flatpak update --user -y "$DRIVERS_NAME//$DRIVERS_BRANCH" &> /dev/null
+            flatpak update $USER_OPT -y "$DRIVERS_NAME//$DRIVERS_BRANCH" &> /dev/null
         fi
-        flatpak build-bundle --runtime ~/.local/share/flatpak/repo "${DATA_PATH}/drivers.flatpak" "$DRIVERS_NAME" "$DRIVERS_BRANCH"
+        flatpak build-bundle --runtime $FLATPAK_REPO_DIR "${DATA_PATH}/drivers.flatpak" "$DRIVERS_NAME" "$DRIVERS_BRANCH"
         if [ $runtime_not_installed -eq 1 ]
         then
-            flatpak uninstall --user -y "$DRIVERS_NAME//$DRIVERS_BRANCH" &> /dev/null
+            flatpak uninstall $USER_OPT -y "$DRIVERS_NAME//$DRIVERS_BRANCH" &> /dev/null
         fi
     else
         echo "[2/4] Skipping exporting NVIDIA drivers bundle as we don't have any GPU"
@@ -72,18 +82,17 @@ then
     inspector_not_installed=$?
     if [ $inspector_not_installed  -eq 1 ]
     then
-        flatpak install --user -y inendi_tmp "${INSPECTOR_NAME}" &> /dev/null
+        flatpak install $USER_OPT -y flathub "${INSPECTOR_NAME}" &> /dev/null
     else
-        flatpak update --user -y "${INSPECTOR_NAME}" &> /dev/null
+        flatpak update $USER_OPT -y "${INSPECTOR_NAME}" &> /dev/null
     fi
-    flatpak build-bundle ~/.local/share/flatpak/repo "${DATA_PATH}/inendi-inspector.flatpak" "${INSPECTOR_NAME}"
+    flatpak build-bundle $FLATPAK_REPO_DIR "${DATA_PATH}/inendi-inspector.flatpak" "${INSPECTOR_NAME}" "stable"
     if [ $inspector_not_installed -eq 1 ]
     then
-        flatpak uninstall --user -y "${INSPECTOR_NAME}" &> /dev/null
+        flatpak uninstall $USER_OPT -y "${INSPECTOR_NAME}" &> /dev/null
     fi
 
-    flatpak remote-delete --user inendi_tmp
-
+    # Download NICE DCV
     wget $NICE_DCV_URL -P "${DATA_PATH}"
 fi
 
