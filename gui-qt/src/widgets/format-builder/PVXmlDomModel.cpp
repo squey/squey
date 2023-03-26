squey/squey

commit 186130c390643a9980479fa2f661e935c8a0e6d1

Fix cppcheck warnings

gui-qt/src/widgets/format-builder/PVXmlDomModel.cpp
@@ -1055,7 +1055,7 @@ void PVInspector::PVXmlDomModel::updateAxesCombination()
 		_axes_combination.reset_to_default();
 	} else {
 		auto comb = _axes_combination.get_combination();
-		std::remove_if(comb.begin(), comb.end(), [this](PVCol c) { return c >= _axes.size(); });
+		comb.erase(std::remove_if(comb.begin(), comb.end(), [this](PVCol c) { return c >= _axes.size(); }));
 		if (comb.empty()) {
 			comb.resize(_axes.size());
 			std::iota(comb.begin(), comb.end(), PVCol(0));

libinendi/plugins/mapping-filters/mac_address/PVMappingFilterMacAddress.cpp
@@ -50,12 +50,12 @@ using mac_t = uint64_t;
 using vendor_t = uint64_t;
 using nic_t = uint64_t;
 
-inline vendor_t mac_to_vendor(const mac_t mac)
+inline vendor_t mac_to_vendor(mac_t mac)
 {
 	return mac >> 24;
 }
 
-inline nic_t mac_to_nic(const mac_t mac)
+inline nic_t mac_to_nic(mac_t mac)
 {
 	return mac % nic_max_count;
 }

libinendi/src/PVAxis.cpp
@@ -90,7 +90,7 @@ PVCore::PVArgumentList Inendi::PVAxis::args_from_node(node_args_t const& args_st
 {
 	PVCore::PVArgumentList ret;
 	node_args_t::const_iterator it;
-	for (it = args_str.begin(); it != args_str.end(); it++) {
+	for (it = args_str.begin(); it != args_str.end(); ++it) {
 		QString const& key(it.key());
 		if (def_args.contains(key)) {
 			ret[it.key()] = PVCore::QString_to_PVArgument(it.value(), def_args.at(key));

libpvguiqt/src/PVAbstractListStatsDlg.cpp
@@ -310,7 +310,7 @@ PVGuiQt::PVAbstractListStatsDlg::PVAbstractListStatsDlg(Inendi::PVView& view,
 	Inendi::PVLayerFilter::p_type fclone = search_multiple->clone<Inendi::PVLayerFilter>();
 	Inendi::PVLayerFilter::hash_menu_function_t const& entries = fclone->get_menu_entries();
 	Inendi::PVLayerFilter::hash_menu_function_t::const_iterator it_ent;
-	for (it_ent = entries.begin(); it_ent != entries.end(); it_ent++) {
+	for (it_ent = entries.begin(); it_ent != entries.end(); ++it_ent) {
 		QAction* act = new QAction(it_ent->key(), _values_view);
 		act->setData(QVariant(search_multiples)); // Save the name of the layer
 		                                          // filter associated to this

libpvguiqt/src/PVHiveDataTreeModel.cpp
@@ -104,7 +104,7 @@ namespace
 {
 struct get_size {
 	template <class T>
-	size_t call(T* v) const
+	static size_t call(T* v)
 	{
 		return v->size();
 	}
@@ -204,7 +204,7 @@ namespace
 {
 struct obj_pos {
 	template <class T>
-	int call(T* v) const
+	static int call(T* v)
 	{
 		auto children = v->get_parent().get_children();
 		return std::distance(children.begin(), std::find(children.begin(), children.end(), v));

libpvguiqt/src/PVListingView.cpp
@@ -110,7 +110,7 @@ PVGuiQt::PVListingView::PVListingView(Inendi::PVView& view, QWidget* parent)
 	// We should add a connect for every plugins action and save a context which
 	// will be updated before sending the signal so that we can process plugins
 	// widgets
-	for (const_layer_iterator it = lf.begin(); it != lf.end(); it++) {
+	for (const_layer_iterator it = lf.begin(); it != lf.end(); ++it) {
 		Inendi::PVLayerFilter::hash_menu_function_t const& entries =
 		    it->value()->get_menu_entries();
 		using const_layer_menu_iterator =
@@ -118,7 +118,7 @@ PVGuiQt::PVListingView::PVListingView(Inendi::PVView& view, QWidget* parent)
 		PVLOG_DEBUG("(listing context-menu) for filter '%s', there are %d entries\n",
 		            qPrintable(it->key()), entries.size());
 		for (const_layer_menu_iterator it_ent = entries.begin(); it_ent != entries.end();
-		     it_ent++) {
+		     ++it_ent) {
 			PVLOG_DEBUG("(listing context-menu) add action '%s' for filter '%s'\n",
 			            qPrintable(it_ent->key()), qPrintable(it->key()));
 			QAction* act = new QAction(it_ent->key(), &_ctxt_menu);

libpvkernel/plugins/common/database/PVDBInfos.h
@@ -83,7 +83,7 @@ class PVDBInfos
 	QString _options;
 	QString _type;
 	QString _dbname;
-	uint16_t _port;
+	uint16_t _port = 0;
 };
 
 class PVDBServ : public PVDBInfos

libpvkernel/plugins/common/elasticsearch/PVElasticsearchAPI.h
@@ -319,8 +319,8 @@ class PVElasticsearchAPI
 	PVRush::PVElasticsearchInfos _infos; // Contains all the info to reach Elasticsearch server
 	size_t _scroll_count = 0; // total count of event that will be returned by a scrolling session
 	bool _init_scroll = true;
-	size_t _slice_count;
-	size_t _max_result_window;
+	size_t _slice_count = 0;
+	size_t _max_result_window = 0;
 	PVCore::PVVersion _version;
 	mutable std::string _mapping_type; // for retro-compatibility
 	using curlp_t = std::unique_ptr<CURL, std::function<void(CURL*)>>;

commit 429240557e4a68066d51ece1ecb6c618132c308e

Fix crash in format builder introduced in commit 186130c390643a9980479fa2f661e935c8a0e6d1

gui-qt/src/widgets/format-builder/PVXmlDomModel.cpp
@@ -1055,7 +1055,7 @@ void PVInspector::PVXmlDomModel::updateAxesCombination()
 		_axes_combination.reset_to_default();
 	} else {
 		auto comb = _axes_combination.get_combination();
-		comb.erase(std::remove_if(comb.begin(), comb.end(), [this](PVCol c) { return c >= _axes.size(); }));
+		std::erase_if(comb, [this](PVCol c) { return c >= _axes.size(); });
 		if (comb.empty()) {
 			comb.resize(_axes.size());
 			std::iota(comb.begin(), comb.end(), PVCol(0));