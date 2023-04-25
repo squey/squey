squey/squey

commit b6590f8e251fe150299355b3384e186dd36d57a9

Add clang-tools-extra ostree dependency

buildstream/plugins/elements/flatpak_image.py
@@ -1,140 +1,0 @@
-# Copyright (c) 2017 freedesktop-sdk
-# Copyright (c) 2018 Codethink Limited
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# Authors:
-#        Valentin David <valentin.david@gmail.com>
-#        Thomas Coldrick <thomas.coldrick@codethink.co.uk>
-
-
-"""Flatpak Image Element
-
-A buildstream plugin used to stage its build-dependencies, and metadata
-provided by the 'metadata' field in a format useful to generate flatpaks.
-"""
-import os
-from buildstream import utils
-from buildstream import Element, ElementError, Scope
-import configparser
-
-class FlatpakImageElement(Element):
-
-    BST_STRICT_REBUILD = True
-    BST_FORMAT_VERSION = 1
-
-    def configure(self, node):
-        self.node_validate(node, [
-            'directory', 'include', 'exclude', 'metadata',
-            'export'
-        ])
-        self.directory = self.node_subst_member(node, 'directory')
-        export = self.node_subst_member(node, 'export')
-        export_abs = os.path.join('/', export)
-        self.export = os.path.relpath(export_abs, '/')
-        self.include = self.node_get_member(node, list, 'include')
-        self.exclude = self.node_get_member(node, list, 'exclude')
-        self.metadata = configparser.ConfigParser()
-        self.metadata.optionxform = str
-        metadata_dict = {}
-        for section, pairs in node.get('metadata').items():
-            if not section.startswith('__bst'):
-                section_dict = {}
-                for key in pairs.keys():
-                    if not key.startswith('__bst'):
-                        section_dict[key] = self.node_subst_member(pairs, key)
-                metadata_dict[section] = section_dict
-
-        self.metadata.read_dict(metadata_dict)
-
-    def preflight(self):
-        runtime_deps = list(self.dependencies(Scope.RUN, recurse=False))
-        if runtime_deps:
-            raise ElementError("{}: Only build type dependencies supported by flatpak_image elements"
-                               .format(self))
-
-        sources = list(self.sources())
-        if sources:
-            raise ElementError("{}: flatpak_image elements may not have sources".format(self))
-
-    def get_unique_key(self):
-        key = {}
-        key['directory'] = self.directory
-        key['include'] = sorted(self.include)
-        key['exclude'] = sorted(self.exclude)
-        key['metadata'] = self.metadata
-        key['export'] = self.export
-        key['version'] = 11              # Used to force rebuilds after editing the plugin
-        return key
-
-    def configure_sandbox(self, sandbox):
-        pass
-
-    def stage(self, sandbox):
-        pass
-
-    def assemble(self, sandbox):
-        self.stage_sources(sandbox, 'input')
-
-        basedir = sandbox.get_directory()
-        allfiles = os.path.join(basedir, 'buildstream', 'allfiles')
-        reldirectory = os.path.relpath(self.directory, '/')
-        subdir = os.path.join(allfiles, reldirectory)
-        exportsrcdir = os.path.join(allfiles, self.export)
-        etcdir = os.path.join(allfiles, 'etc')
-        installdir = os.path.join(basedir, 'buildstream', 'install')
-        filesdir = os.path.join(installdir, 'files')
-        exportdir = os.path.join(installdir, 'export')
-        filesetcdir = os.path.join(filesdir, 'etc')
-        stagedir = os.path.join(os.sep, 'buildstream', 'allfiles')
-
-        os.makedirs(allfiles, exist_ok=True)
-        os.makedirs(filesdir, exist_ok=True)
-        if self.metadata.has_section('Application'):
-            os.makedirs(exportdir, exist_ok=True)
-
-        for section in self.metadata.sections():
-            if section.startswith('Extension '):
-                try:
-                    extensiondir = self.metadata.get(section, 'directory')
-                    os.makedirs(os.path.join(installdir, 'files', extensiondir), exist_ok=True)
-                except PermissionError as e:
-                    raise ElementError("Permission denied: Cannot create {}".format(extensiondir))
-
-        with self.timed_activity("Creating flatpak image", silent_nested=True):
-            self.stage_dependency_artifacts(sandbox, Scope.BUILD,
-                                            path=stagedir,
-                                            include=self.include,
-                                            exclude=self.exclude)
-            utils.link_files(subdir, filesdir)
-            if os.path.exists(etcdir):
-                utils.link_files(etcdir, filesetcdir)
-
-            if os.path.isdir(exportsrcdir):
-                utils.link_files(exportsrcdir, exportdir)
-            elif self.metadata.has_section('Application'):
-                os.makedirs(exportdir, exist_ok=True)
-
-        metadatafile = os.path.join(installdir, 'metadata')
-        with open(metadatafile, "w") as m:
-            self.metadata.write(m)
-        return os.path.join(os.sep, 'buildstream', 'install')
-
-def setup():
-    return FlatpakImageElement

buildstream/plugins/elements/flatpak_image.yaml
@@ -1,4 +1,0 @@
-config:
-  include: []
-  exclude: []
-  export: 'export'

buildstream/plugins/elements/flatpak_repo.py
@@ -1,101 +1,0 @@
-#  Copyright (C) 2018 Abderrahim Kitouni
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
-#  Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library. If not, see <http://www.gnu.org/licenses/>.
-#
-#  Authors:
-#        Abderrahim Kitouni <akitouni@gnome.org>
-
-"""flatpak repository element
-
-A :mod:`ScriptElement <buildstream.scriptelement>` implementation for exporting
-a flatpak repository from a set of `flatpak_image`s.
-
-The flatpak_repo default configuration:
-  .. literalinclude:: ../../../bst_external/elements/flatpak_repo.yaml
-     :language: yaml
-"""
-
-from buildstream import ScriptElement, Scope, ElementError
-
-class FlatpakRepoElement(ScriptElement):
-    BST_FORMAT_VERSION = 1
-    BST_ARTIFACT_VERSION = 1
-
-    def configure(self, node):
-        self.node_validate(node, ['environment', 'copy-refs', 'repo-mode', 'arch', 'branch'])
-
-        self._env = self.node_get_member(node, list, 'environment')
-
-        self._copy_refs = []
-        for subnode in self.node_get_member(node, list, 'copy-refs'):
-            self.node_validate(subnode, ['src', 'dest'])
-            self._copy_refs.append((self.node_subst_member(subnode, 'src'),
-                                    self.node_subst_member(subnode, 'dest')))
-
-        self._arch = self.node_subst_member(node, 'arch')
-        self._branch = self.node_subst_member(node, 'branch')
-
-        self.set_work_dir()
-        self.set_root_read_only(True)
-
-        self._repo_mode = self.node_subst_member(node, 'repo-mode')
-        self.set_install_root('/buildstream/repo')
-        self.add_commands('init repository',
-                          ['ostree init --repo=/buildstream/repo --mode={}'.format(self._repo_mode)])
-
-    def _layout_flatpaks(self, elements):
-        def staging_dir(elt):
-            return '/buildstream/input/{}'.format(elt.name)
-
-        def export_command(elt):
-            return 'flatpak build-export --disable-sandbox --files=files --arch={} /buildstream/repo {} {}'\
-                .format(self._arch, staging_dir(elt), self._branch)
-
-        for elt in elements:
-            if elt.get_kind() == 'flatpak_image':
-                self.layout_add(elt.name, staging_dir(elt))
-                self.add_commands('export {}'.format(elt.name), [export_command(elt)])
-            elif elt.get_kind() == 'stack':
-                self._layout_flatpaks(elt.dependencies(Scope.RUN, recurse=False))
-            else:
-                raise ElementError('Dependency {} is not of kind flatpak_image'.format(elt.name))
-
-    def stage(self, sandbox):
-        env = [self.search(Scope.BUILD, elt) for elt in self._env]
-        flatpaks = [elt for elt in self.dependencies(Scope.BUILD, recurse=False) if elt not in env]
-
-        for elt in env:
-            self.layout_add(elt.name, '/')
-
-        self._layout_flatpaks(flatpaks)
-
-        for src, dest in self._copy_refs:
-            self.add_commands('copy ref {} -> {}'.format(src, dest),
-                              ['flatpak build-commit-from --src-ref={} /buildstream/repo {}'.format(src, dest)])
-
-        super(FlatpakRepoElement, self).stage(sandbox)
-
-    def get_unique_key(self):
-        return {
-            'environment': self._env,
-            'copy-refs': self._copy_refs,
-            'repo-mode': self._repo_mode,
-            'arch': self._arch,
-            'branch': self._branch
-        }
-
-
-# Plugin entry point
-def setup():
-    return FlatpakRepoElement

buildstream/plugins/elements/flatpak_repo.yaml
@@ -1,10 +1,0 @@
-config:
-  # elements (from the build dependencies) that should be staged in /
-  environment: []
-  # refs of which a copy is to be made using flatpak build-commit-from
-  # a list of mappings containing keys `src` and `dest`
-  copy-refs: []
-  # the mode of the ostree repo (one of archive, bare, bare-user, bare-user-only)
-  repo-mode: 'archive'
-  arch: '%{arch}'
-  branch: '%{branch}'

buildstream/plugins/elements/snap_image.py
@@ -1,78 +1,0 @@
-from ruamel import yaml
-import os
-from buildstream import Element, ElementError, Scope
-import collections
-
-class SnapImageElement(Element):
-
-    def configure(self, node):
-        self.node_validate(node, [
-            'directory', 'include', 'exclude', 'metadata'
-        ])
-        self.directory = self.node_subst_member(node, 'directory')
-        self.include = self.node_get_member(node, list, 'include')
-        self.exclude = self.node_get_member(node, list, 'exclude')
-        self.metadata = self._clean_meta_data(node.get('metadata'))
-
-    def _clean_meta_data(self, node):
-        ret = {}
-        for k, v in node.items():
-            if not k.startswith('__bst'):
-                if isinstance(v, collections.Mapping):
-                    ret[k] = self._clean_meta_data(v)
-                elif isinstance(v, list):
-                    ret[k] = self.node_subst_list(node, k)
-                else:
-                    ret[k] = self.node_subst_member(node, k)
-        return ret
-
-    def preflight(self):
-        runtime_deps = list(self.dependencies(Scope.RUN, recurse=False))
-        if runtime_deps:
-            raise ElementError("{}: Only build type dependencies supported by flatpak_image elements"
-                               .format(self))
-
-        sources = list(self.sources())
-        if sources:
-            raise ElementError("{}: flatpak_image elements may not have sources".format(self))
-
-    def get_unique_key(self):
-        key = {}
-        key['directory'] = self.directory
-        key['include'] = sorted(self.include)
-        key['exclude'] = sorted(self.exclude)
-        key['metadata'] = self.metadata
-        key['version'] = 7
-        return key
-
-    def configure_sandbox(self, sandbox):
-        pass
-
-    def stage(self, sandbox):
-        pass
-
-    def assemble(self, sandbox):
-        basedir = sandbox.get_directory()
-
-        reldirectory = os.path.relpath(self.directory, os.sep)
-        rootdir = os.path.join(basedir, reldirectory)
-
-        metadir = os.path.join(rootdir, 'meta')
-        metadata = os.path.join(metadir, 'snap.yaml')
-
-        with self.timed_activity("Creating snap image", silent_nested=True):
-            self.stage_dependency_artifacts(sandbox,
-                                            Scope.BUILD,
-                                            include=self.include,
-                                            exclude=self.exclude)
-
-            os.makedirs(metadir, exist_ok=True)
-
-            with open(metadata, 'w') as f:
-                yaml.dump(self.metadata, f)
-
-
-        return os.path.join(os.sep, reldirectory)
-
-def setup():
-    return SnapImageElement

buildstream/plugins/elements/snap_image.yaml
@@ -1,3 +1,0 @@
-config:
-  include: []
-  exclude: []

project.conf
@@ -12,15 +12,17 @@ element-path: buildstream/elements
 
 plugins:
 - origin: local
-  path: buildstream/plugins/elements
-  elements:
-    flatpak_image: 1
-    flatpak_repo: 1
-
-- origin: local
   path: buildstream/plugins/bst-external/bst_external/sources
+  elements:
+    flatpak_image: 0
+    flatpak_repo: 0
   sources:
     git_tag: 0
+
+- origin: local
+  path: buildstream/plugins/sources
+  sources:
+    ostree: 0
 
 options:
 

buildstream/elements/clang-tools-extra.bst
@@ -1,0 +1,13 @@
+kind: import
+description: Clang tools extra
+
+sources:
+- kind: ostree
+  url: https://flathub.org/repo
+  track: runtime/org.freedesktop.Sdk.Extension.llvm14/x86_64/22.08
+  ref: 0b511a7831204d7a0b307be1eb3b88b47d81cf25a6c5bd9c4d28e5a6dcaf54ef
+
+config:
+  source: files
+  target: usr
+

commit 40044b17446c1151ff9031aa3873dec1ce1092b2

Fix flatpak export broken in commit b6590f8e251fe150299355b3384e186dd36d57a9

buildstream/plugins/elements/flatpak_image.py
@@ -1,0 +1,140 @@
+# Copyright (c) 2017 freedesktop-sdk
+# Copyright (c) 2018 Codethink Limited
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Authors:
+#        Valentin David <valentin.david@gmail.com>
+#        Thomas Coldrick <thomas.coldrick@codethink.co.uk>
+
+
+"""Flatpak Image Element
+
+A buildstream plugin used to stage its build-dependencies, and metadata
+provided by the 'metadata' field in a format useful to generate flatpaks.
+"""
+import os
+from buildstream import utils
+from buildstream import Element, ElementError, Scope
+import configparser
+
+class FlatpakImageElement(Element):
+
+    BST_STRICT_REBUILD = True
+    BST_FORMAT_VERSION = 1
+
+    def configure(self, node):
+        self.node_validate(node, [
+            'directory', 'include', 'exclude', 'metadata',
+            'export'
+        ])
+        self.directory = self.node_subst_member(node, 'directory')
+        export = self.node_subst_member(node, 'export')
+        export_abs = os.path.join('/', export)
+        self.export = os.path.relpath(export_abs, '/')
+        self.include = self.node_get_member(node, list, 'include')
+        self.exclude = self.node_get_member(node, list, 'exclude')
+        self.metadata = configparser.ConfigParser()
+        self.metadata.optionxform = str
+        metadata_dict = {}
+        for section, pairs in node.get('metadata').items():
+            if not section.startswith('__bst'):
+                section_dict = {}
+                for key in pairs.keys():
+                    if not key.startswith('__bst'):
+                        section_dict[key] = self.node_subst_member(pairs, key)
+                metadata_dict[section] = section_dict
+
+        self.metadata.read_dict(metadata_dict)
+
+    def preflight(self):
+        runtime_deps = list(self.dependencies(Scope.RUN, recurse=False))
+        if runtime_deps:
+            raise ElementError("{}: Only build type dependencies supported by flatpak_image elements"
+                               .format(self))
+
+        sources = list(self.sources())
+        if sources:
+            raise ElementError("{}: flatpak_image elements may not have sources".format(self))
+
+    def get_unique_key(self):
+        key = {}
+        key['directory'] = self.directory
+        key['include'] = sorted(self.include)
+        key['exclude'] = sorted(self.exclude)
+        key['metadata'] = self.metadata
+        key['export'] = self.export
+        key['version'] = 11              # Used to force rebuilds after editing the plugin
+        return key
+
+    def configure_sandbox(self, sandbox):
+        pass
+
+    def stage(self, sandbox):
+        pass
+
+    def assemble(self, sandbox):
+        self.stage_sources(sandbox, 'input')
+
+        basedir = sandbox.get_directory()
+        allfiles = os.path.join(basedir, 'buildstream', 'allfiles')
+        reldirectory = os.path.relpath(self.directory, '/')
+        subdir = os.path.join(allfiles, reldirectory)
+        exportsrcdir = os.path.join(allfiles, self.export)
+        etcdir = os.path.join(allfiles, 'etc')
+        installdir = os.path.join(basedir, 'buildstream', 'install')
+        filesdir = os.path.join(installdir, 'files')
+        exportdir = os.path.join(installdir, 'export')
+        filesetcdir = os.path.join(filesdir, 'etc')
+        stagedir = os.path.join(os.sep, 'buildstream', 'allfiles')
+
+        os.makedirs(allfiles, exist_ok=True)
+        os.makedirs(filesdir, exist_ok=True)
+        if self.metadata.has_section('Application'):
+            os.makedirs(exportdir, exist_ok=True)
+
+        for section in self.metadata.sections():
+            if section.startswith('Extension '):
+                try:
+                    extensiondir = self.metadata.get(section, 'directory')
+                    os.makedirs(os.path.join(installdir, 'files', extensiondir), exist_ok=True)
+                except PermissionError as e:
+                    raise ElementError("Permission denied: Cannot create {}".format(extensiondir))
+
+        with self.timed_activity("Creating flatpak image", silent_nested=True):
+            self.stage_dependency_artifacts(sandbox, Scope.BUILD,
+                                            path=stagedir,
+                                            include=self.include,
+                                            exclude=self.exclude)
+            utils.link_files(subdir, filesdir)
+            if os.path.exists(etcdir):
+                utils.link_files(etcdir, filesetcdir)
+
+            if os.path.isdir(exportsrcdir):
+                utils.link_files(exportsrcdir, exportdir)
+            elif self.metadata.has_section('Application'):
+                os.makedirs(exportdir, exist_ok=True)
+
+        metadatafile = os.path.join(installdir, 'metadata')
+        with open(metadatafile, "w") as m:
+            self.metadata.write(m)
+        return os.path.join(os.sep, 'buildstream', 'install')
+
+def setup():
+    return FlatpakImageElement

buildstream/plugins/elements/flatpak_image.yaml
@@ -1,0 +1,4 @@
+config:
+  include: []
+  exclude: []
+  export: 'export'

buildstream/plugins/elements/flatpak_repo.py
@@ -1,0 +1,101 @@
+#  Copyright (C) 2018 Abderrahim Kitouni
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors:
+#        Abderrahim Kitouni <akitouni@gnome.org>
+
+"""flatpak repository element
+
+A :mod:`ScriptElement <buildstream.scriptelement>` implementation for exporting
+a flatpak repository from a set of `flatpak_image`s.
+
+The flatpak_repo default configuration:
+  .. literalinclude:: ../../../bst_external/elements/flatpak_repo.yaml
+     :language: yaml
+"""
+
+from buildstream import ScriptElement, Scope, ElementError
+
+class FlatpakRepoElement(ScriptElement):
+    BST_FORMAT_VERSION = 1
+    BST_ARTIFACT_VERSION = 1
+
+    def configure(self, node):
+        self.node_validate(node, ['environment', 'copy-refs', 'repo-mode', 'arch', 'branch'])
+
+        self._env = self.node_get_member(node, list, 'environment')
+
+        self._copy_refs = []
+        for subnode in self.node_get_member(node, list, 'copy-refs'):
+            self.node_validate(subnode, ['src', 'dest'])
+            self._copy_refs.append((self.node_subst_member(subnode, 'src'),
+                                    self.node_subst_member(subnode, 'dest')))
+
+        self._arch = self.node_subst_member(node, 'arch')
+        self._branch = self.node_subst_member(node, 'branch')
+
+        self.set_work_dir()
+        self.set_root_read_only(True)
+
+        self._repo_mode = self.node_subst_member(node, 'repo-mode')
+        self.set_install_root('/buildstream/repo')
+        self.add_commands('init repository',
+                          ['ostree init --repo=/buildstream/repo --mode={}'.format(self._repo_mode)])
+
+    def _layout_flatpaks(self, elements):
+        def staging_dir(elt):
+            return '/buildstream/input/{}'.format(elt.name)
+
+        def export_command(elt):
+            return 'flatpak build-export --disable-sandbox --files=files --arch={} /buildstream/repo {} {}'\
+                .format(self._arch, staging_dir(elt), self._branch)
+
+        for elt in elements:
+            if elt.get_kind() == 'flatpak_image':
+                self.layout_add(elt.name, staging_dir(elt))
+                self.add_commands('export {}'.format(elt.name), [export_command(elt)])
+            elif elt.get_kind() == 'stack':
+                self._layout_flatpaks(elt.dependencies(Scope.RUN, recurse=False))
+            else:
+                raise ElementError('Dependency {} is not of kind flatpak_image'.format(elt.name))
+
+    def stage(self, sandbox):
+        env = [self.search(Scope.BUILD, elt) for elt in self._env]
+        flatpaks = [elt for elt in self.dependencies(Scope.BUILD, recurse=False) if elt not in env]
+
+        for elt in env:
+            self.layout_add(elt.name, '/')
+
+        self._layout_flatpaks(flatpaks)
+
+        for src, dest in self._copy_refs:
+            self.add_commands('copy ref {} -> {}'.format(src, dest),
+                              ['flatpak build-commit-from --src-ref={} /buildstream/repo {}'.format(src, dest)])
+
+        super(FlatpakRepoElement, self).stage(sandbox)
+
+    def get_unique_key(self):
+        return {
+            'environment': self._env,
+            'copy-refs': self._copy_refs,
+            'repo-mode': self._repo_mode,
+            'arch': self._arch,
+            'branch': self._branch
+        }
+
+
+# Plugin entry point
+def setup():
+    return FlatpakRepoElement

buildstream/plugins/elements/flatpak_repo.yaml
@@ -1,0 +1,10 @@
+config:
+  # elements (from the build dependencies) that should be staged in /
+  environment: []
+  # refs of which a copy is to be made using flatpak build-commit-from
+  # a list of mappings containing keys `src` and `dest`
+  copy-refs: []
+  # the mode of the ostree repo (one of archive, bare, bare-user, bare-user-only)
+  repo-mode: 'archive'
+  arch: '%{arch}'
+  branch: '%{branch}'

buildstream/plugins/elements/snap_image.py
@@ -1,0 +1,78 @@
+from ruamel import yaml
+import os
+from buildstream import Element, ElementError, Scope
+import collections
+
+class SnapImageElement(Element):
+
+    def configure(self, node):
+        self.node_validate(node, [
+            'directory', 'include', 'exclude', 'metadata'
+        ])
+        self.directory = self.node_subst_member(node, 'directory')
+        self.include = self.node_get_member(node, list, 'include')
+        self.exclude = self.node_get_member(node, list, 'exclude')
+        self.metadata = self._clean_meta_data(node.get('metadata'))
+
+    def _clean_meta_data(self, node):
+        ret = {}
+        for k, v in node.items():
+            if not k.startswith('__bst'):
+                if isinstance(v, collections.Mapping):
+                    ret[k] = self._clean_meta_data(v)
+                elif isinstance(v, list):
+                    ret[k] = self.node_subst_list(node, k)
+                else:
+                    ret[k] = self.node_subst_member(node, k)
+        return ret
+
+    def preflight(self):
+        runtime_deps = list(self.dependencies(Scope.RUN, recurse=False))
+        if runtime_deps:
+            raise ElementError("{}: Only build type dependencies supported by flatpak_image elements"
+                               .format(self))
+
+        sources = list(self.sources())
+        if sources:
+            raise ElementError("{}: flatpak_image elements may not have sources".format(self))
+
+    def get_unique_key(self):
+        key = {}
+        key['directory'] = self.directory
+        key['include'] = sorted(self.include)
+        key['exclude'] = sorted(self.exclude)
+        key['metadata'] = self.metadata
+        key['version'] = 7
+        return key
+
+    def configure_sandbox(self, sandbox):
+        pass
+
+    def stage(self, sandbox):
+        pass
+
+    def assemble(self, sandbox):
+        basedir = sandbox.get_directory()
+
+        reldirectory = os.path.relpath(self.directory, os.sep)
+        rootdir = os.path.join(basedir, reldirectory)
+
+        metadir = os.path.join(rootdir, 'meta')
+        metadata = os.path.join(metadir, 'snap.yaml')
+
+        with self.timed_activity("Creating snap image", silent_nested=True):
+            self.stage_dependency_artifacts(sandbox,
+                                            Scope.BUILD,
+                                            include=self.include,
+                                            exclude=self.exclude)
+
+            os.makedirs(metadir, exist_ok=True)
+
+            with open(metadata, 'w') as f:
+                yaml.dump(self.metadata, f)
+
+
+        return os.path.join(os.sep, reldirectory)
+
+def setup():
+    return SnapImageElement

buildstream/plugins/elements/snap_image.yaml
@@ -1,0 +1,3 @@
+config:
+  include: []
+  exclude: []

project.conf
@@ -12,17 +12,20 @@ element-path: buildstream/elements
 
 plugins:
 - origin: local
-  path: buildstream/plugins/bst-external/bst_external/sources
+  path: buildstream/plugins/elements
   elements:
     flatpak_image: 0
     flatpak_repo: 0
-  sources:
-    git_tag: 0
 
 - origin: local
   path: buildstream/plugins/sources
   sources:
     ostree: 0
+
+- origin: local
+  path: buildstream/plugins/bst-external/bst_external/sources
+  sources:
+    git_tag: 0
 
 options:
 